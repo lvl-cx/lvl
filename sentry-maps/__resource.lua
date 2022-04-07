7a236470020cc9cd842eea41ce5b21fcdb251c79
Xonaa <80773857+Xonaa-Dev@users.noreply.github.com>
2022-04-07 05:12:37 -0700

    tweak: maps resource.lua

--- sentry-maps/__resource.lua
+++ sentry-maps/__resource.lua
@@ -1,17 +1,4 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 this_is_a_map "yes"
 
-file 'gabz_timecycle_mods_1.xml'
-data_file 'TIMECYCLEMOD_FILE' 'gabz_timecycle_mods_1.xml'
-
-file 'v_kitch.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/int_corporate.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/v_kitch.ytyp'
-data_file 'INTERIOR_PROXY_ORDER_FILE' 'sinnersinteriorproxies.meta'
-
-file 'ub-maccys.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'ub-maccys.ytyp'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/vesp_props.ytyp'
-file 'vespuccipdinteriorproxies.meta'
-data_file 'INTERIOR_PROXY_ORDER_FILE' 'vespuccipdinteriorproxies.meta'
+data_file 'DLC_ITYP_REQUEST' 'stream/**/*.ytyp'
