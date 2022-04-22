5cc5f28877d9588fe4d6ae06d37ae5b9b8ed5a72
Xonaa <80773857+XonaaXYZ@users.noreply.github.com>
2022-04-22 14:48:41 -0700

    Add: settings menu

--- sentry/client/adminmenu.lua
+++ sentry/client/adminmenu.lua
@@ -67,6 +67,7 @@ admincfg.buttonsEnabled = {
 RMenu.Add('adminmenu', 'main', RageUI.CreateMenu("", "~g~Sentry Admin Menu", 1300, 50, 'admin', 'admin'))
 RMenu.Add("adminmenu", "players", RageUI.CreateSubMenu(RMenu:Get("adminmenu", "main")))
 RMenu.Add("adminmenu", "searchoptions", RageUI.CreateSubMenu(RMenu:Get("adminmenu", "main")))
+RMenu.Add("adminmenu", "settings", RageUI.CreateSubMenu(RMenu:Get("adminmenu", "main"),"","~g~Sentry Admin Menu", 1300, 50,"admin","admin"))
 
 --[[ Functions ]]
 RMenu.Add("adminmenu", "functions", RageUI.CreateSubMenu(RMenu:Get("adminmenu", "main")))
@@ -191,6 +192,10 @@ RageUI.CreateWhile(1.0, true, function()
             end, RMenu:Get('adminmenu', 'functions'))
         end
 
+        if admincfg.buttonsEnabled["adminMenu"][1] and buttons["adminMenu"] then
+            RageUI.Button("Settings", nil, {RightLabel = "~g~→"}, true, function(Hovered, Active, Selected)
+            end, RMenu:Get('adminmenu', 'settings'))
+        end
 
     end)
 end
@@ -213,6 +218,243 @@ RageUI.CreateWhile(1.0, true, function()
 end
 end)
 
+
+RMenu.Add('SettingsMenu', 'MainMenu', RageUI.CreateMenu("", "~g~Sentry Settings Menu", 1300, 50,"settings","settings")) 
+
+
+local statusr = "~r~[Off]"
+local hitsounds = false
+
+local statusc = "~r~[Off]"
+local compass = false
+
+local statusT = "~r~[Off]"
+local toggle = false
+
+local df = {
+    {"10%", 0.1},
+    {"20%", 0.2},
+    {"30%", 0.3},
+    {"40%", 0.4},
+    {"50%", 0.5},
+    {"60%", 0.6},
+    {"70%", 0.7},
+    {"80%", 0.8},
+    {"90%", 0.9},
+    {"100%", 1.0},
+    {"150%", 1.5},
+    {"200%", 2.0},
+    {"250%", 2.5},
+    {"300%", 3.0},
+    {"350%", 3.5},
+    {"400%", 4.0},
+    {"450%", 4.5},
+    {"500%", 5.0},
+    {"600%", 6.0},
+    {"700%", 7.0},
+    {"800%", 8.0},
+    {"900%", 9.0},
+    {"1000%", 10.0},
+}
+
+local d = {"10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%", "150%", "200%", "250%", "300%", "350%", "400%", "450%", "500%", "600%", "700%", "800%", "900%", "1000%"}
+local dts = 9
+local HitmarkerSounds = false
+local Compass = false
+local streetnames = false
+local huddisplay = false
+
+RageUI.CreateWhile(1.0, true, function()
+    if RageUI.Visible(RMenu:Get('SettingsMenu', 'MainMenu')) then
+        RageUI.DrawContent({ header = true, glare = false, instructionalButton = true}, function()
+            RageUI.List("Modify Render Distance", d, dts, nil, {}, true, function(a,b,c,d)
+                if c then
+    
+                end
+    
+                dts = d
+            end)
+
+
+            RageUI.Checkbox("Rust Hitsounds",nil, HitmarkerSounds, {Style = RageUI.CheckboxStyle.Car}, function(Hovered, Active, Selected, Checked)
+                if Selected then 
+                    if not HitmarkerSounds then
+                        TriggerEvent("Sentry:Settings:Hitsounds", true)
+                        HitmarkerSounds = true
+                    else
+                        TriggerEvent("Sentry:Settings:Hitsounds", false)
+                        HitmarkerSounds = false
+                    end
+                end
+            end, ToggleHSTrue2, ToggleHSFalse2)
+    
+
+            RageUI.Checkbox("Compass",nil, Compass, {Style = RageUI.CheckboxStyle.Car}, function(Hovered, Active, Selected, Checked)
+                if Selected then 
+                    if not Compass then
+                        ExecuteCommand("compass")
+                        Compass = true
+                    else
+                        ExecuteCommand("compass")
+                        Compass = false
+                    end
+                end
+            end, ToggleHSTrue2, ToggleHSFalse2)
+    
+            RageUI.Checkbox("Street Names", nil, streetnames, { Style = RageUI.CheckboxStyle.Tick }, function(Hovered, Selected, Active, Checked)
+                streetnames = Checked;
+            end)
+
+       end)
+    end
+end)
+
+RageUI.CreateWhile(1.0, true, function()
+    if RageUI.Visible(RMenu:Get('adminmenu', 'settings')) then
+        RageUI.DrawContent({ header = true, glare = false, instructionalButton = true}, function()
+            RageUI.List("Modify Render Distance", d, dts, nil, {}, true, function(a,b,c,d)
+                if c then
+    
+                end
+    
+                dts = d
+            end)
+
+            
+            RageUI.Checkbox("Rust Hitsounds",nil, HitmarkerSounds, {Style = RageUI.CheckboxStyle.Car}, function(Hovered, Active, Selected, Checked)
+                if Selected then 
+                    if not HitmarkerSounds then
+                        TriggerEvent("Sentry:Settings:Hitsounds", true)
+                        HitmarkerSounds = true
+                    else
+                        TriggerEvent("Sentry:Settings:Hitsounds", false)
+                        HitmarkerSounds = false
+                    end
+                end
+            end, ToggleHSTrue2, ToggleHSFalse2)
+    
+
+            RageUI.Checkbox("Compass",nil, Compass, {Style = RageUI.CheckboxStyle.Car}, function(Hovered, Active, Selected, Checked)
+                if Selected then 
+                    if not Compass then
+                        ExecuteCommand("compass")
+                        Compass = true
+                    else
+                        ExecuteCommand("compass")
+                        Compass = false
+                    end
+                end
+            end, ToggleHSTrue2, ToggleHSFalse2)
+
+            RageUI.Checkbox("HUD Display",nil, huddisplay, {Style = RageUI.CheckboxStyle.Car}, function(Hovered, Active, Selected, Checked)
+                if Selected then 
+                    if not huddisplay then
+                        ExecuteCommand("toggleui")
+                        huddisplay = true
+                    else
+                        ExecuteCommand("toggleui")
+                        huddisplay = false
+                    end
+                end
+            end, ToggleHSTrue2, ToggleHSFalse2)
+    
+            RageUI.Checkbox("Street Names", nil, streetnames, { Style = RageUI.CheckboxStyle.Tick }, function(Hovered, Selected, Active, Checked)
+                streetnames = Checked;
+            end)
+
+       end)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        CheckPlayerPosition()
+        Citizen.Wait(1000)
+    end
+end)
+
+
+Citizen.CreateThread(function()
+    while true do
+        while streetnames do
+            disableHud2()
+            local UI = GetMinimapAnchor2()
+
+            drawTxt2(UI.Left_x + 0.003 , UI.Bottom_y - 0.227 , 0.4, "~b~"..Zone.." / ~w~"..GetStreetNameFromHashKey(rua), 255, 255, 255, 255, 0) -- Street
+            Citizen.Wait(1)
+        end
+        Citizen.Wait(1)
+    end
+end)
+
+function CheckPlayerPosition()
+    pos = GetEntityCoords(PlayerPedId())
+    rua, cross = GetStreetNameAtCoord(pos.x, pos.y, pos.z)
+    Zone = GetLabelText(GetNameOfZone(pos.x, pos.y, pos.z))
+end
+ 
+function drawTxt2(x,y,scale,text,r,g,b,a,font)
+    SetTextFont(font)
+    SetTextScale(scale,scale)
+    SetTextColour(r,g,b,a)
+    SetTextOutline()
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(x,y)
+end
+ 
+function disableHud2()
+    HideHudComponentThisFrame(6)
+    HideHudComponentThisFrame(7)   
+    HideHudComponentThisFrame(8)   
+    HideHudComponentThisFrame(9)
+end
+ 
+function GetMinimapAnchor2()
+    local safezone = GetSafeZoneSize()
+    local safezone_x = 1.0 / 20.0
+    local safezone_y = 1.0 / 20.0
+    local aspect_ratio = GetAspectRatio(0)
+    local res_x, res_y = GetActiveScreenResolution()
+    local xscale = 1.0 / res_x
+    local yscale = 1.0 / res_y
+    local Minimap = {}
+    Minimap.Width = xscale * (res_x / (4 * aspect_ratio))
+    Minimap.height = yscale * (res_y / 5.674)
+    Minimap.Left_x = xscale * (res_x * (safezone_x * ((math.abs(safezone - 1.0)) * 10)))
+    Minimap.Bottom_y = 1.0 - yscale * (res_y * (safezone_y * ((math.abs(safezone - 1.0)) * 10)))
+    Minimap.right_x = Minimap.Left_x + Minimap.Width
+    Minimap.top_y = Minimap.Bottom_y - Minimap.height
+    Minimap.x = Minimap.Left_x
+    Minimap.y = Minimap.top_y
+    Minimap.xunit = xscale
+    Minimap.yunit = yscale
+    return Minimap
+end
+
+RegisterNetEvent('Sentry:OpenSettingsMenu')
+AddEventHandler('Sentry:OpenSettingsMenu', function(admin)
+    if not admin then
+        RageUI.Visible(RMenu:Get("adminmenu", "main"), false)
+        RageUI.Visible(RMenu:Get("SettingsMenu", "MainMenu"), true)
+    else
+        --
+    end
+end)
+
+RegisterCommand('opensettingsmenu',function()
+    TriggerServerEvent('Sentry:OpenSettings')
+end)
+
+RegisterKeyMapping('opensettingsmenu', 'Opens the Settings menu', 'keyboard', 'F2')
+
+Citizen.CreateThread(function() 
+    while true do
+        Citizen.InvokeNative(0xA76359FC80B2438E, df[dts][2])      
+                
+        Citizen.Wait(0)
+    end
+end)
 
 
 
