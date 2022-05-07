b47bf15ae2355c22138ddfbfc1d7fb113ac7a28d
Xonaa-Dev <80773857+XonaaXYZ@users.noreply.github.com>
2022-05-07 12:33:19 +0100

    add: all discord logs

--- lvl/servercfg/cfg_webhooks.lua
+++ lvl/servercfg/cfg_webhooks.lua
@@ -13,8 +13,8 @@ webhooks.kicklog = 'hone' -- when an admin kicks a player
 
 -- JOIN/LEAVE LOGS
 
-webhooks.spawnlog = 'none' -- when player passes queue and is spawned.
-webhooks.leavelog = 'none' -- when any player leaves server.
+webhooks.spawnlog = 'https://discord.com/api/webhooks/972460581246353458/F9UWs44upvXoBbmqkIgPn-sQ3GHUhfG3QpIQ032_3hElNAS6tdi9LHzGCTmUuxgJYcxO' -- when player passes queue and is spawned.
+webhooks.leavelog = 'https://discord.com/api/webhooks/972460642911023184/PQ6lMII6C8w2R_Bxf1YsQjzOrErMeRGlu9oV2FLP7r33YK1tH6A8NMsCmIM3pxHCPt2V' -- when any player leaves server.
 
 
 
