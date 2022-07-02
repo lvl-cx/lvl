dde8bb8373456e5c1bbc748dd6afe63ddabc1081
Xonaa-Dev <80773857+Iagging@users.noreply.github.com>
2022-07-02 01:20:54 +0100

    remove: commission from gun stores

--- arma/armacore/modules/sv_largearms.lua
+++ arma/armacore/modules/sv_largearms.lua
@@ -4,8 +4,6 @@ largearms = {}
 
 largearms.location = vector3(-1111.3123779297,4937.2846679688,218.3872833252)
 
-local commision = 0
-local finalID = nil
 RegisterServerEvent("LargeArms:BuyWeapon")
 AddEventHandler('LargeArms:BuyWeapon', function(price, hash)
     local source = source
@@ -13,19 +11,14 @@ AddEventHandler('LargeArms:BuyWeapon', function(price, hash)
     local coords = largearms.location
     local ped = GetPlayerPed(source)
     local playerCoords = GetEntityCoords(ped)
-    finalCommision = price * (commision / 100)
 
     if #(playerCoords - coords) <= 5.0 then 
         if ARMA.getInventoryWeight(userid) <= 25 then
 
-            if ARMA.tryPayment(userid, finalCommision + price) then
+            if ARMA.tryPayment(userid, price) then
                 GiveWeaponToPed(source, hash, 250, false, false)
                 TriggerClientEvent("ARMA:PlaySound", source, 1)
-                ARMAclient.notify(source, {"~g~Purchased Weapon. Paid £" .. tostring(price + finalCommision) .. " +" .. commision .. "% Commision!"})
-                if finalID ~= nil then
-                    ARMA.giveBankMoney(ARMA.getUserId(finalID),finalCommision)
-                    ARMAclient.notify(finalID,{"~g~You have been given £" .. finalCommision.. "~g~."})
-                end
+                ARMAclient.notify(source, {"~g~Purchased Weapon. Paid £" .. tostring(price)})
 
                 webhook = "https://discord.com/api/webhooks/991558186416996442/hRO_L2cHWlEXggwv3bllV596JaCAKYgzIkzikfIEDNzN9BxKYWrnJDwNpUWwUnHKfNgI"
                 
@@ -62,20 +55,15 @@ AddEventHandler('LargeArms:BuyArmour', function()
     local coords = largearms.location
     local ped = GetPlayerPed(source)
     local playerCoords = GetEntityCoords(ped)
-    finalCommision = 50000 * (commision / 100)
 
     if #(playerCoords - coords) <= 5.0 then 
  
         
-        if ARMA.tryPayment(userid, 50000 + finalCommision) then
+        if ARMA.tryPayment(userid, 50000) then
             SetPedArmour(source, 50)
             TriggerClientEvent("ARMA:PlaySound", source, 1)
-            ARMAclient.notify(source, {"~g~Purchased Level 2 Armour. Paid £" .. tostring(50000 + finalCommision) .. " +" .. commision .. "% Commision!"})
+            ARMAclient.notify(source, {"~g~Purchased Level 2 Armour. Paid £" .. tostring(50000)})
             TriggerClientEvent('ARMA:SetVest', source)
-            if finalID ~= nil then
-                ARMA.giveBankMoney(ARMA.getUserId(finalID),finalCommision)
-                ARMAclient.notify(finalID,{"~g~You have been given £" .. finalCommision.. "~g~."})
-            end
         else 
             ARMAclient.notify(source, {"~r~Not enough money."})
             TriggerClientEvent("ARMA:PlaySound", source, 2)
@@ -85,9 +73,4 @@ AddEventHandler('LargeArms:BuyArmour', function()
         ARMA.banConsole(userid,"perm","Cheating/ Triggering Events")
     end
 end)
-
-function SendLargeArms(som, userid2)
-    commision = som 
-    finalID = userid2
-end
 
