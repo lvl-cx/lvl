b0af9eeb9aaf6fd24594a5443edf30094caec6bf
terrorist <34693611+terrorist@users.noreply.github.com>
2022-07-17 22:58:03 +0100

    a

---
+++ arma-deathui/client/cl_spawnui.lua
@@ -0,0 +1,105 @@
+--[[ local cfg = module("BW","cfg/cfg_spawn")
+local spawnCam = nil
+local spawnCam2 = nil
+
+Citizen.CreateThread(function()
+    Citizen.Wait(3000)
+    local success, result = pcall(function()
+        local homeCfg = module("BW","cfg/homes")
+        for homeName, homeInfo in pairs(homeCfg.homes) do
+            cfg.spawnLocations[homeName] = {
+                name = homeName,
+                coords = vector3(homeInfo.entry_point[1],homeInfo.entry_point[2],homeInfo.entry_point[3]),
+                permission = {},
+                image = homeInfo.image or "https://cdn.cmg.city/content/fivem/houses/citysmallhome.png",
+                price = 5000
+            }
+        end
+    end)
+end)
+
+local function dump(o)
+    if type(o) == 'table' then
+        local s = '{ '
+        for k,v in pairs(o) do
+            if type(k) ~= 'number' then k = '"'..k..'"' end
+            s = s .. '['..k..'] = ' .. dump(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(o)
+    end
+end
+
+RegisterNUICallback("respawnButtonClicked", function(data, cb)
+    local name = data.location.name
+    local price = tonumber(data.location.price)
+    if price and price > 0 then
+        TriggerServerEvent('BW:takeAmountRespawn', price)
+    end
+    local spawnCoords =  cfg.spawnLocations[name].coords
+    TriggerEvent("BW:playGTAIntro")
+    SetEntityCoords(PlayerPedId(), spawnCoords)
+    SendNUIMessage({
+        app = "",
+        type = "APP_TOGGLE",
+    })
+    SetNuiFocus(false, false)
+    SetPlayerControl(PlayerId(), 1, 0)
+
+    SetFocusPosAndVel(spawnCoords.x,spawnCoords.y,spawnCoords.z+1000)
+    local spawnCam3 = CreateCameraWithParams("DEFAULT_SCRIPTED_CAMERA", spawnCoords.x,spawnCoords.y,spawnCoords.z+1000, 0.0, 0.0, 0.0, 65.0, 0, 2)
+    SetCamActive(spawnCam3, true)
+    DestroyCam(spawnCam, 0)
+    DestroyCam(spawnCam2, 0)
+    RenderScriptCams(true, true, 0, 1, 0, 0)
+    local spawnCam4 = CreateCameraWithParams("DEFAULT_SCRIPTED_CAMERA", spawnCoords.x,spawnCoords.y,spawnCoords.z, 0.0, 0.0, 0.0, 65.0, 0, 2)
+    SetCamActiveWithInterp(spawnCam4, spawnCam3, 5000, 0, 0)
+    Wait(2500)
+    ClearFocus()
+    Wait(2000)
+    FreezeEntityPosition(PlayerPedId(),false)
+    DestroyCam(spawnCam3)
+    DestroyCam(spawnCam4)
+    RenderScriptCams(false, true, 2000, 0, 0)
+    TriggerScreenblurFadeOut(2000.0)
+    ExecuteCommand("showui")
+    ClearFocus()
+    cb()
+end)
+
+RegisterNetEvent("BW:openSpawnMenu")
+AddEventHandler("BW:openSpawnMenu", function(location_table)
+    DoScreenFadeIn(1000)
+    TriggerScreenblurFadeIn(100.0)
+    ExecuteCommand("hideui")
+    SetPlayerControl(PlayerId(), 0, 0)
+    FreezeEntityPosition(PlayerPedId(),true)
+    TriggerEvent("BW:removeBackpack2")
+    --applySavedCustomisation()
+    SetFocusPosAndVel(675.57568359375,1107.1724853516,375.29666137695)
+    spawnCam = CreateCameraWithParams("DEFAULT_SCRIPTED_CAMERA", 675.57568359375,1107.1724853516,375.29666137695, 0.0, 0.0, 0.0, 65.0, 0, 2)
+    SetCamActive(spawnCam, true)
+    RenderScriptCams(true, true, 0, 1, 0, 0)
+
+    spawnCam2 = CreateCameraWithParams("DEFAULT_SCRIPTED_CAMERA", -1024.6506347656,-2712.0234375,79.889106750488, 0.0, 0.0, 0.0, 65.0, 0, 2)
+
+    SetCamActiveWithInterp(spawnCam2, spawnCam, 250000, 5, 5)
+    local locations = {}
+    for _, homeName in pairs(location_table) do
+        if cfg.spawnLocations[homeName] then
+            table.insert(locations, cfg.spawnLocations[homeName])
+        end
+    end
+    SendNUIMessage({
+        type = "SET_SPAWN_LOCATIONS",
+        info = {
+            locations = locations
+        }
+    })
+    SendNUIMessage({
+        app = "spawn",
+        type = "APP_TOGGLE",
+    })
+    SetNuiFocus(true, true)
+end) ]]
