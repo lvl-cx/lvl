ba46d43f195de0d7c3efcfdef0b11692f33bdfef
terrorist <34693611+terrorist@users.noreply.github.com>
2022-07-26 14:05:27 +0100

    dumb things

--- arma/client/cl_identity.lua
+++ arma/client/cl_identity.lua
@@ -1,7 +1,6 @@
 RMenu.Add('identitymenu', 'main', RageUI.CreateMenu("", "~b~Identity Menu", tARMA.getRageUIMenuWidth(), tARMA.getRageUIMenuHeight(), 'banners', 'identity'))
 
 RageUI.CreateWhile(1.0, true, function()
-
     if RageUI.Visible(RMenu:Get('identitymenu', 'main')) then
         RageUI.DrawContent({ header = true, glare = true, instructionalButton = true}, function()
             RageUI.Button("New Identity" , nil, {RightLabel = "~g~£100"}, true, function(Hovered, Active, Selected)
@@ -18,16 +17,14 @@ Citizen.CreateThread(function()
     while true do
             local v1 = vector3(-911.130859375,-2022.8002929688,12.833104133606)
             if isInArea(v1, 100.0) then 
-                DrawMarker(29, -911.130859375,-2022.8002929688,12.833104133606, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4, 0.4, 0.4, 14, 212, 0, 250, true, true, false, false, nil, nil, false)
+                DrawMarker(32, -911.130859375,-2022.8002929688,12.833104133606, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4, 0.4, 0.4, 14, 212, 0, 250, true, true, false, false, nil, nil, false)
             end
-            if isInMenu == false then
-            if isInArea(v1, 1.4) then 
+            if isInArea(v1, 1.4) and not isInMenu then 
                 alert('Press ~INPUT_VEH_HORN~ to enter identity menu')
                 if IsControlJustPressed(0, 51) then 
                     RageUI.Visible(RMenu:Get("identitymenu", "main"), true)
                     isInMenu = true
                 end
-            end
             end
             if isInArea(v1, 1.4) == false and isInMenu then
                 RageUI.Visible(RMenu:Get("identitymenu", "main"), false)
