7b717b5177f655bd6400d458ae9974852ee99ebf
terrorist <34693611+terrorist@users.noreply.github.com>
2022-07-26 22:17:39 +0100

    update turf messages and check in gang not for license

--- arma/armacore/modules/sv_turf.lua
+++ arma/armacore/modules/sv_turf.lua
@@ -16,7 +16,7 @@ AddEventHandler('ARMA:TooFar', function(isnTurf)
 			TriggerClientEvent('ARMA:OutOfZone', source)
 			isPlayerInTurf[source] = nil
 
-			TriggerClientEvent('chatMessage', source, "^7[^1ARMA^7]:", {255, 255, 255}, "Turf capture sucessful at ^2" .."Turf capture failed at ^3" .. turf.nameofturf .. "alert")
+			TriggerClientEvent('chatMessage', -1, "^7[^1ARMA^7]:", {255, 255, 255}, "Turf capture failed at ^3" .. turf.nameofturf, "alert")
 			TriggerClientEvent("ARMA:MakeTurfTrue", -1, false)
 			completedTurf = false
 			TriggerClientEvent("ARMA:DontIt", -1, false)
@@ -41,7 +41,7 @@ AddEventHandler('ARMA:PlayerDied', function(isnTurf)
 		if(isPlayerInTurf[source])then
 			TriggerClientEvent('ARMA:PlayerDied', source)
 			isPlayerInTurf[source] = nil
-			TriggerClientEvent('chatMessage', source, "^7[^1ARMA^7]:", {255, 255, 255}, "Turf capture sucessful at ^2" .."Turf capture failed at ^3" .. turf.nameofturf .. "alert")
+			TriggerClientEvent('chatMessage', -1, "^7[^1ARMA^7]:", {255, 255, 255}, "Turf capture failed at ^3" .. turf.nameofturf, "alert")
 
 			TriggerClientEvent("ARMA:MakeTurfTrue", -1, false)
 			TriggerClientEvent("ARMA:DontIt", -1, false)
@@ -128,70 +128,78 @@ RegisterServerEvent('ARMA:TakeTurf')
 AddEventHandler('ARMA:TakeTurf', function(isnTurf)
 	local user_id = ARMA.getUserId(source)
 	local player = ARMA.getUserSource(user_id)
+	local isingang = false
 
-
-	if ARMA.hasGroup(user_id, 'Gang') then
-	  if turfs[isnTurf] then
-		  local turf = turfs[isnTurf]
-
-			if (os.time() - turf.lastisnTurfed) < 150 and turf.lastisnTurfed ~= 0 then
-				ARMAclient.notify(player,{"~r~This Turf has already been capped recently. Please wait another: " .. (150 - (os.time() - turf.lastisnTurfed)) .. " ~r~seconds."})
-
-				return
-			end
-
-			-- [Chat Message]
-			TriggerClientEvent('chatMessage', source, "^7[^1ARMA^7]:", {255, 255, 255}, "Turf capture started at ^3" .. turf.nameofturf .. " ^0by ^3 '" .. GetPlayerName(player), "alert")
+    exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
+		for K,V in pairs(gotGangs) do
+			local array = json.decode(V.gangmembers)
+			for I,L in pairs(array) do
+				if tostring(user_id) == I then
+					if turfs[isnTurf] then
+					local turf = turfs[isnTurf]
 			
-			completedTurf = false
-			TriggerClientEvent("ARMA:MakeTurfTrue", -1, true)
-
-		  	TriggerClientEvent('ARMA:TakenTurf', player, isnTurf)
-
-				
-			if turf.nameofturf == "Heroin" then
-				TriggerClientEvent("HeroinZone", -1)
-				TriggerClientEvent("GlobalTraderHeroin", -1)
-			end
-
-			if turf.nameofturf == "LSD" then
-				TriggerClientEvent("LSDZone", -1)
-				TriggerClientEvent("GlobalTraderLSD", -1)
-			end
-
-			if turf.nameofturf == "Weed" then
-				TriggerClientEvent("ARMA:DontItWeed", -1, false)
-				TriggerClientEvent("WeedZone", -1)
-			end
-
-			if turf.nameofturf == "Cocaine" then
-				TriggerClientEvent("CocaineZone", -1)
-				TriggerClientEvent("ARMA:DontItCocaine", -1, false)
-			end
-			  
-		  	turfs[isnTurf].lastisnTurfed = os.time()
-			  
-		  	isPlayerInTurf[player] = isnTurf
-		  	local savedSource = player
+						if (os.time() - turf.lastisnTurfed) < 150 and turf.lastisnTurfed ~= 0 then
+							ARMAclient.notify(player,{"~r~This Turf has already been capped recently. Please wait another: " .. (150 - (os.time() - turf.lastisnTurfed)) .. " ~r~seconds."})
 			
-		  	SetTimeout(300 * 1000, function()
-			  	if(isPlayerInTurf[savedSource]) then
-					
-				  	if(user_id) then
-
+							return
+						end
+			
+						-- [Chat Message]
+						TriggerClientEvent('chatMessage', -1, "^7[^1ARMA^7]:", {255, 255, 255}, "Turf capture started at ^3" .. turf.nameofturf, "alert")
 						
-						TriggerClientEvent('chatMessage', savedSource, "^7[^1ARMA^7]:", {255, 255, 255}, "Turf capture sucessful at ^2" .. turf.nameofturf .. "^0!" .. "'", "alert")
-
-		
-					 	TriggerClientEvent('ARMA:TurfComplete', savedSource, turf.reward, turf.nameofturf)
-						 TriggerClientEvent("ARMA:MakeTurfTrue", -1, false)
-						 completedTurf = true
-				  	end
-
-			  	end
-		  	end)		
-	  	end
-	else
-		ARMAclient.notify(source, {'~r~You need a Gang License to take a turf!'})
-	end
+						completedTurf = false
+						TriggerClientEvent("ARMA:MakeTurfTrue", -1, true)
+			
+						TriggerClientEvent('ARMA:TakenTurf', player, isnTurf)
+			
+							
+						if turf.nameofturf == "Heroin" then
+							TriggerClientEvent("HeroinZone", -1)
+							TriggerClientEvent("GlobalTraderHeroin", -1)
+						end
+			
+						if turf.nameofturf == "LSD" then
+							TriggerClientEvent("LSDZone", -1)
+							TriggerClientEvent("GlobalTraderLSD", -1)
+						end
+			
+						if turf.nameofturf == "Weed" then
+							TriggerClientEvent("ARMA:DontItWeed", -1, false)
+							TriggerClientEvent("WeedZone", -1)
+						end
+			
+						if turf.nameofturf == "Cocaine" then
+							TriggerClientEvent("CocaineZone", -1)
+							TriggerClientEvent("ARMA:DontItCocaine", -1, false)
+						end
+						
+						turfs[isnTurf].lastisnTurfed = os.time()
+						
+						isPlayerInTurf[player] = isnTurf
+						local savedSource = player
+						
+						SetTimeout(300 * 1000, function()
+							if(isPlayerInTurf[savedSource]) then
+								
+								if(user_id) then
+			
+									
+									TriggerClientEvent('chatMessage', savedSource, "^7[^1ARMA^7]:", {255, 255, 255}, "Turf capture sucessful at ^2" .. turf.nameofturf .. "^0!" .. "'", "alert")
+			
+					
+									TriggerClientEvent('ARMA:TurfComplete', savedSource, turf.reward, turf.nameofturf)
+									TriggerClientEvent("ARMA:MakeTurfTrue", -1, false)
+									completedTurf = true
+								end
+			
+							end
+						end)		
+					end
+					break
+				else
+					ARMAclient.notify(source, {'~r~You must be in a Gang to capture a turf!'})
+				end
+			end
+		end
+	end)
 end)
