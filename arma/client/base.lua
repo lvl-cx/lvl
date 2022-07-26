9fbd2de9eb86a7cecf038a2553d436a5454affd7
terrorist <34693611+terrorist@users.noreply.github.com>
2022-07-26 06:12:38 +0100

    add kb input to base

--- arma/client/base.lua
+++ arma/client/base.lua
@@ -376,6 +376,25 @@ function tARMA.LoadModel(modelName)
   end
 end
 
+function tARMA.KeyboardInput(TextEntry, ExampleText, MaxStringLenght)
+	AddTextEntry('FMMC_KEY_TIP1', TextEntry) 
+	DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "", ExampleText, "", "", "", MaxStringLenght)
+    blockinput = true 
+	while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do 
+		Citizen.Wait(0)
+	end
+	if UpdateOnscreenKeyboard() ~= 2 then
+		local result = GetOnscreenKeyboardResult() 
+		Citizen.Wait(1) 
+		blockinput = false 
+		return result 
+	else
+		Citizen.Wait(1)
+		blockinput = false 
+		return nil 
+	end
+end
+
 function tARMA.drawTxt(L, M, N, D, E, O, P, Q, R, S)
   SetTextFont(M)
   SetTextProportional(0)
