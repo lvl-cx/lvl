76462bd37e9e6f55b1d0a22c99d13a1d0f668922
terrorist <34693611+terrorist@users.noreply.github.com>
2022-07-26 06:43:30 +0100

    redo join perms to get levels in anim

--- arma/modules/player_state.lua
+++ arma/modules/player_state.lua
@@ -60,6 +60,45 @@ AddEventHandler("ARMA:playerSpawn", function(user_id, source, first_spawn)
                 end)
             TriggerClientEvent('FirstSpawn', player, true)
             ARMAclient.spawnAnim(source, {data.customization})
+            ARMAclient.setUserID(source, {user_id})
+
+            if ARMA.hasGroup(user_id, 'dev') then
+                ARMAclient.setDev(source, {})
+            end
+            if ARMA.hasGroup(user_id, 'cardev') then
+                ARMAclient.setCarDev(source, {})
+            end
+                
+            local adminlevel = 0
+            if ARMA.hasGroup(user_id,"dev") then
+                adminlevel = 12
+            elseif ARMA.hasGroup(user_id,"founder") then
+                adminlevel = 11
+            elseif ARMA.hasGroup(user_id,"operationsmanager") then
+                adminlevel = 10
+            elseif ARMA.hasGroup(user_id,"staffmanager") then    
+                adminlevel = 9
+            elseif ARMA.hasGroup(user_id,"commanager") then
+                adminlevel = 8
+            elseif ARMA.hasGroup(user_id,"headadmin") then
+                adminlevel = 7
+            elseif ARMA.hasGroup(user_id,"senioradmin") then
+                adminlevel = 6
+            elseif ARMA.hasGroup(user_id,"administrator") then
+                adminlevel = 5
+            elseif ARMA.hasGroup(user_id,"srmoderator") then
+                adminlevel = 4
+            elseif ARMA.hasGroup(user_id,"moderator") then
+                adminlevel = 3
+            elseif ARMA.hasGroup(user_id,"supportteam") then
+                adminlevel = 2
+            elseif ARMA.hasGroup(user_id,"trialstaff") then
+                adminlevel = 1
+            end
+            ARMAclient.setStaffLevel(source, {adminlevel})
+
+            TriggerClientEvent('ARMA:sendGarageSettings', source)
+            TriggerClientEvent('ARMA:sendSettings', source)
         else
             if data.weapons ~= nil then -- load saved weapons
                 ARMAclient.giveWeapons(source, {data.weapons, true})
