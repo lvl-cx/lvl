d3e77466c52d497791bdd7018edb378f800eadc0
terrorist <34693611+terrorist@users.noreply.github.com>
2022-07-26 05:22:50 +0100

    never used

--- arma/client/base.lua
+++ arma/client/base.lua
@@ -85,20 +85,6 @@ function tARMA.getNearestPlayers(radius)
   local pid = PlayerId()
   local px,py,pz = tARMA.getPosition()
 
-  --[[
-  for i=0,GetNumberOfPlayers()-1 do
-    if i ~= pid then
-      local oped = GetPlayerPed(i)
-
-      local x,y,z = table.unpack(GetEntityCoords(oped,true))
-      local distance = GetDistanceBetweenCoords(x,y,z,px,py,pz,true)
-      if distance <= radius then
-        r[GetPlayerServerId(i)] = distance
-      end
-    end
-  end
-  --]]
-
   for k,v in pairs(players) do
     local player = GetPlayerFromServerId(k)
 
