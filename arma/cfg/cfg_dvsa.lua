f01e9f1e852faa183f91d7467375f57055429cf1
terrorist <34693611+terrorist@users.noreply.github.com>
2022-08-06 18:42:12 +0100

    temp dvsa test model

--- arma/cfg/cfg_dvsa.lua
+++ arma/cfg/cfg_dvsa.lua
@@ -21,7 +21,7 @@ cfg.test = {
         },
     },
     price = 20000,
-    modelName = `police2`,
+    modelName = `londontaxi`,
     examinerModel = `s_m_y_airworker`,
     routes = {
         { -- test route 1 (bay park manoeuvre)
