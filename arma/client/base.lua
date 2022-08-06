1f51da3911426b143011561213b8e6cb34877017
terrorist <34693611+terrorist@users.noreply.github.com>
2022-08-06 18:42:19 +0100

    reformat base funcs

--- arma/client/base.lua
+++ arma/client/base.lua
@@ -157,55 +157,52 @@ function tARMA.doesAreaExist(l)
   end
   return false
 end
-Citizen.CreateThread(
-    function()
-        while true do
-            local M = tARMA.getPlayerCoords()
-            for a0, a1 in pairs(R) do
-                local a2 = #(a1.position - M)
-                local a3 = a2 <= a1.radius and math.abs(M.z - a1.position.z) <= a1.height
-                a1.distance = a2
-                if a1.player_in and not a3 then
-                    if a1.leaveArea then
-                        if a1.metaData == nil then
-                            a1.metaData = {}
-                        end
-                        a1.leaveArea(a1.metaData)
-                    else
-                        vRPserver.leaveArea({a0})
-                    end
-                elseif not a1.player_in and a3 then
-                    if a1.enterArea then
-                        if a1.metaData == nil then
-                            a1.metaData = {}
-                        end
-                        a1.enterArea(a1.metaData)
-                    else
-                        vRPserver.enterArea({a0})
-                    end
-                end
-                a1.player_in = a3
-            end
-            Wait(0)
-        end
-    end
-)
-Citizen.CreateThread(
-    function()
-        while true do
-            for a0, a1 in pairs(R) do
-                if a1.player_in and a1.onTickArea then
+Citizen.CreateThread(function()
+    while true do
+        local M = tARMA.getPlayerCoords()
+        for a0, a1 in pairs(R) do
+            local a2 = #(a1.position - M)
+            local a3 = a2 <= a1.radius and math.abs(M.z - a1.position.z) <= a1.height
+            a1.distance = a2
+            if a1.player_in and not a3 then
+                if a1.leaveArea then
                     if a1.metaData == nil then
                         a1.metaData = {}
                     end
-                    a1.metaData.distance = a1.distance
-                    a1.onTickArea(a1.metaData)
+                    a1.leaveArea(a1.metaData)
+                else
+                    vRPserver.leaveArea({a0})
+                end
+            elseif not a1.player_in and a3 then
+                if a1.enterArea then
+                    if a1.metaData == nil then
+                        a1.metaData = {}
+                    end
+                    a1.enterArea(a1.metaData)
+                else
+                    vRPserver.enterArea({a0})
                 end
             end
-            Wait(0)
+            a1.player_in = a3
         end
+        Wait(0)
     end
-)
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        for a0, a1 in pairs(R) do
+            if a1.player_in and a1.onTickArea then
+                if a1.metaData == nil then
+                    a1.metaData = {}
+                end
+                a1.metaData.distance = a1.distance
+                a1.onTickArea(a1.metaData)
+            end
+        end
+        Wait(0)
+    end
+end)
 
 local a=0
 local b=0
@@ -236,6 +233,21 @@ end
 
 function tARMA.getPlayerCoords()
     return d 
+end
+
+function tARMA.spawnVehicle(W,v,w,H,X,Y,Z,_)
+  local a0=tARMA.LoadModel(W)
+  local a1=CreateVehicle(a0,v,w,H,X,Z,_)
+  SetEntityAsMissionEntity(a1)
+  DecorSetInt(a1,"BWACVeh",955)
+  SetModelAsNoLongerNeeded(a0)
+  if Y then 
+      TaskWarpPedIntoVehicle(PlayerPedId(),a1,-1)
+  end
+  if GetResourceState("BWFuel")=="started"then 
+      exports['BWFuel']:SetFuel(a1,100)
+  end
+  return a1 
 end
 
 local a2={}
