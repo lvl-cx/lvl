7af63060a7efc023e82ce30fb25cb18f683fe6f4
terrorist <34693611+terrorist@users.noreply.github.com>
2022-08-10 04:02:14 +0100

    change community pot input to prompt

--- arma/client/cl_communitypot.lua
+++ arma/client/cl_communitypot.lua
@@ -1,21 +1,6 @@
 local a="0"
 RMenu.Add("communitypot","mainmenu",RageUI.CreateMenu("Community Pot","",tARMA.getRageUIMenuWidth(),tARMA.getRageUIMenuHeight(),nil,nil))
 RMenu:Get("communitypot","mainmenu"):SetSubtitle("~b~Community Pot")
-local function b()
-    AddTextEntry('FMMC_MPM_NA',"Enter amount")
-    DisplayOnscreenKeyboard(1,"FMMC_MPM_NA","Enter amount","","","","",30)
-    while UpdateOnscreenKeyboard()==0 do 
-        DisableAllControlActions(0)
-        Wait(0)
-    end;
-    if GetOnscreenKeyboardResult()then 
-        local c=GetOnscreenKeyboardResult()
-        if c then 
-            return c 
-        end 
-    end;
-    return false 
-end;
 RegisterNetEvent("ARMA:gotCommunityPotAmount",function(d)
     a=tostring(d)
 end)
@@ -26,22 +11,24 @@ RageUI.CreateWhile(1.0, true, function()
             RageUI.Separator("Community Pot Balance: ~g~£"..getMoneyStringFormatted(a))
             RageUI.ButtonWithStyle("Deposit","",{RightLabel="→→→"},true,function(e,f,g)
                 if g then 
-                    local d=b()
-                    if tonumber(d)then 
-                        TriggerServerEvent("ARMA:tryDepositCommunityPot",d)
-                    else 
-                        tARMA.notify("~r~Invalid amount.")
-                    end 
+                    tARMA.clientPrompt("Enter Amount:","",function(d)
+                        if tonumber(d)then 
+                            TriggerServerEvent("ARMA:tryDepositCommunityPot",d)
+                        else 
+                            tARMA.notify("~r~Invalid amount.")
+                        end 
+                    end)
                 end 
             end)
             RageUI.ButtonWithStyle("Withdraw","",{RightLabel="→→→"},true,function(e,f,g)
                 if g then 
-                    local d=b()
-                    if tonumber(d)then 
-                        TriggerServerEvent("ARMA:tryWithdrawCommunityPot",d)
-                    else 
-                        tARMA.notify("~r~Invalid amount.")
-                    end 
+                    tARMA.clientPrompt("Enter Amount:","",function(d)
+                        if tonumber(d)then 
+                            TriggerServerEvent("ARMA:tryWithdrawCommunityPot",d)
+                        else 
+                            tARMA.notify("~r~Invalid amount.")
+                        end 
+                    end)
                 end 
             end)
         end)
