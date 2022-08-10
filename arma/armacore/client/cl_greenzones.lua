3665200e8de6a06f15f3dac84ff45a163ab1a680
terrorist <34693611+terrorist@users.noreply.github.com>
2022-08-10 23:43:55 +0100

    redo greenzones

--- arma/armacore/client/cl_greenzones.lua
+++ arma/armacore/client/cl_greenzones.lua
@@ -1,184 +1,233 @@
-local blips = {
-	{title="[Sandy Hospital]", colour=2, id=1, pos=vector3(1832.4654541016,3678.6604003906,42.009292602539),dist=35,nonRP=false,setBit=false}, -- [Sandy Hospital]
-    {title="[St Thomas]", colour=2, id=1, pos=vector3(333.91488647461,-597.16156005859,29.292747497559),dist=40,nonRP=false,setBit=false}, -- [St Thomas]
-    {title="[Paleto Hospital]", colour=2, id=1, pos=vector3(-254.16650390625,6324.3740234375,39.203189849854),dist=30,nonRP=false,setBit=false}, -- [Paleto Hospital]
-	{title="[License Centre]", colour=2, id=1, pos=vector3(-927.42938232422,-2041.2468261719,13.946600914001),dist=60,nonRP=false,setBit=false}, -- [License Centre]
-	{title="[VIP Island]", colour=2, id=1, pos=vector3(-2170.4770507812,5179.0668945312,15.63862323761),dist=100,nonRP=false,setBit=false}, -- [VIP Island]
-	{title="[Legion]", colour=2, id=1, pos=vector3(170.51364135742,-1021.3690795898,28.816247940063),dist=50,nonRP=false,setBit=false}, -- [Legion]
-	{title="[Admin Island]", colour=2, id=1, pos=vector3(3492.4116210938,2579.2509765625,13.129757881165),dist=200,nonRP=false,setBit=false}, -- [Admin Island]
-	{title="[Casino]", colour=2, id=1, pos=vector3(1134.6573486328,251.09861755371,-51.035732269287),dist=100,nonRP=false,setBit=false}, -- [Casino]
+local a=module("arma-vehicles","cfg_vehiclemaxspeeds")
+isInGreenzone=false
+local b=false
+local c=false
+local d=false
+local e=false
+local f=0
+local g=false
+local h=false
+local i=false
+local j={
+    {colour=2,id=1,pos=vector3(333.91488647461,-597.16156005859,29.292747497559),dist=40,nonRP=false,setBit=false},
+    {colour=2,id=1,pos=vector3(150.11642456055,-1039.6853027344,29.367973327637),dist=25,nonRP=false,setBit=false},
+    {colour=2,id=1,pos=vector3(-110.09871673584,6464.6030273438,31.62672996521),dist=20,nonRP=false,setBit=false},
+    {colour=2,id=1,pos=vector3(-1079.5734863281,-843.14739990234,4.8841333389282),dist=45,nonRP=false,setBit=false},
+    {colour=2,id=1,pos=vector3(-2181.7966308594,5189.8286132813,17.64377784729),dist=150,nonRP=true,setBit=false},
+    {colour=2,id=1,pos=vector3(-540.54748535156,-216.42681884766,37.64966583252),dist=50,nonRP=false,setBit=false},
+    {colour=2,id=1,pos=vector3(246.30143737793,-782.50170898438,30.573167800903),dist=40,nonRP=false,setBit=false},
+    {colour=2,id=1,pos=vector3(3463.3479003906,2589.4458007813,17.442699432373),dist=40,nonRP=true,setBit=false},
+    {colour=2,id=1,pos=vector3(1133.0970458984,250.78565979004,-51.035778045654),dist=100,nonRP=false,setBit=false,interior=true},
+    {colour=2,id=1,pos=vector3(13.929432868958,6711.216796875,-105.85443878174),dist=100,nonRP=false,setBit=false,interior=true},
+    {colour=2,id=1,pos=vector3(-335.19680786133,-699.10406494141,33.036075592041),dist=30,nonRP=false,setBit=false},
+    {colour=2,id=1,pos=vector3(-1671.5692138672,-912.63940429688,8.2297477722168),dist=50,nonRP=false,setBit=false},
+    {colour=2,id=1,pos=vector3(337.64172363281,-1393.6368408203,32.509204864502),dist=50,nonRP=false,setBit=false},
+    {colour=2,id=1,pos=vector3(-2335.1215820313,266.88153076172,169.60194396973),dist=50,nonRP=false,setBit=false}
 }
-
-local function ShowInfo(text)
-	exports['arma-notify']:DoHudText('success', text)
+local k={
+    {vector3(333.91488647461,-597.16156005859,29.292747497559),40.0,2,180},
+    {vector3(150.11642456055,-1039.6853027344,29.367973327637),25.0,2,180},
+    {vector3(-110.09871673584,6464.6030273438,31.62672996521),20.0,2,180},
+    {vector3(-1079.5734863281,-843.14739990234,4.8841333389282),45.0,2,180},
+    {vector3(-2181.7966308594,5189.8286132813,17.64377784729),150.0,2,180},
+    {vector3(-540.54748535156,-216.42681884766,37.64966583252),50.0,2,180},
+    {vector3(246.30143737793,-782.50170898438,30.573167800903),40.0,2,180},
+    {vector3(-335.19680786133,-699.10406494141,33.036075592041),30.0,2,180},
+    {vector3(-1671.5692138672,-912.63940429688,8.2297477722168),50.0,2,180},
+    {vector3(1468.5318603516,6328.529296875,18.894895553589),100.0,1,180},
+    {vector3(4982.5634765625,-5175.1079101562,2.4887988567352),120.0,1,180},
+    {vector3(5115.7465820312,-4623.2915039062,2.642692565918),85.0,1,180},
+    {vector3(337.64172363281,-1393.6368408203,32.509204864502),50.0,2,180},
+    {vector3(-2335.1215820313,266.88153076172,169.60194396973),50.0,2,180}
+}
+local l=Citizen.CreateThread
+local m=Citizen.Wait
+local SetEntityInvincible=SetEntityInvincible
+local SetPlayerInvincible=SetPlayerInvincible
+local ClearPedBloodDamage=ClearPedBloodDamage
+local ResetPedVisibleDamage=ResetPedVisibleDamage
+local ClearPedLastWeaponDamage=ClearPedLastWeaponDamage
+local SetEntityProofs=SetEntityProofs
+local SetEntityCanBeDamaged=SetEntityCanBeDamaged
+local NetworkSetFriendlyFireOption=NetworkSetFriendlyFireOption
+local GetEntityCoords=GetEntityCoords
+local SetEntityNoCollisionEntity=SetEntityNoCollisionEntity
+local SetPedCanRagdoll=SetPedCanRagdoll
+local SetPedCanRagdollFromPlayerImpact=SetPedCanRagdollFromPlayerImpact
+local SetEntityMaxSpeed=SetEntityMaxSpeed
+local GetEntityModel=GetEntityModel
+local SetEntityCollision=SetEntityCollision
+local DisableControlAction=DisableControlAction
+local GetVehiclePedIsIn=GetVehiclePedIsIn
+function tARMA.areGreenzonesDisabled()
+    return i 
 end
-     
-local PaletoHospital = AddBlipForRadius(-254.16650390625,6324.3740234375,39.203189849854, 30.0)
-SetBlipColour(PaletoHospital, 2)
-SetBlipAlpha(PaletoHospital, 150)
-
-local CityHospital = AddBlipForRadius(333.91488647461,-597.16156005859,29.292747497559, 40.0)
-SetBlipColour(CityHospital, 2)
-SetBlipAlpha(CityHospital, 150)
-
-local SandyHospital = AddBlipForRadius(1832.4654541016,3678.6604003906,42.009292602539, 35.0)
-SetBlipColour(SandyHospital, 2)
-SetBlipAlpha(SandyHospital, 150)
-
-local LicenseCentre = AddBlipForRadius(-927.42938232422,-2041.2468261719,13.946600914001, 60.0)
-SetBlipColour(LicenseCentre, 2)
-SetBlipAlpha(LicenseCentre, 150)
-
-local vipisland = AddBlipForRadius(-2170.4770507812,5179.0668945312,15.63862323761, 100.0)
-SetBlipColour(vipisland, 2)
-SetBlipAlpha(vipisland, 170)
-
-local legion = AddBlipForRadius(170.51364135742,-1021.3690795898,28.816247940063, 50.0)
-SetBlipColour(legion, 2)
-SetBlipAlpha(legion, 170)
-
-InsideSafeZone = false
-setDrawGreenZoneUI = false
-setDrawNonRpZoneUI = false
-Citizen.CreateThread(function()
-	while true do
-		for index,info in pairs(blips) do
-			local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
-			safeZoneDist = #(plyCoords-info.pos) 
-			while safeZoneDist < info.dist do
-				local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
-				safeZoneDist = #(plyCoords-info.pos)
-				
-				if info.nonRP then
-					setDrawNonRpZoneUI = true
-				else
-					if not info.setBit then
-						setDrawGreenZoneUI = true
-						showEnterGreenzone = true
-						showExitGreenzone = false
-						greenzoneTimer = 1
-						info.setBit = true
-					end
-					if info.interior then 
-						setDrawGreenInterior = true
-					end
-				end
-				Wait(100)
-			end
-			if info.setBit then
-				showEnterGreenzone = false
-				showExitGreenzone = true
-				greenzoneTimer = 1
-				info.setBit = false
-			end
-			setDrawNonRpZoneUI = false
-			setDrawGreenZoneUI = false
-			showEnterGreenzone = false
-			setDrawGreenInterior = false
-			SetEntityInvincible(GetPlayerPed(-1), false)
-			SetPlayerInvincible(PlayerId(), false)
-			ClearPedBloodDamage(GetPlayerPed(-1))
-			ResetPedVisibleDamage(GetPlayerPed(-1))
-			ClearPedLastWeaponDamage(GetPlayerPed(-1))
-			SetEntityProofs(GetPlayerPed(-1), false, false, false, false, false, false, false, false)
-			SetEntityCanBeDamaged(GetPlayerPed(-1), true)
-			NetworkSetFriendlyFireOption(true)
-		end
-		Wait(100)
-	end
+function tARMA.setGreenzonesDisabled(n)
+    i=n 
+end
+l(function()
+    for o,p in pairs(k)do 
+        local q=AddBlipForRadius(p[1].x,p[1].y,p[1].z,p[2])
+        SetBlipColour(q,p[3])
+        SetBlipAlpha(q,p[4])
+    end 
 end)
-
-Citizen.CreateThread(function()
-	while true do
-		local plyCoords = GetEntityCoords(GetPlayerPed(-1), false)
-		cityZoneDist = #(plyCoords-vector3(171.07974243164,-1024.8974609375,29.3747520446784))
-		if cityZoneDist < 500 then
-			inCityZone = true 
-		else 
-			inCityZone = false 
-		end
-		Wait(1000)
-	end
+l(function()
+    while true do 
+        local r=tARMA.getPlayerPed()
+        local s=tARMA.getPlayerCoords()
+        for t,u in pairs(j)do 
+            local v=#(s-u.pos)while v<u.dist do 
+                local s=tARMA.getPlayerCoords()
+                v=#(s-u.pos)
+                if u.nonRP then 
+                    c=true 
+                else 
+                    if not u.setBit then 
+                        b=true
+                        d=true
+                        e=false
+                        f=5
+                        u.setBit=true 
+                    end
+                    if u.interior then 
+                        setDrawGreenInterior=true 
+                    end 
+                end
+                m(100)
+            end
+            if u.setBit then 
+                d=false
+                e=true
+                f=5
+                u.setBit=false 
+            end
+            c=false
+            b=false
+            d=false
+            setDrawGreenInterior=false
+            SetEntityInvincible(r,false)
+            SetPlayerInvincible(tARMA.getPlayerId(),false)
+            ClearPedBloodDamage(r)
+            ResetPedVisibleDamage(r)
+            ClearPedLastWeaponDamage(r)
+            SetEntityProofs(r,false,false,false,false,false,false,false,false)
+            SetEntityCanBeDamaged(r,true)
+            Citizen.InvokeNative(0x5FFE9B4144F9712F,false)
+            SetNetworkVehicleAsGhost(tARMA.getPlayerVehicle(),false)
+        end
+        m(250)
+    end 
 end)
-
-
 Citizen.CreateThread(function()
-	while true do
-		if setDrawGreenZoneUI then
-			DisableControlAction(2, 37, true)
-			DisablePlayerFiring(GetPlayerPed(-1),true)
-			DisableControlAction(0, 106, true)
-			DisableControlAction(0, 45, true)
-			DisableControlAction(0, 24, true)
-			DisableControlAction(0, 263, true)
-			DisableControlAction(0, 170, true)
-		end
-		if setDrawGreenInterior then 
-			DisableControlAction(0, 106, true)
-			DisableControlAction(0, 45, true)
-			DisableControlAction(0, 24, true)
-			DisableControlAction(0, 263, true)
-			DisableControlAction(0, 170, true)
-			DisableControlAction(0, 22, true)
-		end
-		Wait(0)
+	local y=function(z)
+		inCityZone=true 
 	end
+	local A=function(z)
+		inCityZone=false 
+	end
+	local B=function(z)
+	end
+	tARMA.createArea("cityzone",vector3(-225.30703735352,-916.74755859375,31.216938018799),750.0,100,y,A,B,{})
 end)
-
-
-
-Citizen.CreateThread(function()
-	while true do
-		if setDrawGreenZoneUI or setDrawNonRpZoneUI then
-			SetEntityMaxSpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false),13.0)
-			SetEntityInvincible(GetPlayerPed(-1), true)
-			SetCurrentPedWeapon(GetPlayerPed(-1),GetHashKey("WEAPON_UNARMED"),true)
-			SetPlayerInvincible(PlayerId(), true)
-			ClearPedBloodDamage(GetPlayerPed(-1))
-			ResetPedVisibleDamage(GetPlayerPed(-1))
-			ClearPedLastWeaponDamage(GetPlayerPed(-1))
-			SetEntityProofs(GetPlayerPed(-1), true, true, true, true, true, true, true, true)
-			SetEntityCanBeDamaged(GetPlayerPed(-1), false)
-		else
-			if GetVehiclePedIsIn(PlayerPedId(), false) ~= 0 then
-				if not inCityZone then
-					if GetVehicleClass(GetVehiclePedIsIn(GetPlayerPed(-1),true)) ~= 13 then
-						SetEntityMaxSpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false),111.5)
-					else
-						SetEntityMaxSpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false),11001.5)
-					end
-				else 
-					if GetVehicleClass(GetVehiclePedIsIn(GetPlayerPed(-1),true)) ~= 13 then
-						SetEntityMaxSpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false),44.6)
-					else
-						SetEntityMaxSpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false),11001.5)
-					end
-				end
-			end
-		end
-		Wait(0)
-	end
-end)
-
-showEnterGreenzone = false
-showExitGreenzone = false
-greenzoneTimer = 0
-
-Citizen.CreateThread(function()
-	while true do
-		if showEnterGreenzone and greenzoneTimer > 0 then
-			ShowInfo("You have entered the greenzone")
-		end
-		if showExitGreenzone and greenzoneTimer > 0 then
-			ShowInfo("You have left a greenzone")
-		end
-		Wait(0)
-	end
-end)
-
-Citizen.CreateThread(function()
-	while true do
-		greenzoneTimer = greenzoneTimer - 1
-		Wait(0)
-	end
+l(function()
+    while true do 
+        local r=PlayerPedId()
+        local C=GetVehiclePedIsIn(r,false)
+        SetVehicleAutoRepairDisabled(C,true)
+        local Sped = tARMA.GetRPZoneInfo()
+        if not tARMA.areGreenzonesDisabled()then 
+            isInGreenzone=b or c
+            local D=GetActivePlayers()
+            if b or c then 
+                local playerId=tARMA.getPlayerId()
+                if Sped~=nil then
+                    SetEntityMaxSpeed(C,Sped.vehicles.speed/2.236936)
+                else
+                    SetEntityMaxSpeed(C,a.maxSpeeds["50"])
+                end
+                Citizen.InvokeNative(0x5FFE9B4144F9712F,true)
+                SetNetworkVehicleAsGhost(C,true)
+                SetEntityAlpha(tARMA.getPlayerVehicle(),255)
+                SetEntityAlpha(r,255)
+                for E,F in pairs(D)do 
+                    local G=GetPlayerPed(F)
+                    local H=GetVehiclePedIsIn(G,true)
+                    SetEntityAlpha(G,255)
+                    SetEntityAlpha(H,255)
+                end
+                SetEntityInvincible(r,true)
+                SetPlayerInvincible(playerId,true)
+                ClearPedBloodDamage(r)
+                if usingDelgun then 
+                    tARMA.setWeapon(r,`WEAPON_SPEEDGUNBW`,true)
+                else 
+                    tARMA.setWeapon(r,`WEAPON_UNARMED`,true)
+                end
+                ResetPedVisibleDamage(r)
+                ClearPedLastWeaponDamage(r)
+                SetEntityProofs(r,true,true,true,true,true,true,true,true)
+                SetEntityCanBeDamaged(r,false)
+                SetPedCanRagdoll(r,false)
+                SetPedCanRagdollFromPlayerImpact(r,false)
+            else 
+                SetPedCanRagdoll(r,true)
+                SetPedCanRagdollFromPlayerImpact(r,true)
+                if C~=0 then 
+                    SetEntityCollision(C,true,true)
+                    local I=GetEntityModel(C)
+                    if Sped~=nil then
+                        SetEntityMaxSpeed(C,Sped.vehicles.speed/2.236936)
+                    else
+                        if not inCityZone then 
+                            if a.vehicleMaxSpeeds[I]~=nil then 
+                                SetEntityMaxSpeed(C,a.maxSpeeds[a.vehicleMaxSpeeds[I]])
+                            else 
+                                SetEntityMaxSpeed(C,a.maxSpeeds["250"])
+                            end 
+                        else 
+                            SetEntityMaxSpeed(C,a.maxSpeeds["100"])
+                        end 
+                    end
+                end 
+            end
+            if d and g==false then 
+				exports['arma-notify']:DoHudText('success', "You have entered the greenzone")
+                g=true
+                h=false 
+            end
+            if e and h==false then 
+				exports['arma-notify']:DoHudText('error', "You have left the greenzone")
+                h=true
+                g=false 
+            end
+            if b then 
+                DisableControlAction(2,37,true)
+                DisablePlayerFiring(playerId,true)
+                DisableControlAction(0,106,true)
+                DisableControlAction(0,45,true)
+                DisableControlAction(0,24,true)
+                DisableControlAction(0,263,true)
+                DisableControlAction(0,140,true)
+            end
+            if c then 
+                drawNativeText("You have entered a non-RP greenzone, you may talk out of character here")
+                DisableControlAction(2,37,true)
+                DisablePlayerFiring(playerId,true)
+                DisableControlAction(0,45,true)
+                DisableControlAction(0,24,true)
+                DisableControlAction(0,263,true)
+                DisableControlAction(0,140,true)
+            end
+            if setDrawGreenInterior then 
+                DisableControlAction(0,106,true)
+                DisableControlAction(0,45,true)
+                DisableControlAction(0,24,true)
+                DisableControlAction(0,263,true)
+                DisableControlAction(0,140,true)
+                DisableControlAction(0,22,true)
+            end 
+        end
+        m(0)
+    end 
 end)
