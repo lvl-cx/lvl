f0db4e2189ffe33c4196b0782b1c178a2d42f115
terrorist <34693611+terrorist@users.noreply.github.com>
2022-08-10 02:38:23 +0100

    sort weed blips

--- arma/armacore/client/cl_mapblips.lua
+++ arma/armacore/client/cl_mapblips.lua
@@ -16,3 +16,7 @@ end)
 local cocaine  = AddBlipForRadius(143.98948669434,-1299.4553222656,29.062242507935, 50.0)
 SetBlipColour(cocaine, 0)
 SetBlipAlpha(cocaine, 120)
+
+local weed = AddBlipForRadius(103.47916412354,-1937.1091308594,19.803705215454, 50.0) -- [Weed]
+SetBlipColour(weed, 2)
+SetBlipAlpha(weed, 100)
--- arma/armacore/client/cl_zoneblips.lua
+++
@@ -1,7 +0,0 @@
-local cocaine = AddBlipForRadius(132.79766845703,-1305.4119873047,29.179517745972, 60.0) -- [Cocaine]
-SetBlipColour(cocaine, 0)
-SetBlipAlpha(cocaine, 100)
-
-local weed = AddBlipForRadius(95.212280273438,-1930.4608154297,20.803718566895, 60.0) -- [Weed]
-SetBlipColour(weed, 2)
-SetBlipAlpha(weed, 100)
