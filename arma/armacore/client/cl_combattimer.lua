87b10bd284f547a140e325ef34f5c07253379e04
terrorist <34693611+terrorist@users.noreply.github.com>
2022-08-10 23:44:12 +0100

    redo rz timer

--- arma/armacore/client/cl_combattimer.lua
+++ arma/armacore/client/cl_combattimer.lua
@@ -1,82 +1,106 @@
-local inRedZone = false
-local timeLeft = 30
-local currentRedZone = nil
-local scaleform = RequestScaleformMovie("MP_BIG_MESSAGE_FREEMODE")
-
-zones = {
-    {name = "LSD", coords = vector3(2539.0031738281,-395.42044067383,92.74683380127), blipWidth = 140.0, blipColour = 1},
-    {name = "Large Arms", coords = vector3(-1113.1085205078,4922.0400390625,217.8408203125), blipWidth = 110.0, blipColour = 1},
-    {name = "Heroin", coords = vector3(3529.4702148438,3726.9321289062,36.473175048828), blipWidth = 220.0, blipColour = 1},
-    {name = "Rebel", coords = vector3(1457.4250488281,6308.064453125,63.868026733398), blipWidth = 130.0, blipColour = 1},
+globalInRedzone=false
+local a=false
+local b=0
+local c = {
+    ["Rebel"] = {vector3(1468.5318603516, 6328.529296875, 18.894895553589), 100.0},
+    ["Heroin"] = {vector3(3545.048828125, 3724.0776367188, 36.64262008667), 170.0},
+    ["LargeArms"] = {vector3(-1118.4926757813, 4926.1889648438, 218.35691833496), 170.0},
+    ["LargeArmsCayo"] = {vector3(5115.7465820312, -4623.2915039062, 2.642692565918), 85.0},
+    ["RebelCayo"] = {vector3(4982.5634765625, -5175.1079101562, 2.4887988567352), 120.0},
+    ["LSDNorth"] = {vector3(1317.0300292969, 4309.8359375, 38.005485534668), 90.0},
+    ["LSDSouth"] = {vector3(2539.0964355469, -376.51586914063, 92.986785888672), 120.0}
 }
 
-Citizen.CreateThread(function()
-    for k, v in pairs(zones) do
-        local blip = AddBlipForRadius(v.coords, v.blipWidth)
-        SetBlipHighDetail(blip, true)
-        SetBlipColour(blip, v.blipColour)
-        SetBlipAlpha(blip, 128)
-    end
+local function d(e,f)
+    return#(vector3(e.x,e.y,0.0)-vector3(f.x,f.y,0.0))
+end
 
-    while true do
-        Citizen.Wait(0)
-        for k, v in pairs(zones) do
-            
-            if isInArea(v.coords, v.blipWidth) and not inRedZone then
-                timeLeft = 30
-                inRedZone = true
-                currentRedZone = k
-            end
+function tARMA.setRedzoneTimerDisabled(g)
+    a=g 
+end
 
-            if isInArea(v.coords, v.blipWidth) == false and inRedZone and currentRedZone == k then
-                if timeLeft == 0 then
-                    inRedZone = false
-                else
-                    TaskGoStraightToCoord(PlayerPedId(), v.coords, 2.0, 100.0, 307.0, 1.0)
-                    if HasScaleformMovieLoaded(scaleform) then
-                        PushScaleformMovieFunction(scaleform, "SHOW_SHARD_WASTED_MP_MESSAGE")
-                        BeginTextComponent("STRING")
-                        AddTextComponentString("~r~GET BACK IN THE REDZONE!")
-                        EndTextComponent()
-                        PopScaleformMovieFunctionVoid()
-                        DrawScaleformMovieFullscreen(scaleform, 255, 255, 255, 255)
-                    end
-                    SetTimeout(1000, function()
-                        ClearPedTasks(PlayerPedId())
-                    end)
-                end
-            end
+function tARMA.isPlayerInRedZone()
+    return globalInRedzone 
+end
+function tARMA.getPlayerCombatTimer()
+    return b 
+end
+
+RegisterNetEvent("ARMA:SendLSDCoords")
+AddEventHandler("ARMA:SendLSDCoords", function(A)
+    c["LSD"] = {A[1],A[4]}
+    while true do 
+        if not a then 
+            local h=GetEntityCoords(tARMA.getPlayerPed())
+            globalInRedzone=false
+            for i,j in pairs(c)do 
+                local k=j[1]
+                local l=j[2]
+                if#(h-k)<=l then 
+                    if not globalInRedzone then
+                        TriggerServerEvent('ARMA:UpdateInRZ',true)
+                    end 
+                    globalInRedzone=true
+                    local h=GetEntityCoords(tARMA.getPlayerPed())
+                    b=30
+                    local m
+                    local n=false
+                    while not n do 
+                        h=GetEntityCoords(tARMA.getPlayerPed())
+                        while d(h,k)<=l and not q() do 
+                            h=GetEntityCoords(tARMA.getPlayerPed())
+                            m=h
+                            if IsPedShooting(tARMA.getPlayerPed())and GetSelectedPedWeapon(tARMA.getPlayerPed())~=`WEAPON_UNARMED`then 
+                                b=30
+                            end
+                            if b==0 then 
+                                DrawAdvancedText(0.931,0.914,0.005,0.0028,0.49,"Combat Timer ended, you may leave.",255,51,51,255,7,0)
+                            else 
+                                DrawAdvancedText(0.931,0.914,0.005,0.0028,0.49,"Combat Timer: "..b.." seconds",255,51,51,255,7,0)
+                            end
+                            Wait(0)
+                        end
+                        if b==0 then 
+                            n=true 
+                        else 
+                            if tARMA.isstaffedOn() or inSpectatorAdminMode then
+                                b = 0
+                                n=true
+                            else
+                                local o=j[1]-GetEntityCoords(tARMA.getPlayerPed())
+                                m=m+o*0.01
+                                if GetVehiclePedIsIn(tARMA.getPlayerPed(),false)==0 then 
+                                    TaskGoStraightToCoord(tARMA.getPlayerPed(),m.x,m.y,m.z,8.0,1000,GetEntityHeading(tARMA.getPlayerPed()),0.0)
+                                    local p=GetSoundId()
+                                    PlaySoundFrontend(p,"End_Zone_Flash","DLC_BTL_RB_Remix_Sounds",true)
+                                    ReleaseSoundId(p)
+                                    tARMA.announceMpBigMsg("~r~WARNING","Get back in the redzone!",2000)
+                                else 
+                                    SetEntityCoords(tARMA.getPlayerPed(),m.x,m.y,m.z)
+                                end
+                                SetTimeout(1000,function()
+                                    ClearPedTasks(tARMA.getPlayerPed())
+                                end)
+                            end
+                        end
+                        Wait(0)
+                    end 
+                end 
+            end 
         end
-
-        if inRedZone then
-            if IsPedShooting(PlayerPedId()) then
-                timeLeft = 30
-            end
-
-            if timeLeft ~= 0 then
-                DrawAdvancedText(0.931, 0.914, 0.005, 0.0028, 0.49, timeLeft .. " seconds remaining", 217, 217, 217, 255, 7, 0)
-            elseif timeLeft == 0 then
-                DrawAdvancedText(0.931, 0.914, 0.005, 0.0028, 0.49, "combat timer ended", 217, 217, 217, 255, 7, 0)
-            end
-        end
-    end
+        Wait(500)
+    end 
 end)
 
 Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(1000)
-        if inRedZone then
-            if timeLeft > 0 then
-                timeLeft = timeLeft - 1
-            end
+    while true do 
+        if b>0 then 
+            b=b-1 
         end
-    end
+        Wait(1000)
+    end 
 end)
 
-function isInArea(v, dis) 
-    if #(GetEntityCoords(PlayerPedId()) - v) <= dis then  
-        return true
-    else 
-        return false
-    end
+local function q()
+    return globalOnPoliceDuty or globalOnPrisonDuty or globalNHSOnDuty
 end
