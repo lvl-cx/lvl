d69e2fa5607c1c2a6278f16bce2a2dbe154b3c54
terrorist <34693611+terrorist@users.noreply.github.com>
2022-08-11 17:21:13 +0100

    tweak barbers meaning clothing store hair colours save

--- arma-barbers/server/sv_skin.lua
+++ arma-barbers/server/sv_skin.lua
@@ -11,6 +11,21 @@ AddEventHandler("ARMA:saveFaceData", function(faceSaveData)
     ARMA.setUData({user_id, "ARMA:Face:Data", json.encode(faceSaveData)})
 end)
 
+RegisterNetEvent("ARMA:saveClothingHairData") -- this updates hair from clothing stores
+AddEventHandler("ARMA:saveClothingHairData", function(hairtype, haircolour)
+    local source = source
+    local user_id = ARMA.getUserId({source})
+    facesavedata = {}
+    ARMA.getUData({user_id, "ARMA:Face:Data", function(data)
+        if data ~= nil then
+            facesavedata = json.decode(data)
+            facesavedata["hair"] = hairtype
+            facesavedata["haircolor"] = haircolour
+            ARMA.setUData({user_id, "ARMA:Face:Data", json.encode(facesavedata)})
+        end
+    end})
+end)
+
 RegisterNetEvent("ARMA:changeHairStyle")
 AddEventHandler("ARMA:changeHairStyle", function()
     local source = source
