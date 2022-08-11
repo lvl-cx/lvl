15d23c5ad46a23f1124e51d07277c716dfeb9c39
terrorist <34693611+terrorist@users.noreply.github.com>
2022-08-11 04:55:12 +0100

    update spectate script

--- arma/client/cl_spectate.lua
+++ arma/client/cl_spectate.lua
@@ -81,47 +81,52 @@ Citizen.CreateThread(function()
             local A = GetPlayerFromServerId(TargetSpectate)
             if A ~= -1 then
                 local l = tARMA.getPlayerPed()
-                local B = GetPlayerPed(A)
-                local C = GetEntityCoords(B)
-                local D = GetEntityHealth(B)
-                local E = GetEntityMaxHealth(B)
-                local F = GetSelectedPedWeapon(B)
-                local G = GetPedArmour(B)
-                local H = GetAmmoInPedWeapon(B, F)
-                draw2dText(0.76, 1.44, 1.0, 1.0, 0.4, "Health: " .. D .. "/" .. E, 51, 153, 255, 200)
-                draw2dText(0.76, 1.415, 1.0, 1.0, 0.4, "Armor: " .. G, 51, 153, 255, 200)
-                draw2dText(0.76,1.39,1.0,1.0,0.4,"Vehicle Health: " .. GetEntityHealth(GetVehiclePedIsIn(B, false)),51,153,255,200)
-                local I = WeaponNames[tostring(F)]
-                draw2dText(0.76, 1.365, 1.0, 1.0, 0.4, "Weapon: " .. (I or "N/A"), 51, 153, 255, 200)
-                draw2dText(0.76, 1.340, 1.0, 1.0, 0.4, "Ammo: " .. (H or "N/A"), 51, 153, 255, 200)
-                local J = GetActivePlayers()
-                for K, L in pairs(J) do
-                    local M = GetPlayerPed(L)
-                    SetEntityNoCollisionEntity(l, M, true)
+                local B=GetPlayerPed(A)
+                local C=GetEntityCoords(B)
+                local D=GetEntityHealth(B)
+                local E=GetEntityMaxHealth(B)
+                local F=GetSelectedPedWeapon(B)
+                local G=GetPedArmour(B)
+                local H=GetAmmoInPedWeapon(B,F)
+                draw2dText(0.76,1.415+(GetVehiclePedIsIn(B, false)~=0 and 0 or 0.025),1.0,1.0,0.4,"Health: "..D.."/"..E,51,153,255,200)
+                draw2dText(0.76,1.39+(GetVehiclePedIsIn(B, false)~=0 and 0 or 0.025),1.0,1.0,0.4,"Armor: "..G,51,153,255,200)
+                local I=tostring(WeaponNames[F])
+                draw2dText(0.76,1.365+(GetVehiclePedIsIn(B, false)~=0 and 0 or 0.025),1.0,1.0,0.4,"Weapon: "..(I or"N/A"),51,153,255,200)
+                draw2dText(0.76,1.340+(GetVehiclePedIsIn(B, false)~=0 and 0 or 0.025),1.0,1.0,0.4,"Ammo: "..(H or"N/A"),51,153,255,200)
+                if GetVehiclePedIsIn(B, false)~=0 then
+                    draw2dText(0.76,1.465,1.0,1.0,0.4,"Vehicle Health: "..GetEntityHealth(GetVehiclePedIsIn(B,false)),51,153,255,200)
+                    draw2dText(0.76,1.44,1.0,1.0,0.4,"Vehicle Speed: ".. math.ceil(GetEntitySpeed( GetVehiclePedIsIn(B, false))*2.2369),51,153,255,200)
                 end
-                if IsControlPressed(2, 241) then
-                    d = d + 0.5
+                HideHudComponentThisFrame(19)
+                HideHudComponentThisFrame(20)
+                local J=GetActivePlayers()
+                for K,L in pairs(J)do 
+                    local M=GetPlayerPed(L)
+                    SetEntityNoCollisionEntity(l,M,true)
                 end
-                if IsControlPressed(2, 242) then
-                    d = d - 0.5
+                if IsControlPressed(2,241)then 
+                    d=d+0.5 
                 end
-                if d > -1 then
-                    d = -1
+                if IsControlPressed(2,242)then 
+                    d=d-0.5 
                 end
-                local N = GetDisabledControlNormal(0, 1)
-                local O = GetDisabledControlNormal(0, 2)
-                b = b + N * 10
-                if b >= 360 then
-                    b = 0
+                if d>-1 then 
+                    d=-1 
                 end
-                c = c + O * 10
-                if c >= 360 then
-                    c = 0
+                local N=GetDisabledControlNormal(0,1)
+                local O=GetDisabledControlNormal(0,2)
+                b=b+N*10
+                if b>=360 then 
+                    b=0 
                 end
-                local P = polar3DToWorld3D(C, d, b, c)
-                SetCamCoord(e, P.x, P.y, P.z)
-                PointCamAtEntity(e, B)
-                SetEntityCoordsNoOffset(l, C.x, C.y, C.z - f)
+                c=c+O*10
+                if c>=360 then 
+                    c=0 
+                end
+                local P=polar3DToWorld3D(C,d,b,c)
+                SetCamCoord(e,P.x,P.y,P.z)
+                PointCamAtEntity(e,B)
+                SetEntityCoordsNoOffset(l,C.x,C.y,C.z-f)
             else
                 tARMA.notify("~r~Couldn't spectate, person not in your zone")
             end
