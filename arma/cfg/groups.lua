bf073b9a4eab4d4a2589b17bd727aaa9e7e5ce6b
terrorist <34693611+terrorist@users.noreply.github.com>
2022-08-11 18:47:57 +0100

    remove cosmetics and armour stuff for now

--- arma/cfg/groups.lua
+++ arma/cfg/groups.lua
@@ -1539,30 +1539,6 @@ cfg.groups = {
   
   ["TutorialDone"] = {},
 
-
--- COSMETIC ITEMS --
-
--- CHAINS / WATCHES --
-  ["Rolex"] = {},
-  ["AP"] = {},
-
--- CHAINS --
-  ["Gold Chain"] = {},
-
--- VESTS --
-  ["Army Black Armed Vest"] = {},
-  ["Army Green Armed Vest"] = {},
-  ["Army Black Ammo Vest"] = {},
-  ["Army Green Ammo Vest"] = {},
-  ["Army Black Large Vest"] = {},
-  ["Army Green Large Vest"] = {},
-  ["Standard Grey Vest"] = {},
-  ["Small Army Green Vest"] = {},
-  ["Camo Green Vest"] = {},
-  ["Blue Untar Vest"] = {},
-  ["British Grey Armed Vest"] = {},
-  ["Pink Icons Vest"] = {},
-
 }
 
 -- groups are added dynamically using the API or the menu, but you can add group when an user join here
