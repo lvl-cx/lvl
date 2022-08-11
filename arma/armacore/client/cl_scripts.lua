6483af3391dcdf781afb2eec50eb1ecb32e63b02
terrorist <34693611+terrorist@users.noreply.github.com>
2022-08-11 04:53:42 +0100

    exists in hotkeys

--- arma/armacore/client/cl_scripts.lua
+++ arma/armacore/client/cl_scripts.lua
@@ -96,16 +96,6 @@ Citizen.CreateThread(function()
 	end
 end)
 
--- WHISTLE --
-CreateThread(function()
-    while true do
-      if IsControlPressed(1, 19) and IsControlJustPressed(1, 32) then
-        ExecuteCommand("e whistle2")
-      end
-      Wait(1)
-    end
-end)
-
 -- [Z] BIGGER MINIMAP --
 Citizen.CreateThread(function()
 	while true do
