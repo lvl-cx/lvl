698b543d3a5dbb5a5a94a8eda85fcd537885f1e4
terrorist <34693611+terrorist@users.noreply.github.com>
2022-08-14 18:53:29 +0100

    tweak ammo types

--- arma/sharedcfg/ammotypes.lua
+++ arma/sharedcfg/ammotypes.lua
@@ -1,7 +1,12 @@
 ARMAAmmoTypes = {
-    ["357 Bullets"] = {"WEAPON_DEAGLE"},
-    ["12 Gauge Bullets"] = {"WEAPON_ITACHA"},
-    ["5.56 NATO"] = {"WEAPON_WINCHESTER12"},
-    ["7.62 Bullets"] = {"WEAPON_LR300", "WEAPON_AR15", "WEAPON_PPSH", "WEAPON_M16A1", "WEAPON_VANDALG"},
-    ["9mm Bullets"] = {"WEAPON_UMP45", "WEAPON_BERETTA", "WEAPON_M1911", "WEAPON_REVOLVERS"},
+    [".357 Bullets"] = {},
+    ["12 Gauge Bullets"] = {},
+    ["5.56mm NATO"] = {},
+    ["7.62mm Bullets"] = {},
+    ["9mm Bullets"] = {},
+    [".308 Sniper Rounds"] = {},
+    ["Police Issued 5.56mm"] = {},
+    ["Police Issued 9mm"] = {},
+    ["Police Issued .308 Sniper Rounds"] = {},
+    ["Police Issued 12 Gauge"] = {},
 }
