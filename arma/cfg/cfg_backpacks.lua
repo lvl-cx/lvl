09fd4a5bf2cfdbe03e88acbe27e74fcdf0e1abf8
terrorist <34693611+terrorist@users.noreply.github.com>
2022-08-15 23:58:55 +0100

    backpacks cfg

---
+++ arma/cfg/cfg_backpacks.lua
@@ -0,0 +1,18 @@
+local cfg = {}
+
+cfg.stores = {
+	["JDSports"] = {
+		_config = {130.56843566895,-1034.9497070313,28.555471420288,676,1,"JD Sports Bags","",true},
+		["Green Hiking Backpack (+40kg)"] = {5, 106, 1,120000,40},
+		["Gucci Bag (+20kg)"] = {5, 105, 0,60000,20},
+		["Off White Bag (+15kg)"] = {5, 103, 0,40000,15},
+		["Nike Bag (+30kg)"] = {5, 101, 0,80000,30},
+		["Hunting Backpack (+35kg)"] = {5, 102, 0,100000,35},
+	},
+	["Rebel"] = {
+		_config = {1540.7928466797,6332.4702148438,23.071977615356,676,1,"Rebel Backpacks","rebellicense.whitelisted",true},
+		["Rebel Backpack (+70kg)"] = {5, 109, 0,250000,70},
+	},
+}
+
+return cfg
