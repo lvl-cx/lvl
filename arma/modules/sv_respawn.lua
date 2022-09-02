35b269e5fdfc341545040a7e007e23cc00abf0be
terrorist <34693611+terrorist@users.noreply.github.com>
2022-09-02 01:22:37 +0100

    serverside for spawnmenu to get houses

--- arma/modules/sv_respawn.lua
+++ arma/modules/sv_respawn.lua
@@ -1,23 +1,27 @@
 local cfg=module("armacore/cfg/cfg_respawn")
 
+
 RegisterNetEvent("ARMA:SendSpawnMenu")
 AddEventHandler("ARMA:SendSpawnMenu",function()
     local source=source
     local spawnTable={}
+    local source = source 
+    local user_id = ARMA.getUserId(source)
     for k,v in pairs(cfg.spawnLocations)do
-        if v.permission[1]~=nil then
+        if v.permission[1] ~= nil then
             if ARMA.hasPermission(ARMA.getUserId(source),v.permission[1])then
-                table.insert(spawnTable,{v.name})
+                table.insert(spawnTable, k)
             end
         else
-            table.insert(spawnTable,{v.name})
+            table.insert(spawnTable, k)
         end
     end
-    TriggerClientEvent("ARMA:OpenSpawnMenu",source,spawnTable)
-end)
-
-RegisterNetEvent("ARMA:TakeAmount")
-AddEventHandler("ARMA:TakeAmount",function(amount)
-    local source=source
-    ARMA.tryBankPayment(ARMA.getUserId(source),amount)
+    exports['ghmattimysql']:execute("SELECT * FROM `arma_user_homes` WHERE user_id = @user_id", {user_id = user_id}, function(result)
+        if result ~= nil then 
+            for a,b in pairs(result) do
+                table.insert(spawnTable, b.home)
+            end
+            TriggerClientEvent("ARMA:OpenSpawnMenu",source,spawnTable)
+        end
+    end)
 end)
