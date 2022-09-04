8942f5a575504d940871a0b72ff2ddba9641bfd4
terrorist <34693611+terrorist@users.noreply.github.com>
2022-09-04 23:22:27 +0100

    never used

--- arma/client/inventory.lua
+++
@@ -1,9 +0,0 @@
-
-Citizen.CreateThread(function()
-    while (true) do
-      Citizen.Wait(0)
-      if IsControlJustPressed(1, 182) then
-        TriggerServerEvent('ARMA:OpenInventory', source)
-      end
-    end
-end)
