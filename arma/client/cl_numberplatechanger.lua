7551421fb0b59febbdd2d1c89a5f7070eacce9ba
terrorist <34693611+terrorist@users.noreply.github.com>
2022-09-04 02:22:02 +0100

    new plate changer w/ unique plates

---
+++ arma/client/cl_numberplatechanger.lua
@@ -0,0 +1,101 @@
+local isInMenu = false
+local currentPlate = nil
+
+local carstable = {}
+
+local location = vector3(-532.84381103516,-192.99229431152,38.222408294678)
+local m = module("arma-vehicles", "garages")
+m=m.garages
+
+RMenu.Add('PlateShop', 'main', RageUI.CreateMenu("Number Plate", "~b~Number Plate", 1350, 50))
+RMenu.Add("PlateShop", "sub", RageUI.CreateSubMenu(RMenu:Get("PlateShop", "main"), "Number Plate", "~b~Number Plate", 1350, 50))
+RageUI.CreateWhile(1.0, true, function()
+    if RageUI.Visible(RMenu:Get('PlateShop', 'main')) then
+        RageUI.DrawContent({ header = true, glare = false, instructionalButton = false}, function()
+            if next(carstable) == nil then
+                RageUI.Separator('~r~You do not own any vehicles.')
+            else
+                for i,j in pairs(carstable) do
+                    for k,v in pairs(m) do
+                        for a,l in pairs(v) do
+                            if a ~= "_config" then
+                                if a == j[1] then
+                                    RageUI.Button("~b~"..l[1], '~g~Spawncode: ~w~'..j[1]..' - ~g~Current Plate ~w~'..j[2], "", true,function(Hovered, Active, Selected)
+                                        if Selected then
+                                            selectedCar = j[1]
+                                            selectedCarName = l[1]
+                                        end
+                                    end, RMenu:Get("PlateShop", "sub"))
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end)
+    end
+    if RageUI.Visible(RMenu:Get('PlateShop', 'sub')) then
+        RageUI.DrawContent({ header = true, glare = false, instructionalButton = false}, function()
+            RageUI.Button("Change Number Plate", "~g~Changing plate of "..selectedCarName, {RightLabel = "~g~£50,000"}, true, function(Hovered, Active, Selected)
+                if Selected then
+                    TriggerServerEvent("ARMA:ChangeNumberPlate", selectedCar)
+                end
+            end)
+        end)
+    end
+end)
+
+
+Citizen.CreateThread(function()
+    local blip = AddBlipForCoord(location)
+	SetBlipSprite(blip, 521)
+	SetBlipScale(blip, 0.7)
+	SetBlipColour(blip, 2)
+	SetBlipAsShortRange(blip, true)
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentString("Plate Shop")
+	EndTextCommandSetBlipName(blip)
+    while true do
+        Citizen.Wait(0)
+        if isInArea(location, 100.0) then 
+            DrawMarker(20, location, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5, 0, 0, 255, 60, false, true, 2, true)
+        end
+        if isInArea(location, 1.4) and isInMenu == false then 
+            alert('Press ~INPUT_VEH_HORN~ to Change your number plate')
+            if IsControlJustPressed(0, 51) then 
+                TriggerServerEvent('ARMA:getCars')
+                RageUI.Visible(RMenu:Get("PlateShop", "main"), true)
+                isInMenu = true
+            end
+        end
+        if isInArea(location, 1.4) == false and isInMenu then
+            RageUI.Visible(RMenu:Get("PlateShop", "main"), false)
+            RageUI.Visible(RMenu:Get("PlateShop", "sub"), false)
+            isInMenu = false
+        end
+    end
+end)
+
+RegisterNetEvent("ARMA:RecieveNumberPlate")
+AddEventHandler("ARMA:RecieveNumberPlate", function(numplate)
+    currentPlate = numplate
+end)
+
+function isInArea(v, dis) 
+    if #(GetEntityCoords(PlayerPedId()) - v) <= dis then  
+        return true
+    else 
+        return false
+    end
+end
+
+RegisterNetEvent("ARMA:carsTable")
+AddEventHandler("ARMA:carsTable",function(cars)
+    carstable = cars
+end)
+
+function alert(msg) 
+    SetTextComponentFormat("STRING")
+    AddTextComponentString(msg)
+    DisplayHelpTextFromStringLabel(0,0,1,-1)
+end
---
+++ arma/modules/sv_numberplatechanger.lua
@@ -0,0 +1,116 @@
+local forbiddenNames = {
+	"%^1",
+	"%^2",
+	"%^3",
+	"%^4",
+	"%^5",
+	"%^6",
+	"%^7",
+	"%^8",
+	"%^9",
+	"%^%*",
+	"%^_",
+	"%^=",
+	"%^%~",
+	"admin",
+	"nigger",
+	"cunt",
+	"faggot",
+	"fuck",
+	"fucker",
+	"fucking",
+	"anal",
+	"stupid",
+	"damn",
+	"cock",
+	"cum",
+	"dick",
+	"dipshit",
+	"dildo",
+	"douchbag",
+	"douch",
+	"kys",
+	"jerk",
+	"jerkoff",
+	"gay",
+	"homosexual",
+	"lesbian",
+	"suicide",
+	"mothafucka",
+	"negro",
+	"pussy",
+	"queef",
+	"queer",
+	"weeb",
+	"retard",
+	"masterbate",
+	"suck",
+	"tard",
+	"allahu akbar",
+	"terrorist",
+	"twat",
+	"vagina",
+	"wank",
+	"whore",
+	"wanker",
+	"n1gger",
+	"f4ggot",
+	"n0nce",
+	"d1ck",
+	"h0m0",
+	"n1gg3r",
+	"h0m0s3xual",
+	"free up mandem",
+	"nazi",
+	"hitler",
+	"cheater",
+	"cheating",
+}
+
+MySQL.createCommand("ARMA/update_numplate","UPDATE arma_user_vehicles SET vehicle_plate = @registration WHERE user_id = @user_id AND vehicle = @vehicle")
+
+RegisterNetEvent('ARMA:getCars')
+AddEventHandler('ARMA:getCars', function()
+    local cars = {}
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    exports['ghmattimysql']:execute("SELECT * FROM `arma_user_vehicles` WHERE user_id = @user_id", {user_id = user_id}, function(result)
+        if result ~= nil then 
+            for k,v in pairs(result) do
+                if v.user_id == user_id then
+                    cars[v.vehicle] = {v.vehicle, v.vehicle_plate}
+                end
+            end
+            TriggerClientEvent('ARMA:carsTable', source, cars)
+        end
+    end)
+end)
+
+RegisterNetEvent("ARMA:ChangeNumberPlate")
+AddEventHandler("ARMA:ChangeNumberPlate", function(vehicle)
+    local user_id = ARMA.getUserId(source)
+	ARMA.prompt(source,"Plate Name:","",function(source, plateName)
+		if plateName == '' then return end
+		for name in pairs(forbiddenNames) do
+			if plateName == forbiddenNames[name] then
+				ARMAclient.notify(source,{"~r~You cannot have this plate."})
+				return
+			end
+		end
+        exports['ghmattimysql']:execute("SELECT * FROM `arma_user_vehicles` WHERE vehicle_plate = @plate", {plate = plateName}, function(rows)
+            if result ~= nil then 
+                ARMAclient.notify(source,{"~r~This plate is already taken."})
+                return
+            end
+        end)
+		if ARMA.tryFullPayment(user_id,50000) then
+			ARMAclient.notify(source,{"~g~Changed plate of "..vehicle.." to "..plateName})
+			MySQL.execute("ARMA/update_numplate", {user_id = user_id, registration = plateName, vehicle = vehicle})
+			TriggerClientEvent("ARMA:RecieveNumberPlate", source, plateName)
+			TriggerClientEvent("arma:PlaySound", source, "apple")
+			TriggerEvent('ARMA:getCars')
+		else
+			ARMAclient.notify(source,{"~r~You don't have enough money!"})
+		end
+	end)
+end)
