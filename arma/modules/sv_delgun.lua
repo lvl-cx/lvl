b8de0443371669451f417184db5459d7b068a9a9
terrorist <34693611+terrorist@users.noreply.github.com>
2022-09-04 08:58:55 +0100

    delgun stuff, display name, temp and permid of object creator

---
+++ arma/modules/sv_delgun.lua
@@ -0,0 +1,18 @@
+local netObjects = {}
+
+RegisterServerEvent("ARMA:spawnVehicleCallback")
+AddEventHandler('ARMA:spawnVehicleCallback', function(a, b)
+    netObjects[b] = {source = a, id = ARMA.getUserId(a), name = GetPlayerName(a)}
+end)
+
+RegisterServerEvent("ARMA:delGunDelete")
+AddEventHandler("ARMA:delGunDelete", function(object)
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    if ARMA.hasPermission(user_id, 'admin.tickets') then
+        TriggerClientEvent("ARMA:deletePropClient", -1, object)
+        if netObjects[object] then
+            TriggerClientEvent("ARMA:returnObjectDeleted", source, 'This object was created by ~b~'..netObjects[object].name..' ~w~Temp ID: ~b~'..netObjects[object].source..' ~w~Perm ID: ~b~'..netObjects[object].id)
+        end
+    end
+end)
