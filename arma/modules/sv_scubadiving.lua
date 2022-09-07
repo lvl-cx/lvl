e539bf37a21812f8ab6ec30a2d9f1118a4bb49c0
terrorist <34693611+terrorist@users.noreply.github.com>
2022-09-07 20:59:15 +0100

    add scubadiving

---
+++ arma/modules/sv_scubadiving.lua
@@ -0,0 +1,70 @@
+local a = module("cfg/cfg_scubadiving")
+local currentDivers = {}
+local scubaLocations = {
+    'Heroin Tunnel',
+    'Plane Crash'
+}
+
+RegisterNetEvent("ARMA:requestScubaJob")
+AddEventHandler("ARMA:requestScubaJob", function()
+    local source=source
+    local user_id=ARMA.getUserId(source)
+    if ARMA.hasGroup(user_id,"Scuba Diver")then
+        if currentDivers[user_id]==nil then
+            currentDivers[user_id]={currentJob=""}
+            currentDivers[user_id].currentJob={jobActive = true}
+            local randomJob = math.random(1,#scubaLocations)
+            for k,v in pairs(a.locations)do
+                if v.name==scubaLocations[randomJob] then
+                    local jobInfo={
+                        name=v.name,
+                        position=v.position,
+                        dinghySpawnPositions=v.dinghySpawnPositions,
+                        dinghySpawnHeading=v.dinghySpawnHeading,
+                        blipId=v.blipId,
+                        blipColour=v.blipColour,
+                        rewardObjects=v.rewardObjects,
+                    }
+                    TriggerClientEvent("ARMA:createScubaJob",source,jobInfo,ARMA.getUserId(source))
+                end
+            end
+        end
+    else
+        ARMAclient.notify(source,{"~r~You are not clocked on as a Scuba Diver."})
+    end
+end)
+
+RegisterNetEvent("ARMA:scubaSetVehicle")
+AddEventHandler("ARMA:scubaSetVehicle", function(PermID,Vehicle)
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    if PermID == user_id then
+        if currentDivers[PermID]~=nil and currentDivers[PermID].currentJob.jobActive==true then
+            currentDivers[PermID].currentJob.dinghy=Vehicle
+        end
+    end
+end)
+
+RegisterNetEvent("ARMA:finishScubaJob")
+AddEventHandler("ARMA:finishScubaJob", function(PermID,CollectedPackages)
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    if PermID == user_id then
+        if currentDivers[PermID]~=nil and currentDivers[PermID].currentJob.jobActive==true then
+            currentDivers[PermID].currentJob.jobActive=false
+            currentDivers[PermID].currentJob.collectedPackages=CollectedPackages
+        end
+    end
+end)
+
+RegisterNetEvent("ARMA:claimScubaReward")
+AddEventHandler("ARMA:claimScubaReward", function(PermID)
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    if PermID == user_id then
+        if currentDivers[PermID]~=nil and currentDivers[PermID].currentJob.jobActive==false then
+            ARMA.giveBankMoney(PermID,tonumber("1"..currentDivers[PermID].currentJob.collectedPackages.."000"))
+            currentDivers[PermID]=nil
+        end
+    end
+end)
