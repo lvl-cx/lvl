9cdc0fedc818f320d5076b917b32a44473c9204b
terrorist <34693611+terrorist@users.noreply.github.com>
2022-09-08 22:43:03 +0100

    not used

--- arma/armacore/client/cl_scripts.lua
+++ arma/armacore/client/cl_scripts.lua
@@ -127,21 +127,6 @@ function taserFX()
     SetPedMotionBlur(playerPed, false)
 end
 
--- Remove random GTA UI'S --
-
-Citizen.CreateThread(function()
-    while true do
-      Citizen.Wait(0)
-      HideHudComponentThisFrame( 7 ) -- Area Name
-      HideHudComponentThisFrame( 9 ) -- Street Name
-      HideHudComponentThisFrame( 3 ) -- SP Cash display 
-      HideHudComponentThisFrame( 4 )  -- MP Cash display
-      HideHudComponentThisFrame( 13 ) -- Cash changes
-      HideHudComponentThisFrame( 6 ) -- Veh Name
-      HideHudComponentThisFrame( 8 ) -- Veh Class
-    end
-  end)
-
 -- AntiVDM --
 Citizen.CreateThread(function()
     while true do 
