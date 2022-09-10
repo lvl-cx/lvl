3cb93b937cbc247e1310bbbe6f106d9f4c416e7e
terrorist <34693611+terrorist@users.noreply.github.com>
2022-09-10 20:39:30 +0100

    remove unused fuel triggers

--- arma/modules/sv_fuel.lua
+++
@@ -1,7 +0,0 @@
-RegisterServerEvent('fuel:pay')
-AddEventHandler('fuel:pay', function(price)
-    local user_id = ARMA.getUserId(source)
-    local fuelAmount = math.floor(price)
-    if ARMA.tryFullPayment(user_id ,fuelAmount) then
-    end
-end)
