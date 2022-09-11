3d1a29c7e37cbe1bc1bbc0b0e45a58b85735f36d
terrorist <34693611+terrorist@users.noreply.github.com>
2022-09-11 17:28:59 +0100

    tweak money drops

--- arma/modules/sv_moneydrops.lua
+++ arma/modules/sv_moneydrops.lua
@@ -19,41 +19,37 @@ function tARMA.MoneyDrop()
     end
 end
 
-    RegisterNetEvent('ARMA:Moneydrop')
-    AddEventHandler('ARMA:Moneydrop', function(netid)
-        local source = source
-        if MoneydropEntities[netid] and not MoneydropEntities[netid][3] and #(GetEntityCoords(MoneydropEntities[netid][1]) - GetEntityCoords(GetPlayerPed(source))) < 10.0 then
-            MoneydropEntities[netid][3] = true;
-            local user_id = ARMA.getUserId(source)
-            if user_id ~= nil then
-        
-                TriggerClientEvent("ARMA:MoneyNotInBag",source)
-                if MoneydropEntities[netid].Money ~= 0 then
-                    ARMA.giveMoney(user_id,MoneydropEntities[netid].Money)
-                    ARMAclient.notify(source,{"~g~You have taken £"..tonumber(MoneydropEntities[netid].Money)})
-                    MoneydropEntities[netid].Money = 0
-                else
-                    --print("NO")
-                end
-            else
-                ARMAclient.notify(source,{"~r~The money drop is already being taken"})
-
+RegisterNetEvent('ARMA:Moneydrop')
+AddEventHandler('ARMA:Moneydrop', function(netid)
+    local source = source
+    if MoneydropEntities[netid] and not MoneydropEntities[netid][3] and #(GetEntityCoords(MoneydropEntities[netid][1]) - GetEntityCoords(GetPlayerPed(source))) < 10.0 then
+        MoneydropEntities[netid][3] = true;
+        local user_id = ARMA.getUserId(source)
+        if user_id ~= nil then
+            TriggerClientEvent("ARMA:MoneyNotInBag",source)
+            if MoneydropEntities[netid].Money ~= 0 then
+                ARMA.giveMoney(user_id,MoneydropEntities[netid].Money)
+                ARMAclient.notify(source,{"~g~You have taken £"..tonumber(MoneydropEntities[netid].Money)})
+                MoneydropEntities[netid].Money = 0
             end
+        else
+            ARMAclient.notify(source,{"~r~The money drop is already being taken"})
+
         end
-    end)
+    end
+end)
 
-    -- Get rid of looted moneydrops
-    Citizen.CreateThread(function()
-        while true do 
-            Wait(100)
-            for i,v in pairs(MoneydropEntities) do 
-                if v.Money == 0 then
-                    if DoesEntityExist(v[1]) then 
-                        DeleteEntity(v[1])
+Citizen.CreateThread(function()
+    while true do 
+        Wait(100)
+        for i,v in pairs(MoneydropEntities) do 
+            if v.Money == 0 then
+                if DoesEntityExist(v[1]) then 
+                    DeleteEntity(v[1])
 
-                        MoneydropEntities[i] = nil;
-                    end
+                    MoneydropEntities[i] = nil;
                 end
             end
         end
-    end)
+    end
+end)
