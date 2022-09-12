39982f2bb923e0af3156b096f89a7ff654c2d92c
terrorist <34693611+terrorist@users.noreply.github.com>
2022-09-12 23:11:29 +0100

    add headbag trigger

--- arma/modules/sv_police.lua
+++ arma/modules/sv_police.lua
@@ -16,37 +16,6 @@ function ARMA.insertPoliceRecord(user_id, line)
   end
 end
 
--- Hotkey Open Police PC 1/2
-function ARMA.openPolicePC(source)
-  ARMA.buildMenu("police_pc", {player = source}, function(menudata)
-    menudata.name = "Police PC"
-    menudata.css = {top="75px",header_color="rgba(0,125,255,0.75)"}
-    ARMA.openMenu(source,menudata)
-  end)
-end
-
--- Hotkey Open Police PC 2/2
-function tARMA.openPolicePC()
-  ARMA.openPolicePC(source)
-end
-
--- Hotkey Open Police Menu 1/2
-function ARMA.openPoliceMenu(source)
-  ARMA.buildMenu("police", {player = source}, function(menudata)
-    menudata.name = "Police"
-    menudata.css = {top="75px",header_color="rgba(0,125,255,0.75)"}
-    ARMA.openMenu(source,menudata)
-  end)
-end
-
--- Hotkey Open Police Menu 2/2
-function tARMA.openPoliceMenu()
-  ARMA.openPoliceMenu(source)
-end
-
--- police PC
-
-local menu_pc = {name=lang.police.pc.title(),css={top="75px",header_color="rgba(0,125,255,0.75)"}}
 
 -- search identity by registration
 local function ch_searchreg(player,choice)
@@ -122,94 +91,7 @@ local function ch_delete_police_records(player,choice)
   end)
 end
 
--- close business of an arrested owner
-local function ch_closebusiness(player,choice)
-  ARMAclient.getNearestPlayer(player,{5},function(nplayer)
-    local nuser_id = ARMA.getUserId(nplayer)
-    if nuser_id ~= nil then
-      ARMA.getUserIdentity(nuser_id, function(identity)
-        ARMA.getUserBusiness(nuser_id, function(business)
-          if identity and business then
-            ARMA.request(player,lang.police.pc.closebusiness.request({identity.name,identity.firstname,business.name}),15,function(player,ok)
-              if ok then
-                ARMA.closeBusiness(nuser_id)
-                ARMAclient.notify(player,{lang.police.pc.closebusiness.closed()})
-              end
-            end)
-          else
-            ARMAclient.notify(player,{lang.common.no_player_near()})
-          end
-        end)
-      end)
-    else
-      ARMAclient.notify(player,{lang.common.no_player_near()})
-    end
-  end)
-end
 
-menu_pc[lang.police.pc.searchreg.title()] = {ch_searchreg,lang.police.pc.searchreg.description()}
-menu_pc[lang.police.pc.trackveh.title()] = {ch_trackveh,lang.police.pc.trackveh.description()}
-menu_pc[lang.police.pc.records.show.title()] = {ch_show_police_records,lang.police.pc.records.show.description()}
-menu_pc[lang.police.pc.records.delete.title()] = {ch_delete_police_records, lang.police.pc.records.delete.description()}
-menu_pc[lang.police.pc.closebusiness.title()] = {ch_closebusiness,lang.police.pc.closebusiness.description()}
-
-menu_pc.onclose = function(player) -- close pc gui
-  ARMAclient.removeDiv(player,{"police_pc"})
-end
-
-local function pc_enter(source,area)
-  local user_id = ARMA.getUserId(source)
-  if user_id ~= nil and ARMA.hasPermission(user_id,"police.pc") then
-    ARMA.openMenu(source,menu_pc)
-  end
-end
-
-local function pc_leave(source,area)
-  ARMA.closeMenu(source)
-end
-
--- main menu choices
-
----- handcuff
-local choice_handcuff = {function(player,choice)
-  ARMAclient.getNearestPlayer(player,{10},function(nplayer)
-    local nuser_id = ARMA.getUserId(nplayer)
-    if nuser_id ~= nil then
-      ARMAclient.toggleHandcuff(nplayer,{})
-    else
-      ARMAclient.notify(player,{lang.common.no_player_near()})
-    end
-  end)
-end,lang.police.menu.handcuff.description()}
-
----- putinveh
---[[
--- veh at position version
-local choice_putinveh = {function(player,choice)
-  ARMAclient.getNearestPlayer(player,{10},function(nplayer)
-    local nuser_id = ARMA.getUserId(nplayer)
-    if nuser_id ~= nil then
-      ARMAclient.isHandcuffed(nplayer,{}, function(handcuffed)  -- check handcuffed
-        if handcuffed then
-          ARMAclient.getNearestOwnedVehicle(player, {10}, function(ok,vtype,name) -- get nearest owned vehicle
-            if ok then
-              ARMAclient.getOwnedVehiclePosition(player, {vtype}, function(x,y,z)
-                ARMAclient.putInVehiclePositionAsPassenger(nplayer,{x,y,z}) -- put player in vehicle
-              end)
-            else
-              ARMAclient.notify(player,{lang.vehicle.no_owned_near()})
-            end
-          end)
-        else
-          ARMAclient.notify(player,{lang.police.not_handcuffed()})
-        end
-      end)
-    else
-      ARMAclient.notify(player,{lang.common.no_player_near()})
-    end
-  end)
-end,lang.police.menu.putinveh.description()}
---]]
 
 local choice_putinveh = {function(player,choice)
   ARMAclient.getNearestPlayer(player,{10},function(nplayer)
@@ -227,23 +109,6 @@ local choice_putinveh = {function(player,choice)
     end
   end)
 end,lang.police.menu.putinveh.description()}
-
-local choice_getoutveh = {function(player,choice)
-  ARMAclient.getNearestPlayer(player,{10},function(nplayer)
-    local nuser_id = ARMA.getUserId(nplayer)
-    if nuser_id ~= nil then
-      ARMAclient.isHandcuffed(nplayer,{}, function(handcuffed)  -- check handcuffed
-        if handcuffed then
-          ARMAclient.ejectVehicle(nplayer, {})
-        else
-          ARMAclient.notify(player,{lang.police.not_handcuffed()})
-        end
-      end)
-    else
-      ARMAclient.notify(player,{lang.common.no_player_near()})
-    end
-  end)
-end,lang.police.menu.getoutveh.description()}
 
 ---- askid
 local choice_askid = {function(player,choice)
@@ -428,6 +293,63 @@ RegisterCommand('storeallweapons', function(source)
   choice_store_weapons(source)
 end)
 
+
+RegisterCommand('shield', function(source, args)
+  local source = source
+  local user_id = ARMA.getUserId(source)
+  if ARMA.hasPermission(user_id, 'police.onduty.permission') then
+    TriggerClientEvent('ARMA:toggleShieldMenu', source)
+  end
+end)
+
+RegisterCommand('cuff', function(source, args)
+  local source = source
+  local user_id = ARMA.getUserId(source)
+  if ARMA.hasPermission(user_id, 'police.onduty.permission') then
+    ARMAclient.getNearestPlayer(source,{10},function(nplayer)
+      if nplayer ~= nil then
+          ARMAclient.isHandcuffed(nplayer,{},function(handcuffed)
+            if handcuffed then
+              TriggerClientEvent('ARMA:uncuffAnim', source, nplayer, false)
+              TriggerClientEvent('ARMA:unHandcuff', nplayer, false)
+            else
+              TriggerClientEvent('ARMA:arrestCriminal', nplayer, source)
+              TriggerClientEvent('ARMA:arrestFromPolice', source)
+            end
+            TriggerClientEvent('ARMA:toggleHandcuffs', nplayer, false)
+            TriggerClientEvent('ARMA:playHandcuffSound', -1, GetEntityCoords(GetPlayerPed(source)))
+          end)
+      else
+        ARMAclient.notify(source,{lang.common.no_player_near()})
+      end
+    end)
+  end
+end)
+
+RegisterCommand('frontcuff', function(source, args)
+  local source = source
+  local user_id = ARMA.getUserId(source)
+  if ARMA.hasPermission(user_id, 'police.onduty.permission') then
+    ARMAclient.getNearestPlayer(source,{10},function(nplayer)
+      if nplayer ~= nil then
+        ARMAclient.isHandcuffed(nplayer,{},function(handcuffed)
+          if handcuffed then
+            TriggerClientEvent('ARMA:uncuffAnim', source, nplayer, true)
+            TriggerClientEvent('ARMA:unHandcuff', nplayer, true)
+          else
+            TriggerClientEvent('ARMA:arrestCriminal', nplayer, source)
+            TriggerClientEvent('ARMA:arrestFromPolice', source)
+          end
+          TriggerClientEvent('ARMA:toggleHandcuffs', nplayer, true)
+          TriggerClientEvent('ARMA:playHandcuffSound', -1, GetEntityCoords(GetPlayerPed(source)))
+      end)
+      else
+        ARMAclient.notify(source,{lang.common.no_player_near()})
+      end
+    end)
+  end
+end)
+
 local section60s = {}
 RegisterCommand('s60', function(source, args)
     local source = source
@@ -463,5 +385,140 @@ RegisterCommand('handbook', function(source, args)
     local user_id = ARMA.getUserId(source)
     if ARMA.hasPermission(user_id, 'police.onduty.permission') then
         TriggerClientEvent('ARMA:toggleHandbook', source)
+    end
+end)
+
+local draggingPlayers = {}
+
+RegisterServerEvent('ARMA:dragPlayer')
+AddEventHandler('ARMA:dragPlayer', function(playersrc)
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    if user_id ~= nil and ARMA.hasPermission(user_id, "police.onduty.permission") then
+      if playersrc ~= nil then
+        local nuser_id = ARMA.getUserId(playersrc)
+          if nuser_id ~= nil then
+            ARMAclient.isHandcuffed(playersrc,{},function(handcuffed)
+                if handcuffed then
+                    if draggingPlayers[user_id] then
+                      TriggerClientEvent("ARMA:drag", source, playersrc)
+                    else
+                      TriggerClientEvent("ARMA:undrag", source, playersrc)
+                      draggingPlayers[user_id] = playersrc
+                    end
+                else
+                    ARMAclient.notify(source,{"~r~Player is not handcuffed."})
+                end
+            end)
+          else
+              ARMAclient.notify(source,{"~r~There is no player nearby"})
+          end
+      else
+          ARMAclient.notify(source,{"~r~There is no player nearby"})
+      end
+    end
+end)
+
+RegisterServerEvent('ARMA:putInVehicle')
+AddEventHandler('ARMA:putInVehicle', function(playersrc)
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    if user_id ~= nil and ARMA.hasPermission(user_id, "police.onduty.permission") then
+      if playersrc ~= nil then
+        ARMAclient.isHandcuffed(playersrc,{}, function(handcuffed)  -- check handcuffed
+          if handcuffed then
+            ARMAclient.putInNearestVehicleAsPassenger(playersrc, {10})
+          else
+            ARMAclient.notify(source,{lang.police.not_handcuffed()})
+          end
+        end)
+      end
+    end
+end)
+
+RegisterServerEvent('ARMA:ejectFromVehicle')
+AddEventHandler('ARMA:ejectFromVehicle', function()
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    if user_id ~= nil and ARMA.hasPermission(user_id, "police.onduty.permission") then
+      ARMAclient.getNearestPlayer(source,{10},function(nplayer)
+        local nuser_id = ARMA.getUserId(nplayer)
+        if nuser_id ~= nil then
+          ARMAclient.isHandcuffed(nplayer,{}, function(handcuffed)  -- check handcuffed
+            if handcuffed then
+              ARMAclient.ejectVehicle(nplayer, {})
+            else
+              ARMAclient.notify(source,{lang.police.not_handcuffed()})
+            end
+          end)
+        else
+          ARMAclient.notify(source,{lang.common.no_player_near()})
+        end
+      end)
+    end
+end)
+
+
+RegisterServerEvent("ARMA:Knockout")
+AddEventHandler('ARMA:Knockout', function()
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    ARMAclient.getNearestPlayer(player, {10}, function(nplayer)
+        local nuser_id = ARMA.getUserId(nplayer)
+        if nuser_id ~= nil then
+            TriggerClientEvent('ARMA:knockOut', nplayer)
+            SetTimeout(30000, function()
+                TriggerClientEvent('ARMA:knockOutDisable', nplayer)
+            end)
+        end
+    end)
+end)
+
+RegisterServerEvent("ARMA:KnockoutNoAnim")
+AddEventHandler('ARMA:KnockoutNoAnim', function()
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    if ARMA.hasGroup(user_id, 'Founder') then
+      ARMAclient.getNearestPlayer(player, {10}, function(nplayer)
+          local nuser_id = ARMA.getUserId(nplayer)
+          if nuser_id ~= nil then
+              TriggerClientEvent('ARMA:knockOut', nplayer)
+              SetTimeout(30000, function()
+                  TriggerClientEvent('ARMA:knockOutDisable', nplayer)
+              end)
+          end
+      end)
+    end
+end)
+
+RegisterServerEvent("ARMA:requestPlaceBagOnHead")
+AddEventHandler('ARMA:requestPlaceBagOnHead', function()
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    if ARMA.getInventoryItemAmount(user_id, 'headbag') >= 1 then
+      ARMAclient.getNearestPlayer(source, {10}, function(nplayer)
+          local nuser_id = ARMA.getUserId(nplayer)
+          if nuser_id ~= nil then
+              ARMA.tryGetInventoryItem(user_id, 'headbag', 1, true)
+              TriggerClientEvent('ARMA:placeHeadBag', nplayer)
+          end
+      end)
+    end
+end)
+
+RegisterServerEvent('ARMA:gunshotTest')
+AddEventHandler('ARMA:gunshotTest', function(playersrc)
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    if user_id ~= nil and ARMA.hasPermission(user_id, "police.onduty.permission") then
+      if playersrc ~= nil then
+        ARMAclient.hasRecentlyShotGun(playersrc,{}, function(shotagun)  -- check handcuffed
+          if shotagun then
+            ARMAclient.notify(source, {"~r~Player has recently shot a gun."})
+          else
+            ARMAclient.notify(source, {"~r~Player has no gunshot residue on fingers."})
+          end
+        end)
+      end
     end
 end)
