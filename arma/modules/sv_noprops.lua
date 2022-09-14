5513a1983f7266c7d610072afade2fa0ca9bb701
terrorist <34693611+terrorist@users.noreply.github.com>
2022-09-14 19:43:08 +0100

    slight change to a kick func

--- arma/modules/sv_noprops.lua
+++ arma/modules/sv_noprops.lua
@@ -3,5 +3,5 @@ AddEventHandler("ARMA:alertNoProps", function(a,b,c,d,e)
 	local source = source
 	local user_id = ARMA.getUserId(source)
     if tonumber(user_id) == 1 or tonumber(user_id) == 2 then return end
-	ARMA.kick(source, "No props was detected. Remove the pack to join.")
+	DropPlayer(source, "No props was detected. Remove the pack to join.")
 end)
