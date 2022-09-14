9b314519e9dc5ef47e537850bb3b21f1fa922e2e
terrorist <34693611+terrorist@users.noreply.github.com>
2022-09-14 20:35:36 +0100

    unused

--- arma/armacore/modules/sv_idsabovehead.lua
+++
@@ -1,8 +0,0 @@
-RegisterNetEvent('ARMA:IDsAboveHead')
-AddEventHandler('ARMA:IDsAboveHead', function(status)
-    local status = status
-    local user_id = ARMA.getUserId({source})
-    if ARMA.hasPermission(user_id, 'admin.noclip') then
-        TriggerClientEvent('ARMA:ChangeIDs', source, status)
-    end
-end)
