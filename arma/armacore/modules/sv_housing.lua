881e6e6dffcc945bb4e9beee7a32b6e25452db96
terrorist <34693611+terrorist@users.noreply.github.com>
2022-09-18 17:47:10 +0100

    sort server side for house rent management (renting and ending rent)

--- arma/armacore/modules/sv_housing.lua
+++ arma/armacore/modules/sv_housing.lua
@@ -8,6 +8,21 @@ MySQL.createCommand("ARMA/get_address","SELECT home, number FROM arma_user_homes
 MySQL.createCommand("ARMA/get_home_owner","SELECT user_id FROM arma_user_homes WHERE home = @home AND number = @number")
 MySQL.createCommand("ARMA/rm_address","DELETE FROM arma_user_homes WHERE user_id = @user_id AND home = @home")
 MySQL.createCommand("ARMA/set_address","REPLACE INTO arma_user_homes(user_id,home,number) VALUES(@user_id,@home,@number)")
+MySQL.createCommand("ARMA/fetch_rented_houses", "SELECT * FROM arma_user_homes WHERE rented = 1")
+MySQL.createCommand("ARMA/rentedupdatehouse", "UPDATE arma_user_homes SET user_id = @id, rented = @rented, rentedid = @rentedid, rentedtime = @rentedunix WHERE user_id = @user_id AND home = @home")
+
+Citizen.CreateThread(function()
+    while true do
+        Wait(300000)
+        MySQL.query('ARMA/fetch_rented_houses', {}, function(rentedhouses)
+            for i,v in pairs(rentedhouses) do 
+               if os.time() > tonumber(v.rentedtime) then
+                  MySQL.execute('ARMA/rentedupdatehouse', {id = v.rentedid, rented = 0, rentedid = "", rentedunix = "", user_id = v.user_id, home = v.home})
+               end
+            end
+        end)
+    end
+end)
 
 function getUserAddress(user_id, cbr)
     local task = Task(cbr)
@@ -245,6 +260,91 @@ AddEventHandler("ARMAHousing:Sell", function(house)
         end
     end)
 end)
+
+RegisterNetEvent('ARMAHousing:Rent')
+AddEventHandler('ARMAHousing:Rent', function(house)
+    local user_id = ARMA.getUserId(source)
+    local player = ARMA.getUserSource(user_id)
+
+    getUserByAddress(house, 1, function(huser_id)
+        if huser_id == user_id then
+            ARMAclient.getNearestPlayers(player,{15},function(nplayers) --get nearest players
+                usrList = ""
+                for k, v in pairs(nplayers) do
+                    usrList = usrList .. "[" .. ARMA.getUserId(k) .. "]" .. GetPlayerName(k) .. " | " --add ids to usrList
+                end
+                if usrList ~= "" then
+                    ARMA.prompt(player,"Players Nearby: " .. usrList .. "","",function(player, target_id) --ask for id
+                        target_id = target_id
+                        if target_id ~= nil and target_id ~= "" then --validation
+                            local target = ARMA.getUserSource(tonumber(target_id)) --get source of the new owner id
+                            if target ~= nil then
+                                ARMA.prompt(player,"Price £: ","",function(player, amount) --ask for price
+                                    if tonumber(amount) and tonumber(amount) > 0 then
+                                        ARMA.prompt(player,"Duration: ","",function(player, duration) --ask for price
+                                            if tonumber(duration) and tonumber(duration) > 0 then
+                                                ARMA.prompt(player, "Please replace text with YES or NO to confirm", "Rent Details:\nHouse: "..house.."\nRent Cost: "..amount.."\nDuration: "..duration.." hours\nRenting to player: "..GetPlayerName(target).."("..target_id..")",function(player,details)
+                                                    if string.upper(details) == 'YES' then
+                                                        ARMAclient.notify(player, {'~g~Rent offer sent!'})
+                                                        ARMA.request(target,GetPlayerName(player).." wants to rent: " ..house.. " for "..duration.." hours, for £"..amount, 30, function(target,ok) --request new owner if they want to buy
+                                                            if ok then 
+                                                                local buyer_id = ARMA.getUserId(target) --get perm id of new owner
+                                                                amount = tonumber(amount) --convert amount str to int
+                                                                if ARMA.tryFullPayment(buyer_id,amount) then
+                                                                    local rentedTime = os.time()
+                                                                    rentedTime = rentedTime  + (60 * 60 * tonumber(duration)) 
+                                                                    MySQL.execute("ARMA/rentedupdatehouse", {user_id = user_id, home = house, id = target_id, rented = 1, rentedid = user_id, rentedunix =  rentedTime }) 
+                                                                    ARMA.giveBankMoney(user_id, amount)
+                                                                    ARMAclient.notify(player,{"~g~You have successfully rented "..house.." to ".. GetPlayerName(target).." for £"..amount.."!"}) --notify original owner
+                                                                    ARMAclient.notify(target,{"~g~"..GetPlayerName(player).." has successfully rented you "..house.." for £"..amount.."!"}) --notify new owner
+                                                                    local webhook = 'https://discord.com/api/webhooks/973347553280135198/dnkc8GYV2hOIe6oi0Nl6YXo-ymdP2OgV6zhCOG6e_SJGuPL9SawLHLCag8bvx5GbsEe6'
+                                                                    local embed = {
+                                                                        {
+                                                                            ["color"] = "16777215",
+                                                                            ["title"] = "House Logs",
+                                                                            ["description"] = "**User Name:** "..GetPlayerName(source).."\n**User ID:** "..ARMA.getUserId(source).."\n**Buyer Name: **"..GetPlayerName(source).. "\n**Buyer ID: **" ..ARMA.getUserId(source).. "\n**Price: **".. amount.. "\n**House Name: **" ..house,
+                                                                            ["footer"] = {
+                                                                                ["text"] = os.date("%X"),
+                                                                            },
+                                                                        }
+                                                                    }
+                                                                    PerformHttpRequest(webhook, function (err, text, headers) end, 'POST', json.encode({username = 'ARMA', embeds = embed}), { ['Content-Type'] = 'application/json' })
+                                                    
+                                                            
+                                                                else
+                                                                    ARMAclient.notify(player,{"~r~".. GetPlayerName(target).." doesn't have enough money!"}) --notify original owner
+                                                                    ARMAclient.notify(target,{"~r~You don't have enough money!"}) --notify new owner
+                                                                end
+                                                            else
+                                                                ARMAclient.notify(player,{"~r~"..GetPlayerName(target).." has refused to rent "..house.."!"}) --notify owner that refused
+                                                                ARMAclient.notify(target,{"~r~You have refused to rent "..house.."!"}) --notify new owner that refused
+                                                            end
+                                                        end)
+                                                    end
+                                                end)
+                                            end
+                                        end)
+                                    else
+                                        ARMAclient.notify(player,{"~r~Price of home needs to be a number!"}) -- if price of home is a string not a int
+                                    end
+                                end)
+                            else
+                                ARMAclient.notify(player,{"~r~That Perm ID seems to be invalid!"}) --couldnt find perm id
+                            end
+                        else
+                            ARMAclient.notify(player,{"~r~No Perm ID selected!"}) --no perm id selected
+                        end
+                    end)
+                else
+                    ARMAclient.notify(player,{"~r~No players nearby!"}) --no players nearby
+                end
+            end)
+        else
+            ARMAclient.notify(player,{"~r~You do not own "..house.."!"})
+        end
+    end)
+end)
+
 
 --Chest
 
