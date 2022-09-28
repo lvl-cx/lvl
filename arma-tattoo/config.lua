9180607c6ad4b67c9755d1e25ddb43a10396da2e
terrorist <34693611+terrorist@users.noreply.github.com>
2022-09-28 01:11:10 +0100

    sort tattoo config

--- arma-tattoo/config.lua
+++ arma-tattoo/config.lua
@@ -10,6 +10,13 @@ Config.TattooCats = {
 	{"ZONE_TORSO", "Torso Tattoo", {vec(0.0, 0.7, 0.2), vec(0.0, -0.7, 0.2)}, vec(0.0, 0.0, 0.2)},
 }
 
+Config.Shops = {
+	vec(-1153.6, -1425.6, 4.9),
+	vec(322.1, 180.4, 103.5),
+	vec(-3170.0, 1075.0, 20.8),
+	vec(1864.6, 3747.7, 33.0),
+	vec(-293.7, 6200.0, 31.4)
+}
 
 Config.interiorIds = {}
 for k, v in ipairs(Config.Shops) do
