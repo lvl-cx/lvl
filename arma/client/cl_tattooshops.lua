73b8a58d1c617fb46e5ef3ac0e5b4f33ce5487c1
terrorist <34693611+terrorist@users.noreply.github.com>
2022-09-28 01:11:17 +0100

    fix tattoo marker / area

--- arma/client/cl_tattooshops.lua
+++ arma/client/cl_tattooshops.lua
@@ -1,5 +1,4 @@
 local a = {
-    vector3(-3170.5043945313,1075.6791992188,20.829191207886),
 	vector3(-1153.6, -1425.6, 4.9),
 	vector3(322.1, 180.4, 103.5),
 	vector3(-3170.0, 1075.0, 20.8),
@@ -16,7 +15,6 @@ AddEventHandler("ARMA:onClientSpawn",function(D, E)
         local f = function()
         end
         for g, h in pairs(a) do
-            tARMA.createArea("tattooshop_" .. g, h, 1.5, 6, d, e, f)
             tARMA.addMarker(h.x, h.y, h.z - 0.2, 0.5, 0.5, 0.5, 0, 50, 255, 170, 50, 20, false, false, true)
             tARMA.addBlip(h.x, h.y, h.z, 75, 1, "Tattoo Shop")
         end
