6f322288bdeca9deff7172ae5ce484cffe0e18fe
terrorist <34693611+terrorist@users.noreply.github.com>
2022-10-13 12:27:07 +0100

    unused f10 type

--- arma/modules/sv_warningsystem.lua
+++ arma/modules/sv_warningsystem.lua
@@ -1,8 +1,3 @@
---(Thanks to Rubbertoe98) (https://github.com/rubbertoe98/FiveM-Scripts/tree/master/arma_punishments) for the original script.
--- Edits by JamesUK#6793 (to support js ghmatti version)
-
-
-
 RegisterCommand('sw', function(player, args)
     local user_id = ARMA.getUserId(player)
     local permID =  tonumber(args[1])
@@ -18,8 +13,6 @@ RegisterCommand('sw', function(player, args)
         end
     end
 end)
-
-
 	
 function getarmaWarnings(user_id,source) 
 	armawarningstables = exports['ghmattimysql']:executeSync("SELECT * FROM arma_warnings WHERE user_id = @uid", {uid = user_id})
@@ -45,26 +38,12 @@ AddEventHandler("arma:refreshWarningSystem",function()
 	end
 end)
 
-function f10Warn(target_id,adminName,warningReason)
-	warning = "Warning"
-	warningDate = getCurrentDate()
-	exports['ghmattimysql']:execute("INSERT INTO arma_warnings (`user_id`, `warning_type`, `admin`, `warning_date`, `reason`) VALUES (@user_id, @warning_type, @admin, @warning_date,@reason);", {user_id = target_id, warning_type = warning, admin = adminName, warning_date = warningDate, reason = warningReason}, function() end)
-end
-
 function f10Kick(target_id,adminName,warningReason)
 	warning = "Kick"
-	warningDate = getCurrentDate()
-	exports['ghmattimysql']:execute("INSERT INTO arma_warnings (`user_id`, `warning_type`, `admin`, `warning_date`, `reason`) VALUES (@user_id, @warning_type, @admin, @warning_date,@reason);", {user_id = target_id, warning_type = warning, admin = adminName, warning_date = warningDate, reason = warningReason}, function() end)
+	exports['ghmattimysql']:execute("INSERT INTO arma_warnings (`user_id`, `warning_type`, `admin`, `warning_date`, `reason`) VALUES (@user_id, @warning_type, @admin, @warning_date,@reason);", {user_id = target_id, warning_type = "Kick", admin = adminName, warning_date = os.date("%Y/%m/%d"), reason = warningReason}, function() end)
 end
 
 function f10Ban(target_id,adminName,warningReason,warning_duration)
 	warning = "Ban"
-	warningDate = getCurrentDate()
-	exports['ghmattimysql']:execute("INSERT INTO arma_warnings (`user_id`, `warning_type`, `duration`, `admin`, `warning_date`, `reason`) VALUES (@user_id, @warning_type, @duration, @admin, @warning_date,@reason);", {user_id = target_id, warning_type = warning, admin = adminName, duration = warning_duration, warning_date = warningDate, reason = warningReason}, function() end)
-end
-
-
-function getCurrentDate()
-	date = os.date("%Y/%m/%d")
-	return date
+	exports['ghmattimysql']:execute("INSERT INTO arma_warnings (`user_id`, `warning_type`, `duration`, `admin`, `warning_date`, `reason`) VALUES (@user_id, @warning_type, @duration, @admin, @warning_date,@reason);", {user_id = target_id, warning_type = "Ban", admin = adminName, duration = warning_duration, warning_date = os.date("%Y/%m/%d"), reason = warningReason}, function() end)
 end
