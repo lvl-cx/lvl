3225fb9d93951eb5b8397ebcec69fd36410660a0
terrorist <34693611+terrorist@users.noreply.github.com>
2022-11-24 04:09:36 +0000

    add gang markers

---
+++ arma/modules/sv_gangmarkers.lua
@@ -0,0 +1,29 @@
+RegisterServerEvent("ARMA:sendGangMarker")
+AddEventHandler("ARMA:sendGangMarker", function(coords)
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    local peoplesids = {}
+    local gangmembers = {}
+    exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
+        for K,V in pairs(gotGangs) do
+            local array = json.decode(V.gangmembers)
+            for I,L in pairs(array) do
+                if tostring(user_id) == I then
+                    isingang = true
+                    for U,D in pairs(array) do
+                        peoplesids[tostring(U)] = tostring(D.gangPermission)
+                    end
+                    exports['ghmattimysql']:execute('SELECT * FROM arma_users', function(gotUser)
+                        for J,G in pairs(gotUser) do
+                            if peoplesids[tostring(G.id)] ~= nil then
+                                local player = ARMA.getUserSource(tonumber(G.id))
+                                TriggerClientEvent('ARMA:drawGangMarker', player, coords)
+                            end
+                        end
+                    end)
+                    break
+                end
+            end
+        end
+    end)
+end)
