a20e99d7b80d7503c30ae0ffca3d5819ef653492
terrorist <34693611+terrorist@users.noreply.github.com>
2022-11-27 08:25:04 +0000

    redo antivdm

--- arma/armacore/client/cl_scripts.lua
+++ arma/armacore/client/cl_scripts.lua
@@ -66,29 +66,3 @@ AddEventHandler("playerSpawned", function(spawn)
 	SetCanAttackFriendly(GetPlayerPed(-1), true, false)
 	NetworkSetFriendlyFireOption(true)
 end)
-
--- AntiVDM --
-Citizen.CreateThread(function()
-    while true do 
-        local ped = PlayerPedId()
-        local vehiclecheck = GetVehiclePedIsIn(ped, false)
-        local players = GetActivePlayers()
-        for d,e in pairs(GetGamePool("CVehicle"))do 
-            local VehicleC = GetVehicleClass(e)
-            if f~=14 and f~=15 and f~=16 then 
-                if GetEntitySpeed(e) > 2.0 then 
-                    if GetPedInVehicleSeat(e, -1) ~= 0 then 
-                        SetEntityNoCollisionEntity(ped, e, true)
-                        SetEntityNoCollisionEntity(e, ped, true)
-                    end;
-                    for d,g in pairs(players) do 
-                        local h=GetPlayerPed(g)
-                        SetEntityNoCollisionEntity(vehiclecheck, h, true)
-                        SetEntityNoCollisionEntity(h, vehiclecheck, true)
-                    end 
-                end 
-            end
-         end;
-         Wait(0)
-        end 
-end)
