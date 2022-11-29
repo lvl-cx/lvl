b40d81c82872edd698049c58699b1a03fced2b6d
cnr <34693611+terrorist@users.noreply.github.com>
2022-11-29 15:57:53 -0800

    move tackle to police file

--- arma/modules/sv_police.lua
+++ arma/modules/sv_police.lua
@@ -526,3 +526,13 @@ AddEventHandler('ARMA:gunshotTest', function(playersrc)
       end
     end
 end)
+
+RegisterServerEvent('ARMA:tryTackle')
+AddEventHandler('ARMA:tryTackle', function(id)
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    if ARMA.hasPermission(user_id, 'police.onduty.permission') or ARMA.hasPermission(user_id, 'prisonguard.onduty.permission') then
+        TriggerClientEvent('ARMA:playTackle', source)
+        TriggerClientEvent('ARMA:getTackled', id, source)
+    end
+end)
