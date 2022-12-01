b1b134f3d7bc2c89f40aea9aa91f6baea00ae327
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-01 21:40:07 +0000

    tweak(userdata): optimise customisation saving

--- arma/client/cl_userdata.lua
+++ arma/client/cl_userdata.lua
@@ -1,4 +1,28 @@
 local a={}
+function getDefaultCustomization()
+    local s = {}
+    s = {}
+    s.model = "mp_m_freemode_01"
+    for t = 0, 19 do
+        s[t] = {0, 0}
+    end
+    s[0] = {0, 0}
+    s[1] = {0, 0}
+    s[2] = {47, 0}
+    s[3] = {5, 0}
+    s[4] = {4, 0}
+    s[5] = {0, 0}
+    s[6] = {7, 0}
+    s[7] = {51, 0}
+    s[8] = {0, 240}
+    s[9] = {0, 1}
+    s[10] = {0, 0}
+    s[11] = {5, 0}
+    s[12] = {4, 0}
+    s[15] = {0, 2}
+    return s
+end
+
 Citizen.CreateThread(function()
     print("[ARMA] Loading cached user data.")
     a=json.decode(GetResourceKvpString("ARMA_userdata")or"{}")
@@ -10,10 +34,13 @@ Citizen.CreateThread(function()
     end 
 end)
 function tARMA.updateCustomization(b)
-    a.customisation=tARMA.getCustomization()
-    if b then 
-        SetResourceKvp("ARMA_userdata",json.encode(a))
-    end 
+    local c = tARMA.getCustomization()
+    if c.modelhash ~= 0 and IsModelValid(c.modelhash) then
+        a.customisation=c
+        if b then 
+            SetResourceKvp("ARMA_userdata",json.encode(a))
+        end 
+    end
 end
 function tARMA.updatePos(b)
     a.position=GetEntityCoords(PlayerPedId())
@@ -35,7 +62,7 @@ function tARMA.updateArmour(b)
 end
 local c
 local d
-local function e()
+function tARMA.checkCustomization()
     if a.customisation==nil then 
         tARMA.setCustomization(getDefaultCustomization(),true,true)
     else 
