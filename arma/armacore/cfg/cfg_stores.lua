4c3c67cfd28414819a6f5de56880983b53bc1672
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-01 17:53:51 +0000

    tweak(stores): modify shaver price 250k

--- arma/armacore/cfg/cfg_stores.lua
+++ arma/armacore/cfg/cfg_stores.lua
@@ -27,7 +27,7 @@ cfg.shops = {
 cfg.shopItems = {
     {name = "Morphine", itemID = "morphine", price = 50000},
     {name = "Lockpick", itemID = "lockpick", price = 25000,},
-    {name = "Shaver", itemID = "shaver", price = 500000},
+    {name = "Shaver", itemID = "shaver", price = 250000},
     {name = "Head Bag", itemID = "headbag", price = 10000},
 }
 
