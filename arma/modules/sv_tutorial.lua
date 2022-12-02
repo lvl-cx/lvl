4471d5f59e0301c319d7294c9bf0ef058f3db62b
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-02 18:15:05 +0000

    tweak(dvsa): change speed limit

--- arma/modules/sv_tutorial.lua
+++ arma/modules/sv_tutorial.lua
@@ -1,12 +1,10 @@
-local tutorialBucket = 10
 RegisterNetEvent('ARMA:checkTutorial')
 AddEventHandler('ARMA:checkTutorial', function()
     local source = source
     local user_id = ARMA.getUserId(source)
     if not ARMA.hasGroup(user_id, 'TutorialDone') then
-        tutorialBucket = tutorialBucket + 1
         TriggerClientEvent('ARMA:startTutorial', source)
-        SetPlayerRoutingBucket(source, tutorialBucket)
+        SetPlayerRoutingBucket(source, user_id)
     end
 end)
 
