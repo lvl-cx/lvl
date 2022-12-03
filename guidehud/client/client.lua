806451735f49c4b894a897bbf73c114dde607f32
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-03 01:34:52 +0000

    Update client.lua

--- guidehud/client/client.lua
+++ guidehud/client/client.lua
@@ -1,7 +1,6 @@
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-
         if IsControlJustPressed(1, 288) then
             if IsUsingKeyboard(2) then
                 SetNuiFocus(true, true)
@@ -13,19 +12,4 @@ end)
 
 RegisterNUICallback('closeGuideHud', function(data, cb)
 	SetNuiFocus(false, false)
-end)
-
-
--- Variable to check if native has already been run
-local Ran = false
-
--- Wait until client is loaded into the map
-AddEventHandler("playerSpawned", function ()
-	-- If not already ran
-	if not Ran then
-		-- Close loading screen resource
-		ShutdownLoadingScreenNui()
-		-- Set as ran
-		Ran = true
-	end
 end)
