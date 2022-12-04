fa013738575fcd120bbbceeeabcf5a2c642d1b13
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-04 08:51:51 +0000

    add(bot): !cleargarage [permid]

---
+++ arma-bot/commands/cleargarage.js
@@ -0,0 +1,26 @@
+const resourcePath = global.GetResourcePath ?
+    global.GetResourcePath(global.GetCurrentResourceName()) : global.__dirname
+const settingsjson = require(resourcePath + '/settings.js')
+
+exports.runcmd = (fivemexports, client, message, params) => {
+    if (!params[0] || !parseInt(params[0])) {
+        return message.reply('Invalid args! Correct term is: ' + process.env.PREFIX + 'cleargarage [perm id]')
+    }
+    fivemexports['ghmattimysql'].execute("DELETE FROM vrp_user_vehicles WHERE user_id = ?", [parseInt(params[0])])
+    
+    let embed = {
+        "title": "Cleared Garage",
+        "description": `\nPerm ID: **${params[0]}**\n\nAdmin: <@${message.author.id}>`,
+        "color": 16711680,
+        "footer": {
+            "text": ""
+        },
+        "timestamp": new Date()
+    }
+    message.channel.send({ embed })
+}
+
+exports.conf = {
+    name: "cleargarage",
+    perm: 6
+}
