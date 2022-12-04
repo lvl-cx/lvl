ae01f878f4e1f2e65315289edb29e42b0fd9ece3
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-04 08:52:12 +0000

    add(bot): !clearwarnings [permid]

---
+++ arma-bot/commands/clearwarnings.js
@@ -0,0 +1,25 @@
+const resourcePath = global.GetResourcePath ?
+    global.GetResourcePath(global.GetCurrentResourceName()) : global.__dirname
+const settingsjson = require(resourcePath + '/settings.js')
+
+exports.runcmd = (fivemexports, client, message, params) => {
+    if (!params[0] || !parseInt(params[0])) {
+        return message.reply('Invalid args! Correct term is: ' + process.env.PREFIX + 'clearwarnings [perm id]')
+    }
+    fivemexports['ghmattimysql'].execute("DELETE FROM vrp_warnings WHERE user_id = ?", [parseInt(params[0])])
+    let embed = {
+        "title": "Cleared Warnings",
+        "description": `\nPerm ID: **${params[0]}**\n\nAdmin: <@${message.author.id}>`,
+        "color": 16711680,
+        "footer": {
+            "text": ""
+        },
+        "timestamp": new Date()
+    }
+    message.channel.send({ embed })
+}
+
+exports.conf = {
+    name: "clearwarings",
+    perm: 5
+}
