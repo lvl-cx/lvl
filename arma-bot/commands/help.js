77fdeb1a1c67d08f68d519b45be5fef1de8b0e8d
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-04 08:52:36 +0000

    tweak(bot): update !help accordingly

--- arma-bot/commands/help.js
+++ arma-bot/commands/help.js
@@ -6,7 +6,7 @@ exports.runcmd = (fivemexports, client, message, params) => {
     if (!params[0]) {
         let embed = {
             "title": "ARMA Bot Commands",
-            "description": `**Public Commands:** \n${process.env.PREFIX}verify [code] \n${process.env.PREFIX}hmc [spawncode] \n${process.env.PREFIX}gco [spawncode] \n${process.env.PREFIX}status \n${process.env.PREFIX}ip \n${process.env.PREFIX}top10 \n${process.env.PREFIX}staffapp \n${process.env.PREFIX}banappeal \n${process.env.PREFIX}locklist\n\n**Lower Staff Commands:**\n${process.env.PREFIX}ch [permid] \n${process.env.PREFIX}d2p [@user] \n${process.env.PREFIX}p2d [permid] \n${process.env.PREFIX}showf10 [permid] \n${process.env.PREFIX}userinfo [permid] \n${process.env.PREFIX}notes [permid] \n${process.env.PREFIX}addnote [permid] [text] \n${process.env.PREFIX}delnote [note-id]\n\n**Moderator Commands:** \n${process.env.PREFIX}ban [permid] [time (hours)] [reason] \n${process.env.PREFIX}garage [permid]\n\n**Senior Moderator Commands:** \n${process.env.PREFIX}unban [permid] \n${process.env.PREFIX}groups [permid] \n\n**Admin Commands:** \n${process.env.PREFIX}clearf10 [permid]`,
+            "description": `**Public Commands:** \n${process.env.PREFIX}verify [code] \n${process.env.PREFIX}hmc [spawncode] \n${process.env.PREFIX}gco [spawncode] \n${process.env.PREFIX}status \n${process.env.PREFIX}ip \n${process.env.PREFIX}staffapp \n${process.env.PREFIX}banappeal \n${process.env.PREFIX}locklist\n\n**Lower Staff Commands:**\n${process.env.PREFIX}ch [permid] \n${process.env.PREFIX}d2p [@user] \n${process.env.PREFIX}p2d [permid] \n${process.env.PREFIX}sw [permid] \n${process.env.PREFIX}profile [permid] \n${process.env.PREFIX}notes [permid] \n${process.env.PREFIX}addnote [permid] [text] \n${process.env.PREFIX}delnote [note-id]\n\n**Moderator Commands:** \n${process.env.PREFIX}ban [permid] [time (hours)] [reason] \n${process.env.PREFIX}garage [permid]\n\n**Senior Moderator Commands:** \n${process.env.PREFIX}unban [permid] \n${process.env.PREFIX}groups [permid]`,
             "color": settingsjson.settings.botColour,
             "footer": {
                 "text": ""
@@ -18,7 +18,7 @@ exports.runcmd = (fivemexports, client, message, params) => {
     else if (params[0] == 'management') {
         let embed = {
             "title": "ARMA Bot Commands",
-            "description": `**Management Commands:** \n${process.env.PREFIX}addcar [permid] \n${process.env.PREFIX}delcar [permid] \n${process.env.PREFIX}vote [text] \n${process.env.PREFIX}profile [text]`,
+            "description": `**Management Commands:** \n${process.env.PREFIX}addcar [permid] \n${process.env.PREFIX}delcar [permid] \n${process.env.PREFIX}vote [text] \n${process.env.PREFIX}clearwarnings [permid]`,
             "color": settingsjson.settings.botColour,
             "footer": {
                 "text": ""
@@ -30,7 +30,7 @@ exports.runcmd = (fivemexports, client, message, params) => {
     else if (params[0] == 'dev') {
         let embed = {
             "title": "ARMA Bot Commands",
-            "description": `**Developer Commands:** \n*(These commands will interact with the server database, do not run any if unsure of the outcome.)* \n${process.env.PREFIX}clearbans \n${process.env.PREFIX}punishments \n${process.env.PREFIX}delcardb \n${process.env.PREFIX}lockcar`,
+            "description": `**Developer Commands:** \n*(These commands will interact with the server database, do not run any if unsure of the outcome.)* \n${process.env.PREFIX}clearbans \n${process.env.PREFIX}punishments \n${process.env.PREFIX}clearvehicleowners [spawncode] \n${process.env.PREFIX}cleargarage [permid] \n${process.env.PREFIX}lockcar [spawncode]`,
             "color": settingsjson.settings.botColour,
             "footer": {
                 "text": ""
