2d437a652ca39642a1ebaffe012555f7b0da5d79
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-04 02:03:44 +0000

    add(nitro): /craftmoped, /craftbmx

---
+++ arma/modules/sv_discordnitro.lua
@@ -0,0 +1,22 @@
+RegisterCommand('craftbmx', function(source)
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    exports["discordroles"]:isRolePresent(source, {'975543463808487465'}, function(hasRole, roles)
+        if (not roles) then 
+            ARMAclient.notify(source,{"~r~It seems you don't have discord running or installed try restart fivem if this issue persists /calladmin."})
+        end
+        if hasRole then
+            TriggerClientEvent("ARMA:spawnNitroBMX", source)
+        end
+    end)
+end)
+
+RegisterCommand('craftmoped', function(source)
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    ARMAclient.isPlatClub(source, {}, function(isPlatClub)
+        if isPlatClub then
+            TriggerClientEvent("ARMA:spawnMoped", source)
+        end
+    end)
+end)
