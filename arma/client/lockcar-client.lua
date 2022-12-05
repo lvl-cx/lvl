ab87b9e9b8743db37b521a210516d5fa7de0c659
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-05 21:26:13 +0000

    tweak(lockcar): fix locking whilst dead

--- arma/client/lockcar-client.lua
+++ arma/client/lockcar-client.lua
@@ -1,8 +1,12 @@
 RegisterCommand('lockcar', function()
     local veh, name, nveh = tARMA.getNearestOwnedVehicle(5)
-    if veh then 
-        tARMA.vc_toggleLock(name)
-        tARMA.playSound("HUD_MINI_GAME_SOUNDSET", "5_SEC_WARNING")
+    if GetEntityHealth(tARMA.getPlayerPed()) > 102 then
+        if veh then 
+            tARMA.vc_toggleLock(name)
+            tARMA.playSound("HUD_MINI_GAME_SOUNDSET", "5_SEC_WARNING")
+        end
+    else
+        tARMA.notify("~r~You cannot lock a vehicle whilst you are dead.")
     end
 end)
 
