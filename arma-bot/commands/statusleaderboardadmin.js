e7e32869a2a1d492d30615b8370cba8762548b5f
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-06 11:12:01 +0000

    tweak(bot): update leaderboard admin

--- arma-bot/commands/statusleaderboardadmin.js
+++ arma-bot/commands/statusleaderboardadmin.js
@@ -6,14 +6,13 @@ var statusLeaderboard = require(resourcePath + '/statusleaderboard.json')
 let descriptionText = ''
 
 exports.runcmd = (fivemexports, client, message, params) => {
-    message.delete()
     var promoters = []
     const sortable = Object.fromEntries(
         Object.entries(statusLeaderboard['leaderboard']).sort(([,a],[,b]) => b-a)
     );
     for (i = 0; i < Object.keys(statusLeaderboard['leaderboard']).length; i++) {
         if (i < 10) {
-            promoters.push(`${Object.keys(sortable)[i]} - ${Object.values(sortable)[i]} minutes promoted\n`)
+            promoters.push(`${Object.keys(sortable)[i]} - ${(Object.values(sortable)[i]/60).toFixed(2)} hours promoted\n`)
         }
     }
     let embed = {
