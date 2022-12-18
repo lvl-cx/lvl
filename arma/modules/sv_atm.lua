9a4357a4b48720c322e6fb59974864315a9798d7
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-18 19:26:28 +0000

    tweak(atm): redo coords check func

--- arma/modules/sv_atm.lua
+++ arma/modules/sv_atm.lua
@@ -1,59 +1,72 @@
--- a basic ATM implementation
 local lang = ARMA.lang
-local cfg = module("cfg/cfg_atms")
-local atms = cfg.atms
-local onesync = GetConvar('onesync', nil)
-local function play_atm_enter(player)
-    ARMAclient.playAnim(player, {false,{{"amb@prop_human_atm@male@enter", "enter"},{"amb@prop_human_atm@male@idle_a", "idle_a"}}, false})
-end
-local function play_atm_exit(player)
-    ARMAclient.playAnim(player, {false, {{"amb@prop_human_atm@male@exit", "exit"}}, false})
-end
+local atms = {
+    vector3(89.577018737793,2.16031360626221,68.322021484375),
+    vector3(-526.497131347656,-1222.79455566406,18.4549674987793),
+    vector3(-2072.48413085938,-317.190521240234,13.315972328186),
+    vector3(-821.565551757813,-1081.90270996094,11.1324348449707),
+    vector3(1686.74694824219,4815.8828125,42.0085678100586),
+    vector3(-386.899444580078,6045.78466796875,31.5001239776611),
+    vector3(1171.52319335938,2702.44897460938,38.1754684448242),
+    vector3(1968.11157226563,3743.56860351563,32.3437271118164),
+    vector3(2558.85815429688,351.045166015625,108.621520996094),
+    vector3(1153.75634765625,-326.805023193359,69.2050704956055),
+    vector3(-56.9172439575195,-1752.17590332031,29.4210166931152),
+    vector3(-3241.02856445313,997.587158203125,12.5503988265991),
+    vector3(-1827.1884765625,784.907104492188,138.302581787109),
+    vector3(-1091.54748535156,2708.55786132813,18.9437484741211),
+    vector3(112.45637512207,-819.25048828125,31.3392715454102),
+    vector3(-256.173187255859,-716.031921386719,33.5202751159668),
+    vector3(174.227737426758,6637.88623046875,31.5730476379395),
+    vector3(-660.727661132813,-853.970336914063,24.484073638916),
+    vector3(147.72689819336,-1035.3897705078,29.342636108398),
+    vector3(-906.779296875,-2031.4281005859,9.4023332595825),
+    vector3(1095.2824707031,244.13655090332,-50.440773010254),
+    vector3(-1073.9733886719,-827.63989257812,19.036619186401),
+    vector3(1735.4162597656,6410.650390625,35.037200927734),
+    vector3(33.325065612793,-1348.1293945312,29.497016906738),
+    vector3(-2153.775390625,5236.8115234375,18.772468566895),
+}
+local organheist = module('cfg/cfg_organheist') 
 
-local function atm_choice_deposit(player, choice)
-    play_atm_enter(player) -- anim
-    ARMA.prompt(source, lang.atm.deposit.prompt(), "", function(player, v)
-        play_atm_exit(player)
-        v = parseInt(v)
-        if v > 0 then
-            local user_id = ARMA.getUserId(source)
-            if user_id ~= nil then
-                if ARMA.tryDeposit(user_id, v) then
-                    ARMAclient.notify(source, {lang.atm.deposit.deposited({v})})
-                else
-                    ARMAclient.notify(source, {lang.money.not_enough()})
-                end
+AddEventHandler("ARMA:playerSpawn", function(user_id, source, first_spawn)
+    if first_spawn then
+       TriggerClientEvent('ARMA:setupAtms', source, atms)
+    end
+end)
+
+local function coordsCheck(coords)
+    for i, v in pairs(cfg.atms) do
+        local atmCoords = vec3(v[1], v[2], v[3])
+        for j,k in pairs(organheist.locations) do
+            if #(coords - atmCoords) <= 5.0 or #(coords - k.atmLocation) <= 5.0 then
+                return true
             end
-        else
-            ARMAclient.notify(source, {lang.common.invalid_value()})
         end
-    end)
+    end
+    return false
 end
-
 
 RegisterNetEvent('ARMA:Withdraw')
 AddEventHandler('ARMA:Withdraw', function(amount)
     local source = source
     amount = parseInt(amount)
-    --Onesync allows extra security behind events should enable it if it's not already.
     local ped = GetPlayerPed(source)
     local playerCoords = GetEntityCoords(ped)
-    for i, v in pairs(cfg.atms) do
-        local coords = vec3(v[1], v[2], v[3])
-        if #(playerCoords - coords) <= 5.0 then
-            if amount > 0 then
-                local user_id = ARMA.getUserId(source)
-                if user_id ~= nil then
-                    if ARMA.tryWithdraw(user_id, amount) then
-                        ARMAclient.notify(source, {lang.atm.withdraw.withdrawn({amount})})
-                    else
-                        ARMAclient.notify(source, {lang.atm.withdraw.not_enough()})
-                    end
+    if coordsCheck(playerCoords) then
+        if amount > 0 then
+            local user_id = ARMA.getUserId(source)
+            if user_id ~= nil then
+                if ARMA.tryWithdraw(user_id, amount) then
+                    ARMAclient.notify(source, {lang.atm.withdraw.withdrawn({amount})})
+                else
+                    ARMAclient.notify(source, {lang.atm.withdraw.not_enough()})
                 end
-            else
-                ARMAclient.notify(source, {lang.common.invalid_value()})
             end
+        else
+            ARMAclient.notify(source, {lang.common.invalid_value()})
         end
+    else
+        TriggerEvent("ARMA:acBan", user_id, 11, GetPlayerName(source), source, 'Trigger Atm Function outside radius')
     end
 end)
 
@@ -62,25 +75,23 @@ RegisterNetEvent('ARMA:Deposit')
 AddEventHandler('ARMA:Deposit', function(amount)
     local source = source
     amount = parseInt(amount)
-    --Onesync allows extra security behind events should enable it if it's not already.
     local ped = GetPlayerPed(source)
     local playerCoords = GetEntityCoords(ped)
-    for i, v in pairs(cfg.atms) do
-        local coords = vec3(v[1], v[2], v[3])
-        if #(playerCoords - coords) <= 5.0 then
-            if amount > 0 then
-                local user_id = ARMA.getUserId(source)
-                if user_id ~= nil then
-                    if ARMA.tryDeposit(user_id, amount) then
-                        ARMAclient.notify(source, {lang.atm.deposit.deposited({amount})})
-                    else
-                        ARMAclient.notify(source, {lang.money.not_enough()})
-                    end
+    if coordsCheck(playerCoords) then
+        if amount > 0 then
+            local user_id = ARMA.getUserId(source)
+            if user_id ~= nil then
+                if ARMA.tryDeposit(user_id, amount) then
+                    ARMAclient.notify(source, {lang.atm.deposit.deposited({amount})})
+                else
+                    ARMAclient.notify(source, {lang.money.not_enough()})
                 end
-            else
-                ARMAclient.notify(source, {lang.common.invalid_value()})
             end
+        else
+            ARMAclient.notify(source, {lang.common.invalid_value()})
         end
+    else
+        TriggerEvent("ARMA:acBan", user_id, 11, GetPlayerName(source), source, 'Trigger Atm Function outside radius')
     end
 end)
 
@@ -89,24 +100,23 @@ AddEventHandler('ARMA:WithdrawAll', function()
     local source = source
     local userid = ARMA.getUserId(source)
     local amount = ARMA.getBankMoney(userid)
-    if onesync ~= "off" then    
-        --Onesync allows extra security behind events should enable it if it's not already.
-        local ped = GetPlayerPed(source)
-        local playerCoords = GetEntityCoords(ped)
-        for i, v in pairs(cfg.atms) do
-            local coords = vec3(v[1], v[2], v[3])
-            if #(playerCoords - coords) <= 5.0 then
-            
-                local user_id = ARMA.getUserId(source)
-                if user_id ~= nil then
-                    if ARMA.tryWithdraw(user_id, amount) then
-                        ARMAclient.notify(source, {lang.atm.withdraw.withdrawn({amount})})
-                    else
-                        ARMAclient.notify(source, {lang.atm.withdraw.not_enough()})
-                    end
+    local ped = GetPlayerPed(source)
+    local playerCoords = GetEntityCoords(ped)
+    if coordsCheck(playerCoords) then
+        if amount > 0 then
+            local user_id = ARMA.getUserId(source)
+            if user_id ~= nil then
+                if ARMA.tryWithdraw(user_id, amount) then
+                    ARMAclient.notify(source, {lang.atm.withdraw.withdrawn({amount})})
+                else
+                    ARMAclient.notify(source, {lang.atm.withdraw.not_enough()})
                 end
             end
+        else
+            ARMAclient.notify(source, {lang.common.invalid_value()})
         end
+    else
+        TriggerEvent("ARMA:acBan", user_id, 11, GetPlayerName(source), source, 'Trigger Atm Function outside radius')
     end
 end)
 
@@ -116,85 +126,22 @@ AddEventHandler('ARMA:DepositAll', function()
     local source = source
     local userid = ARMA.getUserId(source)
     local amount = ARMA.getMoney(userid)
-    --Onesync allows extra security behind events should enable it if it's not already.
     local ped = GetPlayerPed(source)
     local playerCoords = GetEntityCoords(ped)
-    for i, v in pairs(cfg.atms) do
-        local coords = vec3(v[1], v[2], v[3])
-        if #(playerCoords - coords) <= 5.0 then
-            if amount > 0 then
-                local user_id = ARMA.getUserId(source)
-                if user_id ~= nil then
-                    if ARMA.tryDeposit(user_id, amount) then
-                        ARMAclient.notify(source, {lang.atm.deposit.deposited({amount})})
-                    else
-                        ARMAclient.notify(source, {lang.money.not_enough()})
-                    end
-                end
-            else
-                ARMAclient.notify(source, {lang.common.invalid_value()})
-            end
-        end
-    end
-end)
-
-local function atm_choice_withdraw(player, choice)
-    play_atm_enter(player)
-
-    ARMA.prompt(source, lang.atm.withdraw.prompt(), "", function(player, v)
-        play_atm_exit(player) -- anim
-
-        v = parseInt(v)
-
-        if v > 0 then
+    if coordsCheck(playerCoords) then
+        if amount > 0 then
             local user_id = ARMA.getUserId(source)
             if user_id ~= nil then
-                if ARMA.tryWithdraw(user_id, v) then
-                    ARMAclient.notify(source, {lang.atm.withdraw.withdrawn({v})})
+                if ARMA.tryDeposit(user_id, amount) then
+                    ARMAclient.notify(source, {lang.atm.deposit.deposited({amount})})
                 else
-                    ARMAclient.notify(source, {lang.atm.withdraw.not_enough()})
+                    ARMAclient.notify(source, {lang.money.not_enough()})
                 end
             end
         else
             ARMAclient.notify(source, {lang.common.invalid_value()})
         end
-    end)
-end
-
-local atm_menu = {
-    name = lang.atm.title(),
-    css = {
-        top = "75px",
-        header_color = "rgba(0,255,125,0.75)"
-    }
-}
-
-atm_menu[lang.atm.deposit.title()] = {atm_choice_deposit, lang.atm.deposit.description()}
-atm_menu[lang.atm.withdraw.title()] = {atm_choice_withdraw, lang.atm.withdraw.description()}
-
-local function atm_enter()
-    local user_id = ARMA.getUserId(source)
-    if user_id ~= nil then
-        atm_menu[lang.atm.info.title()] = {function()
-        end, lang.atm.info.bank({ARMA.getBankMoney(user_id)})}
-        ARMA.openMenu(source, atm_menu)
+    else
+        TriggerEvent("ARMA:acBan", user_id, 11, GetPlayerName(source), source, 'Trigger Atm Function outside radius')
     end
-end
-
-local function atm_leave()
-    ARMA.closeMenu(source)
-end
-
-local function build_client_atms(source)
-    local user_id = ARMA.getUserId(source)
-    if user_id ~= nil then
-        for k, v in pairs(atms) do
-            local x, y, z = table.unpack(v)
-
-            ARMAclient.addBlip(source, {x, y, z, 108, 4, lang.atm.title()})
-            ARMAclient.addMarker(source, {x, y, z - 1, 0.7, 0.7, 0.5, 0, 255, 125, 125, 150})
-
-            ARMA.setArea(source, "ARMA:atm" .. k, x, y, z, 1, 1.5, atm_enter, atm_leave)
-        end
-    end
-end
+end)
