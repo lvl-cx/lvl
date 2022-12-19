27262b73ff3eaccf4710005ae8e35f09d54d7768
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-19 05:31:25 +0000

    add(loginrewards): start of rewrite

---
+++ arma/cfg/cfg_loginrewards.lua
@@ -0,0 +1,18 @@
+local cfg = {}
+
+cfg.rewards = {
+    {day = 1, amount = 10000},
+    {day = 2, amount = 20000},
+    {day = 3, amount = 30000},
+    {day = 4, amount = 40000},
+    {day = 5, amount = 50000},
+    {day = 6, amount = 60000},
+    {day = 7, amount = 70000},
+    {day = 8, amount = 80000},
+    {day = 9, amount = 90000},
+    {day = 10, amount = 100000},
+    {day = 60, amount = 150000},
+    {day = 120, amount = 250000}
+}
+
+return cfg
---
+++ arma/modules/sv_loginrewards.lua
@@ -0,0 +1,5 @@
+-- when player spawns if first spawn
+-- send this ARMA:setDailyRewardInfo with parameters (streak, )
+
+-- allow player to trigger ARMA:claimNextLoginReward and it will 
+-- give them the next reward they have due
