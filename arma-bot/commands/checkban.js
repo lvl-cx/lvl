defd58878ce5367752ec88f29f06b531bb74d925
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-20 19:03:41 +0000

    tweak(bot): update permanent duration !checkban

--- arma-bot/commands/checkban.js
+++ arma-bot/commands/checkban.js
@@ -13,9 +13,13 @@ exports.runcmd = (fivemexports, client, message, params) => {
                 if (baninfo == null) {
                     baninfo = "No ban info provided"
                 }
+                var banExpires = new Date(result[0].bantime * 1000)
+                if (banExpires == "Invalid Date") {
+                    banExpires = "Never"
+                }
                 var embed = {
                     "title": `Checking Ban Status for ${result[0].username}`,
-                    "description": `Ban Status: **${result[0].banned}**\nPlayer Name: **${result[0].username}**\nPlayer PermID: **${result[0].id}**\nBan Reason: **${result[0].banreason}**\nBan Expires: **${new Date(result[0].bantime * 1000)}**\nBan Admin: **${result[0].banadmin}**\nBan Info: **${result[0].baninfo}**`,
+                    "description": `Ban Status: **${result[0].banned}**\nPlayer Name: **${result[0].username}**\nPlayer PermID: **${result[0].id}**\nBan Reason: **${result[0].banreason}**\nBan Expires: **${banExpires}**\nBan Admin: **${result[0].banadmin}**\nBan Info: **${result[0].baninfo}**`,
                     "color": settingsjson.settings.botColour,
                     "footer": {
                         "text": ""
