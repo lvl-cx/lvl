cbd0ccdf15a6c244e6d3a4e6c1897cd2719c108e
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-21 06:04:27 +0000

    remove unused inventory funcs

--- arma/cfg/inventory.lua
+++ arma/cfg/inventory.lua
@@ -5,14 +5,6 @@ local cfg = {}
 cfg.default_vehicle_chest_weight = 30
 
 cfg.vehicle_chest_weights = {
-    ["300kg"] = 200,
-    ["m977hl"] = 200,
-    ["m9395"] = 200,
-    ["mtfft"] = 200,
-    ["MH6"] = 150,
-    ["al18"] = 150,
-    ["sjtoyota"] = 50,
-    ["sjtoyota"] = 50,
     ["m977l"] = 200,
     ["m977t"] = 200,
     ["Brickade"] = 100,
--- arma/modules/inventory.lua
+++ arma/modules/inventory.lua
@@ -5,15 +5,6 @@ local cfg = module("cfg/inventory")
 
 ARMA.items = {}
 
--- define an inventory item (call this at server start) (parametric or plain text data)
--- idname: unique item name
--- name: display name or genfunction
--- description: item description (html) or genfunction
--- choices: menudata choices (see gui api) only as genfunction or nil
--- weight: weight or genfunction
---
--- genfunction are functions returning a correct value as: function(args) return value end
--- where args is a list of {base_idname,arg,arg,arg,...}
 function ARMA.defInventoryItem(idname,name,description,choices,weight)
   if weight == nil then
     weight = 0
@@ -279,6 +270,8 @@ function ARMA.RunInventoryTask(source, itemName)
         choices['Eat'][1](source)
     elseif choices['Equip'] then 
         choices['Equip'][1](source)
+    elseif choices['Take'] then 
+        choices['Take'][1](source)
     end
     TriggerEvent('ARMA:RefreshInventory', source)
 end
@@ -373,254 +366,13 @@ function ARMA.clearInventory(user_id)
   end
 end
 
--- INVENTORY MENU
 
--- open player inventory
-function ARMA.openInventory(source)
-  local user_id = ARMA.getUserId(source)
-
-  if user_id ~= nil then
-    local data = ARMA.getUserDataTable(user_id)
-    if data then
-      -- build inventory menu
-      local menudata = {name=lang.inventory.title(),css={top="75px",header_color="rgba(0,125,255,0.75)"}}
-      -- add inventory info
-      local weight = ARMA.getInventoryWeight(user_id)
-      local max_weight = ARMA.getInventoryMaxWeight(user_id)
-      local hue = math.floor(math.max(125*(1-weight/max_weight), 0))
-      menudata["<div class=\"dprogressbar\" data-value=\""..string.format("%.2f",weight/max_weight).."\" data-color=\"hsl("..hue..",100%,50%)\" data-bgcolor=\"hsl("..hue..",100%,25%)\" style=\"height: 12px; border: 3px solid black;\"></div>"] = {function()end, lang.inventory.info_weight({string.format("%.2f",weight),max_weight})}
-      local kitems = {}
-
-      -- choose callback, nested menu, create the item menu
-      local choose = function(player,choice)
-        if string.sub(choice,1,1) ~= "@" then -- ignore info choices
-        local choices = ARMA.getItemChoices(kitems[choice])
-          -- build item menu
-          local submenudata = {name=choice,css={top="75px",header_color="rgba(0,125,255,0.75)"}}
-
-          -- add computed choices
-          for k,v in pairs(choices) do
-            submenudata[k] = v
-          end
-
-          -- nest menu
-          submenudata.onclose = function()
-            ARMA.openInventory(source) -- reopen inventory when submenu closed
-          end
-
-          -- open menu
-          ARMA.openMenu(source,submenudata)
-        end
-      end
-
-      -- add each item to the menu
-      for k,v in pairs(data.inventory) do 
-        local name,description,weight = ARMA.getItemDefinition(k)
-        if name ~= nil then
-          kitems[name] = k -- reference item by display name
-          menudata[name] = {choose,lang.inventory.iteminfo({v.amount,description,string.format("%.2f",weight)})}
-        end
-      end
-
-      -- open menu
-      ARMA.openMenu(source,menudata)
-    end
-  end
-end
-
--- init inventory
 AddEventHandler("ARMA:playerJoin", function(user_id,source,name,last_login)
   local data = ARMA.getUserDataTable(user_id)
   if data.inventory == nil then
     data.inventory = {}
   end
 end)
-
-
--- add open inventory to main menu
-local choices = {}
-choices[lang.inventory.title()] = {function(player, choice) ARMA.openInventory(player) end, lang.inventory.description()}
-
-ARMA.registerMenuBuilder("main", function(add, data)
-  add(choices)
-end)
-
--- CHEST SYSTEM
-
-local chests = {}
-
--- build a menu from a list of items and bind a callback(idname)
-local function build_itemlist_menu(name, items, cb)
-  local menu = {name=name, css={top="75px",header_color="rgba(0,255,125,0.75)"}}
-
-  local kitems = {}
-
-  -- choice callback
-  local choose = function(player,choice)
-    local idname = kitems[choice]
-    if idname then
-      cb(idname)
-    end
-  end
-
-  -- add each item to the menu
-  for k,v in pairs(items) do 
-    local name,description,weight = ARMA.getItemDefinition(k)
-    if name ~= nil then
-      kitems[name] = k -- reference item by display name
-      menu[name] = {choose,lang.inventory.iteminfo({v.amount,description,string.format("%.2f", weight)})}
-    end
-  end
-
-  return menu
-end
-
--- open a chest by name
--- cb_close(): called when the chest is closed (optional)
--- cb_in(idname, amount): called when an item is added (optional)
--- cb_out(idname, amount): called when an item is taken (optional)
-function ARMA.openChest(source, name, max_weight, cb_close, cb_in, cb_out)
-  local user_id = ARMA.getUserId(source)
-  if user_id ~= nil then
-    local data = ARMA.getUserDataTable(user_id)
-    if data.inventory ~= nil then
-      if not chests[name] then
-        local close_count = 0 -- used to know when the chest is closed (unlocked)
-
-        -- load chest
-        local chest = {max_weight = max_weight}
-        chests[name] = chest 
-        ARMA.getSData("chest:"..name, function(cdata)
-          chest.items = json.decode(cdata) or {} -- load items
-
-          -- open menu
-          local menu = {name=lang.inventory.chest.title(), css={top="75px",header_color="rgba(0,255,125,0.75)"}}
-          -- take
-          local cb_take = function(idname)
-            local citem = chest.items[idname]
-            ARMA.prompt(source, lang.inventory.chest.take.prompt({citem.amount}), "", function(player, amount)
-              amount = parseInt(amount)
-              if amount >= 0 and amount <= citem.amount then
-                -- take item
-                
-                -- weight check
-                local new_weight = ARMA.getInventoryWeight(user_id)+ARMA.getItemWeight(idname)*amount
-                if new_weight <= ARMA.getInventoryMaxWeight(user_id) then
-                  ARMA.giveInventoryItem(user_id, idname, amount, true)
-                  citem.amount = citem.amount-amount
-
-                  if citem.amount <= 0 then
-                    chest.items[idname] = nil -- remove item entry
-                  end
-
-                  if cb_out then cb_out(idname,amount) end
-
-                  -- actualize by closing
-                  ARMA.closeMenu(player)
-                else
-                  ARMAclient.notify(source,{lang.inventory.full()})
-                end
-              else
-                ARMAclient.notify(source,{lang.common.invalid_value()})
-              end
-            end)
-          end
-
-          local ch_take = function(player, choice)
-            local submenu = build_itemlist_menu(lang.inventory.chest.take.title(), chest.items, cb_take)
-            -- add weight info
-            local weight = ARMA.computeItemsWeight(chest.items)
-            local hue = math.floor(math.max(125*(1-weight/max_weight), 0))
-            submenu["<div class=\"dprogressbar\" data-value=\""..string.format("%.2f",weight/max_weight).."\" data-color=\"hsl("..hue..",100%,50%)\" data-bgcolor=\"hsl("..hue..",100%,25%)\" style=\"height: 12px; border: 3px solid black;\"></div>"] = {function()end, lang.inventory.info_weight({string.format("%.2f",weight),max_weight})}
-
-
-            submenu.onclose = function()
-              close_count = close_count-1
-              ARMA.openMenu(player, menu)
-            end
-            close_count = close_count+1
-            ARMA.openMenu(player, submenu)
-          end
-
-
-          -- put
-          local cb_put = function(idname)
-            ARMA.prompt(source, lang.inventory.chest.put.prompt({ARMA.getInventoryItemAmount(user_id, idname)}), "", function(player, amount)
-              amount = parseInt(amount)
-
-              -- weight check
-              local new_weight = ARMA.computeItemsWeight(chest.items)+ARMA.getItemWeight(idname)*amount
-              if new_weight <= max_weight then
-                if amount >= 0 and ARMA.tryGetInventoryItem(user_id, idname, amount, true) then
-                  local citem = chest.items[idname]
-
-                  if citem ~= nil then
-                    citem.amount = citem.amount+amount
-                  else -- create item entry
-                    chest.items[idname] = {amount=amount}
-                  end
-
-                  -- callback
-                  if cb_in then cb_in(idname,amount) end
-
-                  -- actualize by closing
-                  ARMA.closeMenu(player)
-                end
-              else
-                ARMAclient.notify(source,{lang.inventory.chest.full()})
-              end
-            end)
-          end
-
-          local ch_put = function(player, choice)
-            local submenu = build_itemlist_menu(lang.inventory.chest.put.title(), data.inventory, cb_put)
-            -- add weight info
-            local weight = ARMA.computeItemsWeight(data.inventory)
-            local max_weight = ARMA.getInventoryMaxWeight(user_id)
-            local hue = math.floor(math.max(125*(1-weight/max_weight), 0))
-            submenu["<div class=\"dprogressbar\" data-value=\""..string.format("%.2f",weight/max_weight).."\" data-color=\"hsl("..hue..",100%,50%)\" data-bgcolor=\"hsl("..hue..",100%,25%)\" style=\"height: 12px; border: 3px solid black;\"></div>"] = {function()end, lang.inventory.info_weight({string.format("%.2f",weight),max_weight})}
-
-            submenu.onclose = function() 
-              close_count = close_count-1
-              ARMA.openMenu(player, menu) 
-            end
-            close_count = close_count+1
-            ARMA.openMenu(player, submenu)
-          end
-
-
-          -- choices
-          menu[lang.inventory.chest.take.title()] = {ch_take}
-          menu[lang.inventory.chest.put.title()] = {ch_put}
-
-          menu.onclose = function()
-            if close_count == 0 then -- close chest
-              -- save chest items
-              ARMA.setSData("chest:"..name, json.encode(chest.items))
-              chests[name] = nil
-              if cb_close then cb_close() end -- close callback
-            end
-          end
-
-          -- Ugly patch to close the "already opened" chest. 
-			    SetTimeout(300000, function()
-            if not close_count == 0 then
-			        close_count = 0
-              ARMA.setSData("chest:"..name, json.encode(chest.items))
-              chests[name] = nil
-			      end
-          end)
-          -- Ugly patch to close the "already opened" chest.
-
-          -- open menu
-          ARMA.openMenu(source, menu)
-        end)
-      else
-        ARMAclient.notify(source,{lang.inventory.chest.already_opened()})
-      end
-    end
-  end
-end
 
 RegisterCommand("storebackpack", function(source, args)
   local source = source
