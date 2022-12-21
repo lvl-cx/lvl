02a15eaf448d6cd286e4c78015320afb10b44792
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-21 06:05:36 +0000

    fix(ui2):

--- ARMAUI2/cl_moneyui.lua
+++ ARMAUI2/cl_moneyui.lua
@@ -7,7 +7,6 @@ proximityIdToString = {
     [2] = "Talking",
     [3] = "Shouting",
 }
-prox = 35.01
 
 local ARMA = {}
 
@@ -48,33 +47,27 @@ function ARMA.getCachedMinimapAnchor()
     return cachedMinimapAnchor
 end
 
-function setProximity(vprox)
-    if vprox == 1 then
-        prox = 5.01
-    elseif vprox == 2 then
-        prox = 15.01
-    elseif vprox == 3 then
-        prox = 35.01
-    end
-end
-
-TriggerServerEvent('ARMA:requestPlayerBankBalance')
-
 function getMoneyStringFormatted(cashString)
 	local i, j, minus, int, fraction = tostring(cashString):find('([-]?)(%d+)([.]?%d*)')
 	int = int:reverse():gsub("(%d%d%d)", "%1,")
 	return minus .. int:reverse():gsub("^,", "") .. fraction 
 end
 
-function updateHungerThirstHUD(cash, bank, proximity, topLeftAnchor,yAnchor)
-  SendNUIMessage({
-    update = true,
-    cash = cash,
-    bank = bank,
-    proximity = proximityIdToString[proximity],
-    topLeftAnchor = topLeftAnchor,
-    yAnchor = yAnchor,
-  })
+AddEventHandler('pma-voice:setTalkingMode', function(newTalkingRange)
+    voiceChatProximity = newTalkingRange
+    local topLeftAnchor = ARMA.getCachedMinimapAnchor()
+    updateMoneyUI("£" .. moneyDisplay, "£" .. bankMoneyDisplay,voiceChatProximity,topLeftAnchor.rightX * topLeftAnchor.resX)
+end)
+
+function updateMoneyUI(cash, bank, proximity, topLeftAnchor,yAnchor)
+    SendNUIMessage({
+        update = true,
+        cash = cash,
+        bank = bank,
+        proximity = proximityIdToString[proximity],
+        topLeftAnchor = topLeftAnchor,
+        yAnchor = yAnchor,
+    })
 end
 
 RegisterNetEvent("ARMAHUD:show")
@@ -82,7 +75,6 @@ AddEventHandler("ARMAHUD:show", function()
     SendNUIMessage({
         showhud = true
     })
-    DisplayRadar(true)
     TriggerEvent('ARMA:showHUD', true)
 end)
 
@@ -91,7 +83,6 @@ AddEventHandler("ARMAHUD:hide", function()
     SendNUIMessage({
         showhud = false
     })
-    DisplayRadar(false)
     TriggerEvent('ARMA:showHUD', false)
 end)
 
@@ -99,12 +90,16 @@ RegisterNetEvent("ARMA:setDisplayMoney")
 AddEventHandler("ARMA:setDisplayMoney",function(value)
 	local moneyString = tostring(math.floor(value))
 	moneyDisplay = getMoneyStringFormatted(moneyString)
+    local topLeftAnchor = ARMA.getCachedMinimapAnchor()
+    updateMoneyUI("£" .. moneyDisplay, "£" .. bankMoneyDisplay,voiceChatProximity,topLeftAnchor.rightX * topLeftAnchor.resX)
 end)
 
 RegisterNetEvent("ARMA:setDisplayBankMoney")
 AddEventHandler("ARMA:setDisplayBankMoney",function(value)
 	local moneyString = tostring(math.floor(value))
 	bankMoneyDisplay = getMoneyStringFormatted(moneyString)
+    local topLeftAnchor = ARMA.getCachedMinimapAnchor()
+    updateMoneyUI("£" .. moneyDisplay, "£" .. bankMoneyDisplay,voiceChatProximity,topLeftAnchor.rightX * topLeftAnchor.resX)
 end)
 
 RegisterNetEvent("ARMA:initMoney")
@@ -116,51 +111,34 @@ AddEventHandler("ARMA:initMoney",function(cash,bank)
     bankMoneyDisplay = getMoneyStringFormatted(moneyString)
 
     local topLeftAnchor = ARMA.getCachedMinimapAnchor()
-    updateHungerThirstHUD("£" .. moneyDisplay, "£" .. bankMoneyDisplay,voiceChatProximity,topLeftAnchor.rightX * topLeftAnchor.resX)
+    updateMoneyUI("£" .. moneyDisplay, "£" .. bankMoneyDisplay,voiceChatProximity,topLeftAnchor.rightX * topLeftAnchor.resX)
 end)
 
 Citizen.CreateThread(function()
+    TriggerServerEvent('ARMA:requestPlayerBankBalance')
+    local talking = false
     while true do
         local res_x, res_y = GetActiveScreenResolution()
         if res_x ~= cachedXRes or res_y ~= cachedYRes then
             cachedXRes, cachedYRes = GetActiveScreenResolution()
             cachedMinimapAnchor = GetMinimapAnchor()
-            updateHungerThirstHUD("£" .. moneyDisplay, "£" .. bankMoneyDisplay,voiceChatProximity,cachedMinimapAnchor.rightX * cachedMinimapAnchor.resX)
+            updateMoneyUI("£" .. moneyDisplay, "£" .. bankMoneyDisplay,voiceChatProximity,cachedMinimapAnchor.rightX * cachedMinimapAnchor.resX)
         end
-        NetworkSetTalkerProximity(prox)
-        NetworkSetVoiceActive(true)
-        HideHudComponentThisFrame(9)
-        HideHudComponentThisFrame(7)
         if NetworkIsPlayerTalking(PlayerId()) then
-            SendNUIMessage({
+            if not talking then
                 talking = true
-            })
+                SendNUIMessage({
+                    talking = true
+                })
+            end
         else
-            SendNUIMessage({
+            if talking then
                 talking = false
-            })
+                SendNUIMessage({
+                    talking = false
+                })
+            end
         end
 		Wait(0)
     end
-end)
-
-Citizen.CreateThread(function()
-	voiceChatProximity = 2
-	while true do
-		if IsControlPressed(0, 10) then
-			if voiceChatProximity ~= 3 then
-				voiceChatProximity = voiceChatProximity + 1
-				setProximity(voiceChatProximity)
-				Wait(250)
-			end
-		end
-		if IsControlPressed(0, 11) then
-			if voiceChatProximity ~= 1 then
-				voiceChatProximity = voiceChatProximity - 1
-				setProximity(voiceChatProximity)
-				Wait(250)
-			end
-		end
-		Wait(0)
-	end	
 end)
