16dd87fa95ad751d3cfc5ad8ef5c73bf09098886
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-24 04:14:02 +0000

    tweak(discordroles): fix nhs groups in cfg

--- arma/cfg/discordroles.lua
+++ arma/cfg/discordroles.lua
@@ -31,37 +31,31 @@ cfg.Guild_Roles = {
 		['Police Constable'] = 991815812312739950,
 		['PCSO'] = 991815812967051344,
 		['Large Arms Access'] = 991813471475859538,
-		-- ['Drone Trained'] = 1007419420731265034, (need discord group made)
+		--['Drone Trained'] = 1007419420731265034, (need discord group made)
 	}
 
-	-- none of these groups are finished
-
 	-- ['NHS'] = {
-	-- 	['Chief Medical Director'] = 1013072143208157234,
-	-- 	['Deputy Medical Director'] = 1013072231695388743,
-	-- 	['Assistant Medical Director'] = 1013072284061270127,
-	-- 	['Captain'] = 1017618355047383141,
-	-- 	['Specialist'] = 1017618411284615260,
-	-- 	['Medic of the week'] = 1019584557785301003,
-	-- 	['Senior Doctor'] = 1017618467731554364,
-	-- 	['Doctor'] = 1017618570940780576,
-	-- 	['Junior Doctor'] = 1017618604541362277,
-	-- 	['Critical Care Paramedic'] = 1017618670559698995,
-	-- 	['Paramedic'] = 1017618728348811306,
-	-- 	['Trainee Paramedic'] = 1017618794333614140,
-	-- 	['NHS Needs Training'] = 1017863422748151860,
-	-- 	['NHS Suspended'] = 1017991729971994725,
+	-- 	['NHS Head Chief'] = 1013072143208157234,
+	-- 	['NHS Assistant Chief'] = 1013072231695388743,
+	-- 	['NHS Deputy Chief'] = 1013072284061270127,
+	-- 	['NHS Captain'] = 1017618355047383141,
+	-- 	['NHS Specialist'] = 1017618411284615260,
+	-- 	['NHS Senior Doctor'] = 1017618467731554364,
+	-- 	['NHS Doctor'] = 1017618570940780576,
+	-- 	['NHS Junior Doctor'] = 1017618604541362277,
+	-- 	['NHS Critical Care Paramedic'] = 1017618670559698995,
+	-- 	['NHS Paramedic'] = 1017618728348811306,
+	-- 	['NHS Trainee Paramedic'] = 1017618794333614140,
 	-- 	['Drone Trained'] = 1022642333696675840,
 	-- },
-	
+
 	-- ['HMP'] = {
 	-- 	['Governor'] = 1017619823804547113,
 	-- 	['Deputy Governor'] = 1017619884177358882,
-	-- 	['Assistant Governor'] = 1017619931526877246,
+	-- 	['Divisional Commander'] = 1017619931526877246,
 	-- 	['Custodial Supervisor'] = 1017619970349334590,
 	-- 	['Custodial Officer'] = 1017620004373544992,
 	-- 	['Honourable Guard'] = 1017620044798251148,
-	-- 	['Guard of the week'] = 1019584898396327946,
 	-- 	['Supervising Officer'] = 1017620085654958080,
 	-- 	['Principal Officer'] = 1017620118433431653,
 	-- 	['Specialist Officer'] = 1017620166990888960,
@@ -75,7 +69,6 @@ cfg.Guild_Roles = {
 	-- 	['Divisional Command'] = 1017621512255197254,
 	-- 	['Sector Command'] = 1017621546212261888,
 	-- 	['Honourable Firefighter'] = 1017621584850190417,
-	-- 	['Firefighter of the week'] = 1019584345108918393,
 	-- 	['Leading Firefighter'] = 1017621616416538677,
 	-- 	['Specialist Firefighter'] = 1017621659332644865,
 	-- 	['Advanced Firefighter'] = 1017621694740959233,
