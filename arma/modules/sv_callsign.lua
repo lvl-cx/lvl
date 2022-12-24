dbe25bbea1c0f6c43a9f4af0d57dcef7f0df5c74
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-24 07:25:07 +0000

    tweak(callsign): actually get faction callsign for display

--- arma/modules/sv_callsign.lua
+++ arma/modules/sv_callsign.lua
@@ -1,12 +1,30 @@
+local function getCallsign(guildType, source, user_id, type)
+    local discord_id = exports['arma']:Get_Client_Discord_ID(source)
+    if discord_id then
+        local guilds_info = exports['arma']:Get_Guilds()
+        for guild_name, guild_id in pairs(guilds_info) do
+            if guild_name == guildType then
+                local nick_name = exports['arma']:Get_Guild_Nickname(guild_id, discord_id)
+                if nick_name then
+                    local open_bracket = string.find(nick_name, '[', nil, true) -- Extra Params to toggle pattern matching
+                    local closed_bracket = string.find(nick_name, ']', nil, true) -- Extra Params to toggle pattern matching
+                    if open_bracket and closed_bracket then
+                        local callsign_value = string.sub(nick_name, open_bracket + 1, closed_bracket - 1)
+                        return callsign_value, string.gsub(getGroupInGroups(user_id, type), ' Clocked', ''), GetPlayerName(source)
+                    end
+                end
+            end
+        end
+    end
+end
+
 RegisterServerEvent("ARMA:getCallsign")
 AddEventHandler("ARMA:getCallsign", function(type)
     local source = source
     local user_id = ARMA.getUserId(source)
     if type == 'police' and ARMA.hasPermission(user_id, 'police.onduty.permission') then
-        -- add callsign, rank and name check from google sheets
-        TriggerClientEvent("ARMA:receivePoliceCallsign", source, 'GC-1', 'Comissioner', 'cnr')
+        TriggerClientEvent("ARMA:receivePoliceCallsign", source, getCallsign('MPD', source, user_id, 'police'))
     elseif type == 'prison' and ARMA.hasPermission(user_id, 'prisonguard.onduty.permission') then
-        -- add callsign, rank and name check from google sheets
-        TriggerClientEvent("ARMA:receivePoliceCallsign", source, 'PA-1', 'Governor', 'cnr')
+        TriggerClientEvent("ARMA:receiveHmpCallsign", source,getCallsign('HMP', source, user_id, 'hmp'))
     end
 end)
