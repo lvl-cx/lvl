eeed13b5f7608d256d6cf3dae1a575969abe53e2
cnr <34693611+terrorist@users.noreply.github.com>
2022-12-27 23:08:59 +0000

    tweak(gunstores): redo the rest of the menus

--- arma/cfg/cfg_gunstores.lua
+++ arma/cfg/cfg_gunstores.lua
@@ -5,82 +5,57 @@ local cfg = {}
 cfg.GunStores={
     ["policeLargeArms"]={
         ["_config"]={{vector3(1840.6104736328,3691.4741210938,33.350730895996),vector3(461.43179321289,-982.66412353516,29.689668655396),vector3(-449.9557800293,6016.5454101563,31.716398239136),vector3(-1102.5059814453,-820.62091064453,13.282785415649)},110,5,"MET Police Large Arms",{"police.loadshop2"},false,true}, 
-        ["WEAPON_SMOKEGRENADE"]={"Smoke/Tear Gas",0,0,"N/A","w_me_smokegrenade"},
         ["WEAPON_FLASHBANG"]={"Flashbang",0,0,"N/A","w_me_flashbang"},
-        --["WEAPON_G36"]={"G36C",0,0,"N/A","w_ar_g36"},
+        ["WEAPON_G36"]={"G36C",0,0,"N/A","w_ar_g36"}, -- need model
+        ["WEAPON_M4CARBINE"]={"M4 Carbine",0,0,"N/A","w_ar_g36"}, -- need model
         ["WEAPON_MP5"]={"MP5",0,0,"N/A","w_sb_mp5"},
-        --["WEAPON_MSR2"]={"MSR",0,0,"N/A","w_sr_msr2"},
-        --["WEAPON_SIGMCX"]={"SigMCX",0,0,"N/A","w_ar_sigmcx"},
+        ["WEAPON_REMINGTON700"]={"Remington 700",0,0,"N/A","w_sb_mp5"}, -- need model
+        ["WEAPON_SIGMCX"]={"SigMCX",0,0,"N/A","w_ar_sigmcx"},
+        -- smoke grenade
         ["WEAPON_SPAR17"]={"SPAR17",0,0,"N/A","w_ar_spar17"},
-        ["WEAPON_PDM4A1"]={"M4A1",0,0,"N/A","w_ar_m4a1"},
-        --["WEAPON_STING"]={"Sting 9mm",0,0,"N/A","w_sb_sting"},
-        --["WEAPON_STAC"]={"Stac 33",0,0,"N/A","w_sr_stac"},
+        ["WEAPON_STING"]={"Sting 9mm",0,0,"N/A","w_sb_sting"},
     },
     ["policeSmallArms"]={
         ["_config"]={{vector3(461.53082275391,-979.35876464844,29.689668655396),vector3(1842.9096679688,3690.7692871094,33.267082214355),vector3(-448.93994140625,6015.4150390625,31.716398239136),vector3(-1104.5264892578,-821.70153808594,13.282785415649)},110,5,"MET Police Small Arms",{"police.onduty.permission"},false,true},
-        ["WEAPON_FLARE"]={"Flare",0,0,"N/A","w_am_flare"},
-        ["WEAPON_FLAREGUN"]={"Flare Gun",0,0,"N/A","w_pi_flaregun"},
         ["WEAPON_FLASHLIGHT"]={"Flashlight",0,0,"N/A","w_me_flashlight"},
         ["WEAPON_PDGLOCK"]={"Glock",0,0,"N/A","w_pi_glock"},
-        ["WEAPON_NIGHTSTICK"]={"Nightstick",0,0,"N/A","w_me_nightstick"},
+        ["WEAPON_NIGHTSTICK"]={"Police Baton",0,0,"N/A","w_me_nightstick"},
         ["WEAPON_REMINGTON870"]={"Remington 870",0,0,"N/A","w_sg_remington870"},
         ["WEAPON_STAFFGUN"]={"Speed Gun",0,0,"N/A","w_pi_staffgun"},
         ["WEAPON_STUNGUN"]={"Tazer",0,0,"N/A","w_pi_stungun"},
     },
     ["prisonArmoury"]={
         ["_config"]={{vector3(1779.3741455078,2542.5639648438,45.797782897949)},110,5,"Prison Armoury",{"prisonguard.onduty.permission"},false,true},
-        ["WEAPON_FLARE"]={"Flare",0,0,"N/A","w_am_flare"},
-        ["WEAPON_FLAREGUN"]={"Flare Gun",0,0,"N/A","w_pi_flaregun"},
         ["WEAPON_FLASHLIGHT"]={"Flashlight",0,0,"N/A","w_me_flashlight"},
         ["WEAPON_PDGLOCK"]={"Glock",0,0,"N/A","w_pi_glock"},
-        ["WEAPON_NIGHTSTICK"]={"Nightstick",0,0,"N/A","w_me_nightstick"},
+        ["WEAPON_NIGHTSTICK"]={"Police Baton",0,0,"N/A","w_me_nightstick"},
         ["WEAPON_REMINGTON870"]={"Remington 870",0,0,"N/A","w_sg_remington870"},
         ["WEAPON_STUNGUN"]={"Tazer",0,0,"N/A","w_pi_stungun"},
     },
     ["NHS"]={
         ["_config"]={{vector3(340.41757202148,-582.71209716797,27.973259765625),vector3(-435.27032470703,-318.29010009766,34.08971484375)},110,5,"NHS Armoury",{"nhs.onduty.permission"},false,true},
-        ["WEAPON_FLARE"]={"Flare",0,0,"N/A","w_am_flare"},
-        ["WEAPON_FLAREGUN"]={"Flare Gun",0,0,"N/A","w_pi_flaregun"},
         ["WEAPON_FLASHLIGHT"]={"Flashlight",0,0,"N/A","w_me_flashlight"},
     },
     ["LFB"]={
         ["_config"]={{vector3(1210.193359375,-1484.1494140625,34.241326171875),vector3(216.63296508789,-1648.6680908203,29.0179375)},110,5,"LFB Armoury",{"lfb.onduty.permission"},false,true},
-        ["WEAPON_FLARE"]={"Flare",0,0,"N/A","w_am_flare"},
-        ["WEAPON_FLAREGUN"]={"Flare Gun",0,0,"N/A","w_pi_flaregun"},
         ["WEAPON_FLASHLIGHT"]={"Flashlight",0,0,"N/A","w_me_flashlight"},
-        --["WEAPON_FIREAXE"]={"Fireaxe",0,0,"N/A","w_me_fireaxe"},
+        ["WEAPON_FIREAXE"]={"Fireaxe",0,0,"N/A","w_me_fireaxe"},
     },
     ["VIP"]={
-        ["_config"]={{vector3(-2151.5739746094,5191.2548828125,14.718822479248)},110,5,"VIP Gun Store",{""},true}, -- need to add vip.gunstore back after beta
-        -- ["WEAPON_FORRESTAX"]={"The Forrest Axe",10000,0,"N/A","w_me_forrestax"},
-        -- ["WEAPON_SKULLCLUB"]={"Skull Club",25000,0,"N/A","w_me_skullclub"},
-        -- ["WEAPON_SLEDGESHAMMER"]={"Sledges Hammer",30000,0,"N/A","w_me_sledgeshammer"},
-        -- ["WEAPON_CLIMBINGAXE"]={"Climbing Axe",15000,0,"N/A","w_me_climbingaxe"},
-        -- ["WEAPON_CRAFTEDAXE"]={"Crafted Axe",10000,0,"N/A","w_me_craftedaxe"},
-        ["WEAPON_MOSIN"]={"Mosin Bolt-Action",900000,0,"N/A","w_ar_mosin",900000},
-        ["WEAPON_MK1EMR"]={"MK-1 EMR",800000,0,"N/A","w_ar_mk1emr"},
-        ["WEAPON_MXM"]={"MXM",700000,0,"N/A","w_ar_mxm"},
-        ["WEAPON_SPAR16"]={"SPAR-16",700000,0,"N/A","w_ar_spar16"},
-        ["WEAPON_UMP45"]={"UMP45 SMG",250000,0,"N/A","w_sb_ump45",250000},
+        ["_config"]={{vector3(-2151.5739746094,5191.2548828125,14.718822479248)},110,5,"VIP Gun Store",{"vip.gunstore"},true},
+        ["WEAPON_AKGOLD"]={"Golden AK-47",10000,0,"N/A","w_ar_goldak"}, -- price and model
+        ["WEAPON_AKGOLD"]={"Fire Extinguisher",10000,0,"N/A",""}, -- price and model
+        ["WEAPON_AKGOLD"]={"Mjlonir",10000,0,"N/A",""}, -- price and model
+        ["WEAPON_AKGOLD"]={"Molotov Cocktail",10000,0,"N/A",""}, -- price and model
+        ["WEAPON_AKGOLD"]={"Smoke Grenade",10000,0,"N/A",""}, -- price and model
+        ["WEAPON_SNOWBALL"]={"Snowball",10000,0,"N/A","w_ex_snowball"}, -- price and model
         ["item1"]={"LVL 1 Armour",25000,0,"N/A","prop_armour_pickup"},
         ["item2"]={"LVL 2 Armour",50000,0,"N/A","prop_bodyarmour_02"},
-        ["item3"]={"LVL 3 Armour",75000,0,"N/A","prop_bodyarmour_03"},
-        ["item4"]={"LVL 4 Armour",100000,0,"N/A","prop_bodyarmour_04"},
-        ["item|fillUpArmour"]={"Replenish Armour",100000,0,"N/A","prop_armour_pickup"},
     },
     ["Rebel"]={
         ["_config"]={{vector3(1545.2521972656,6331.5615234375,23.07857131958),vector3(4925.6259765625,-5243.0908203125,1.524599313736)},110,5,"Rebel Gun Store",{"rebellicense.whitelisted"},true},
         ["GADGET_PARACHUTE"]={"Parachute",1000,0,"N/A","p_parachute_s"},
-        --["WEAPON_AK200"]={"AK-200",750000,0,"N/A","w_ar_ak200"},
-        --["WEAPON_MK14"]={"MK14",4000000,0,"N/A","w_sr_mk14"},
-        ["WEAPON_MK1EMR"]={"MK-1 EMR",800000,0,"N/A","w_ar_mk1emr"},
-        ["WEAPON_MXM"]={"MXM",700000,0,"N/A","w_ar_mxm"},
-        ["WEAPON_SPAR16"]={"SPAR-16",700000,0,"N/A","w_ar_spar16"},
-        --["WEAPON_ANARCHY"]={"Anarchy lr300",800000,0,"N/A","w_ar_anarchy"},
-        ["WEAPON_SVD"]={"Dragunov SVD",4000000,0,"N/A","w_sr_svd"},
-        --["WEAPON_AA12"]={"A12 Assault Shotgun",350000,0,"N/A","w_sg_aa12"},
-        --["WEAPON_SPAZ"]={"Spaz 12 Shotgun",350000,0,"N/A","w_sg_spaz"},
-        --["WEAPON_VAL"]={"AS VAL",350000,0,"N/A","w_ar_val"},
+        -- need the ars
         ["item1"]={"LVL 1 Armour",25000,0,"N/A","prop_armour_pickup"},
         ["item2"]={"LVL 2 Armour",50000,0,"N/A","prop_bodyarmour_02"},
         ["item3"]={"LVL 3 Armour",75000,0,"N/A","prop_bodyarmour_03"},
@@ -89,42 +64,29 @@ cfg.GunStores={
     },
     ["LargeArmsDealer"]={
         ["_config"]={{vector3(-1108.3199462891,4934.7392578125,217.35540771484),vector3(5065.6201171875,-4591.3857421875,1.8652405738831)},110,1,"Large Arms Dealer",{"gang.whitelisted"},false},
-        --["WEAPON_AK74KASHNAR"]={"AK-47 Assault Rifle",750000,0,"N/A","w_ar_ak74kashnar",750000},
+        ["WEAPON_AK74KASHNAR"]={"AK-47 Assault Rifle",750000,0,"N/A","w_ar_ak74kashnar",750000},
         ["WEAPON_MOSIN"]={"Mosin Bolt-Action",900000,0,"N/A","w_ar_mosin",900000},
-        --["WEAPON_WINCHESTER12"]={"Winchester Shotgun",300000,0,"N/A","w_sg_winchester12",300000},
-        ["WEAPON_UMP45"]={"UMP45 SMG",250000,0,"N/A","w_sb_ump45",250000},
-        --["WEAPON_MAC11"]={"Mac11 SMG",300000,0,"N/A","w_sb_mac11",300000},
+        ["WEAPON_OLYMPIA"]={"Olympia Shotgun",900000,0,"N/A","w_ar_mosin",900000}, -- model and price
+        ["WEAPON_UMP45"]={"UMP45 SMG",300000,0,"N/A","w_sb_ump45",300000},
+        ["WEAPON_UZI"]={"Uzi SMG",250000,0,"N/A","w_sb_ump45",250000}, -- model and price
         ["item1"]={"LVL 1 Armour",25000,0,"N/A","prop_armour_pickup",25000},
         ["item2"]={"LVL 2 Armour",50000,0,"N/A","prop_bodyarmour_02",50000},
     },
     ["SmallArmsDealer"]={
         ["_config"]={{vector3(2437.5708007813,4966.5610351563,41.34761428833),vector3(-1500.4978027344,-216.72758483887,46.889373779297),vector3(1242.7232666016,-426.84201049805,67.913963317871)},110,1,"Small Arms Dealer",{""},true},
-        ["WEAPON_BERETTA"]={"Berreta M9 Pistol",40000,0,"N/A","w_pi_beretta"},
-        --["WEAPON_FNX"]={"FNX Pistol",50000,"N/A","w_pi_fnx"},
-        --["WEAPON_TX22"]={"TX22 Pistol",60000,0,"N/A","w_pi_tx22"},
-        ["WEAPON_UMP45"]={"UMP-45",250000,0,"N/A","w_sb_ump45"},
-        --["WEAPON_MPXC"]={"Sig MPXC",300000,0,"N/A","w_sb_mpxc"},
+        ["WEAPON_BERETTA"]={"Berreta M9 Pistol",60000,0,"N/A","w_pi_beretta"},
+        ["WEAPON_M1911"]={"M1911 Pistol",60000,0,"N/A","w_pi_beretta"}, -- need model
+        ["WEAPON_MPX"]={"MPX",300000,0,"N/A","w_pi_beretta"}, -- need model
+        ["WEAPON_PYTHON"]={"Python .357 Revolver",50000,0,"N/A","w_pi_beretta"}, -- need model
+        ["WEAPON_ROOK"]={"Rook 9mm",60000,0,"N/A","w_pi_beretta"}, -- need model
+        ["WEAPON_TEC9"]={"Tec-9",50000,0,"N/A","w_pi_beretta"}, -- need model
+        ["WEAPON_UMP45"]={"UMP-45",300000,0,"N/A","w_sb_ump45"},
         ["item1"]={"LVL 1 Armour",25000,0,"N/A","prop_armour_pickup"},
     },
     ["Shank"]={
         ["_config"]={{vector3(-3171.5241699219,1087.5402832031,19.838747024536),vector3(-330.56484985352,6083.6059570312,30.454759597778),vector3(2567.6704101562,294.36923217773,107.70868457031)},154,1,"B&Q Tool Shop",{""},true},
         ["WEAPON_BROOM"]={"Broom",1000,0,"N/A","prop_tool_broom"},
-        ["WEAPON_BUTTERFLYKNIFE"]={"Butterfly Knife",5000,0,"N/A","w_me_butterflyknife"},
-        --["WEAPON_CLEAVER"]={"Cleaver",300,0,"N/A","w_me_cleaver"},
-        --["WEAPON_CRICKETBAT"]={"Cricket Bat",1200,0,"N/A","w_me_cricketbat"},
-        ["WEAPON_DILDO"]={"Dildo",9000,0,"N/A","w_me_dildo"},
-        --["WEAPON_FIREAXE"]={"Fireaxe",3000,0,"N/A","w_me_fireaxe"},
-        ["WEAPON_FLASHLIGHT"]={"Flashlight",500,0,"N/A","w_me_flashlight"},
-        --["WEAPON_GUITAR"]={"Guitar",6000,0,"N/A","w_me_guitar"},
-        --["WEAPON_HAMMERAXE"]={"Hammer+Axe+Hammer",12000,0,"N/A","w_me_hammeraxe"},
-        --["WEAPON_KITCHENKNIFE"]={"Kitchen Knife",300,0,"N/A","w_me_kitchenknife"},
-        ["WEAPON_SHANK"]={"Shank",3000,0,"N/A","w_me_shank"},
-        --["WEAPON_SHOVEL"]={"Shovel",900,0,"N/A","w_me_shovel"},
-        --["WEAPON_SLEDGEHAMMER"]={"Sledge Hammer",2000,0,"N/A","w_me_sledgehammer"},
-        ["WEAPON_SWITCHBLADE"]={"Switch Blade",500,0,"N/A","w_me_switchblade"},
-        ["WEAPON_TOILETBRUSH"]={"Toilet Brush",100,0,"N/A","w_me_toiletbrush"},
-        --["WEAPON_TRAFFICSIGN"]={"Traffic Sign",8000,0,"N/A","w_me_trafficsign"},
-        --["WEAPON_BASEBALLBAT"]={"Baseball Bat",1200,0,"N/A","w_me_baseballbat"},
+        -- do the rest
     }
 }
 
