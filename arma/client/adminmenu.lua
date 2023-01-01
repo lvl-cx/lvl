04cc2e1b5cc3719e20a7b3ea1f99b19a326526d0
cnr <34693611+terrorist@users.noreply.github.com>
2023-01-01 20:26:05 +0000

    tweak(admin): correct perm

--- arma/client/adminmenu.lua
+++ arma/client/adminmenu.lua
@@ -781,7 +781,7 @@ RageUI.CreateWhile(1.0, true, function()
                     end
                 end, RMenu:Get('adminmenu', 'submenu'))
             end
-            if tARMA.getStaffLevel() >= 4 then
+            if tARMA.getStaffLevel() >= 5 then
                 RageUI.ButtonWithStyle("Slap Player", SelectedPlayer[1] .. " Perm ID: " .. SelectedPlayer[3] .. " Temp ID: " .. SelectedPlayer[2], {RightLabel = "→→→"}, true, function(Hovered, Active, Selected)
                     if Selected then
                         local uid = GetPlayerServerId(PlayerId())
