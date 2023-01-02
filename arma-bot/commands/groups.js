373f8a4c3d19f28863eb7676c83667ec6120544e
cnr <34693611+terrorist@users.noreply.github.com>
2023-01-02 22:20:46 +0000

    tweak(bot): fix groups command if nil

--- arma-bot/commands/groups.js
+++ arma-bot/commands/groups.js
@@ -9,26 +9,30 @@ exports.runcmd = (fivemexports, client, message, params) => {
     fivemexports.ghmattimysql.execute("SELECT * FROM `arma_user_data` WHERE user_id = ?", [params[0]], (result) => {
         if (result.length > 0) {
             obj = JSON.parse(result[0].dvalue).groups
-            let embed = {
-                "title": `**User Groups** ${params[0]}`,
-                "description": `**Success**! Groups have been fetched for User ID: ***${params[0]}***`,
-                "color": settingsjson.settings.botColour,
-                "fields": [
-                    {
-                        name: '**Perm ID:**',
-                        value: `${params[0]}`,
+            if (Object.entries(obj).length > 0) {
+                let embed = {
+                    "title": `**User Groups** ${params[0]}`,
+                    "description": `**Success**! Groups have been fetched for User ID: ***${params[0]}***`,
+                    "color": settingsjson.settings.botColour,
+                    "fields": [
+                        {
+                            name: '**Perm ID:**',
+                            value: `${params[0]}`,
+                        },
+                        {
+                            name: '**Groups:**',
+                            value: `${(JSON.stringify(Object.keys(obj)).replace(/"/g, '').replace('[', '').replace(']', '')).replace(/,/g, ', ')}`,
+                        }
+                    ],
+                    "footer": {
+                        "text": ""
                     },
-                    {
-                        name: '**Groups:**',
-                        value: `${(JSON.stringify(Object.keys(obj)).replace(/"/g, '').replace('[', '').replace(']', '')).replace(/,/g, ', ')}`,
-                    }
-                ],
-                "footer": {
-                    "text": ""
-                },
-                "timestamp": new Date()
+                    "timestamp": new Date()
+                }
+                message.channel.send({ embed })
+            } else {
+                message.reply('No groups for this user.')
             }
-            message.channel.send({ embed })
         } else {
             message.reply('No groups for this user.')
         }
