da37e0c43d700ffac1118a82443f7b95103849c9
cnr <34693611+terrorist@users.noreply.github.com>
2023-01-03 22:50:03 +0000

    tweak(compass): new compass funcs for checking enabled

--- arma/client/cl_compass.lua
+++ arma/client/cl_compass.lua
@@ -1,230 +1,186 @@
-local CompassConfig = {}
-CompassConfig.compass = { cardinal={}, intercardinal={}, needle={} }
-CompassConfig.compass.show = true
-CompassConfig.compass.position = {x = 0.375, y = 0.035, centered = true}
-CompassConfig.compass.width = 0.25
-CompassConfig.compass.fov = 180
-CompassConfig.compass.followGameplayCam = true
-
-CompassConfig.compass.ticksBetweenCardinals = 9.0
-CompassConfig.compass.tickColour = {r = 255, g = 255, b = 255, a = 255}
-CompassConfig.compass.tickSize = {w = 0.001, h = 0.003}
-
-CompassConfig.compass.cardinal.textSize = 0.15
-CompassConfig.compass.cardinal.textOffset = 0.01
-CompassConfig.compass.cardinal.textColour = {r = 255, g = 255, b = 255, a = 255}
-
-CompassConfig.compass.cardinal.tickShow = true
-CompassConfig.compass.cardinal.tickSize = {w = 0.001, h = 0.012}
-CompassConfig.compass.cardinal.tickColour = {r = 255, g = 255, b = 255, a = 255}
-
-CompassConfig.compass.intercardinal.show = true
-CompassConfig.compass.intercardinal.textShow = true
-CompassConfig.compass.intercardinal.textSize = 0.2
-CompassConfig.compass.intercardinal.textOffset = 0.01
-CompassConfig.compass.intercardinal.textColour = {r = 255, g = 255, b = 255, a = 255}
-
-CompassConfig.compass.intercardinal.tickShow = true
-CompassConfig.compass.intercardinal.tickSize = {w = 0.001, h = 0.006}
-CompassConfig.compass.intercardinal.tickColour = {r = 255, g = 255, b = 255, a = 255}
-
-CompassConfig.compass.needle.show = true
-CompassConfig.compass.needle.textSize = 0.20
-CompassConfig.compass.needle.textOffset = 0.03
-CompassConfig.compass.needle.textColour = {r = 255, g = 255, b = 255, a = 255}
-CompassConfig.compass.needle.needleSize = {w = 0.0015, h = 0.018}
-CompassConfig.compass.needle.needleColour ={r = 255, g = 255, b = 255, a = 255}
-
-CompassConfig.streetName = {}
-
--- Configuration. Please be careful when editing. It does not check for errors.
-CompassConfig.streetName.show = false
-CompassConfig.streetName.position = {x = 0.5, y = 0.015, centered = true}
-CompassConfig.streetName.textSize = 0.35
-CompassConfig.streetName.textColour = {r = 255, g = 255, b = 255, a = 255}
-
-
--- DrawText method wrapper, draws text to the screen.
--- @param1	string	The text to draw
--- @param2	float	Screen x-axis coordinate
--- @param3	float	Screen y-axis coordinate
--- @param4	table	Optional. Styles to apply to the text
--- @return  void
-function drawStyledText(str, x, y, style)
-    if style == nil then
-        style = {}
-    end
-
-    SetTextFont((style.font ~= nil) and style.font or 0)
-    SetTextScale(0.0, (style.size ~= nil) and style.size or 1.0)
-    SetTextProportional(1)
-
-    if style.colour ~= nil then
-        SetTextColour(style.colour.r ~= nil and style.colour.r or 255, style.colour.g ~= nil and style.colour.g or 255, style.colour.b ~= nil and style.colour.b or 255, style.colour.a ~= nil and style.colour.a or 255)
-    else
-        SetTextColour(255, 255, 255, 255)
-    end
-
-    if style.shadow ~= nil then
-        SetTextDropShadow(style.shadow.distance ~= nil and style.shadow.distance or 0, style.shadow.r ~= nil and style.shadow.r or 0, style.shadow.g ~= nil and style.shadow.g or 0, style.shadow.b ~= nil and style.shadow.b or 0, style.shadow.a ~= nil and style.shadow.a or 255)
-    else
-        SetTextDropShadow(0, 0, 0, 0, 255)
-    end
-
-    if style.border ~= nil then
-        SetTextEdge(style.border.size ~= nil and style.border.size or 1, style.border.r ~= nil and style.border.r or 0, style.border.g ~= nil and style.border.g or 0, style.border.b ~= nil and style.border.b or 0, style.border.a ~= nil and style.shadow.a or 255)
-    end
-
-    if style.centered ~= nil and style.centered == true then
-        SetTextCentre(true)
-    end
-
-    if style.outline ~= nil and style.outline == true then
-        SetTextOutline()
-    end
-
-    SetTextEntry("STRING")
-    AddTextComponentString(str)
-
-    DrawText(x, y)
-end
-
--- Converts degrees to (inter)cardinal directions.
--- @param1	float	Degrees. Expects EAST to be 90° and WEST to be 270°.
--- 					In GTA, WEST is usually 90°, EAST is usually 270°. To convert, subtract that value from 360.
---
--- @return			The converted (inter)cardinal direction.
-function degreesToIntercardinalDirection( dgr )
-    dgr = dgr % 360.0
-
-    if (dgr >= 0.0 and dgr < 22.5) or dgr >= 337.5 then
+local a = {}
+a.compass = {cardinal = {}, intercardinal = {}, needle = {}}
+a.compass.show = true
+a.compass.position = {x = 0.375, y = 0.035, centered = true}
+a.compass.width = 0.25
+a.compass.fov = 180
+a.compass.followGameplayCam = true
+a.compass.ticksBetweenCardinals = 9.0
+a.compass.tickColour = {r = 255, g = 255, b = 255, a = 255}
+a.compass.tickSize = {w = 0.001, h = 0.003}
+a.compass.cardinal.textSize = 0.15
+a.compass.cardinal.textOffset = 0.01
+a.compass.cardinal.textColour = {r = 255, g = 255, b = 255, a = 255}
+a.compass.cardinal.tickShow = true
+a.compass.cardinal.tickSize = {w = 0.001, h = 0.012}
+a.compass.cardinal.tickColour = {r = 0, g = 168, b = 255, a = 255}
+a.compass.intercardinal.show = true
+a.compass.intercardinal.textShow = true
+a.compass.intercardinal.textSize = 0.2
+a.compass.intercardinal.textOffset = 0.01
+a.compass.intercardinal.textColour = {r = 255, g = 255, b = 255, a = 255}
+a.compass.intercardinal.tickShow = true
+a.compass.intercardinal.tickSize = {w = 0.001, h = 0.006}
+a.compass.intercardinal.tickColour = {r = 255, g = 255, b = 255, a = 255}
+a.compass.needle.show = true
+a.compass.needle.textSize = 0.20
+a.compass.needle.textOffset = 0.03
+a.compass.needle.textColour = {r = 255, g = 255, b = 255, a = 255}
+a.compass.needle.needleSize = {w = 0.0015, h = 0.018}
+a.compass.needle.needleColour = {r = 0, g = 168, b = 255, a = 255}
+a.streetName = {}
+a.streetName.show = false
+a.streetName.position = {x = 0.5, y = 0.015, centered = true}
+a.streetName.textSize = 0.35
+a.streetName.textColour = {r = 255, g = 255, b = 255, a = 255}
+function degreesToIntercardinalDirection(b)
+    b = b % 360.0
+    if b >= 0.0 and b < 22.5 or b >= 337.5 then
         return "N "
-    elseif dgr >= 22.5 and dgr < 67.5 then
+    elseif b >= 22.5 and b < 67.5 then
         return "NE"
-    elseif dgr >= 67.5 and dgr < 112.5 then
+    elseif b >= 67.5 and b < 112.5 then
         return "E"
-    elseif dgr >= 112.5 and dgr < 157.5 then
+    elseif b >= 112.5 and b < 157.5 then
         return "SE"
-    elseif dgr >= 157.5 and dgr < 202.5 then
+    elseif b >= 157.5 and b < 202.5 then
         return "S"
-    elseif dgr >= 202.5 and dgr < 247.5 then
+    elseif b >= 202.5 and b < 247.5 then
         return "SW"
-    elseif dgr >= 247.5 and dgr < 292.5 then
+    elseif b >= 247.5 and b < 292.5 then
         return "W"
-    elseif dgr >= 292.5 and dgr < 337.5 then
+    elseif b >= 292.5 and b < 337.5 then
         return "NW"
     end
 end
-
--- Rounds a number to the closest integer
--- @param1 number to be rounded
--- @param2 decimal places to round
---
--- @return rounded number
-function round(num, numDecimalPlaces)
-    local mult = 10^(numDecimalPlaces or 0)
-    return math.floor(num + 0.5 * mult)
+function round(c, d)
+    local e = 10 ^ (d or 0)
+    return math.floor(c + 0.5 * e)
 end
-
-local showCompass = false
-RegisterCommand("compass",function()
-	if not showCompass then 
-		showCompass = true
-	else
-		showCompass = false
-	end
+RegisterCommand("showcompass",function()
+	showCompass = not showCompass
 end)
-
-local function func_compass() 
-	if showCompass and not globalHideUi then
-
-		local pxDegree = CompassConfig.compass.width / CompassConfig.compass.fov
-		local playerHeadingDegrees = 0
-
-		if CompassConfig.compass.followGameplayCam then
-			-- Converts [-180, 180] to [0, 360] where E = 90 and W = 270
-			local camRot = Citizen.InvokeNative(0x837765A25378F0BB, 0, Citizen.ResultAsVector())
-			playerHeadingDegrees = 360.0 - ((camRot.z + 360.0) % 360.0)
-		else
-			-- Converts E = 270 to E = 90
-			playerHeadingDegrees = 360.0 - GetEntityHeading(GetPlayerPed(-1))
-		end
-
-		local tickDegree = playerHeadingDegrees - CompassConfig.compass.fov / 2
-		local tickDegreeRemainder = CompassConfig.compass.ticksBetweenCardinals - (tickDegree % CompassConfig.compass.ticksBetweenCardinals)
-		local tickPosition = CompassConfig.compass.position.x + tickDegreeRemainder * pxDegree
-
-		tickDegree = tickDegree + tickDegreeRemainder
-
-		while tickPosition < CompassConfig.compass.position.x + CompassConfig.compass.width do
-			if (tickDegree % 90.0) == 0 then
-				-- Draw cardinal
-				if CompassConfig.compass.cardinal.tickShow then
-					DrawRect(tickPosition, CompassConfig.compass.position.y, CompassConfig.compass.cardinal.tickSize.w, CompassConfig.compass.cardinal.tickSize.h, CompassConfig.compass.cardinal.tickColour.r, CompassConfig.compass.cardinal.tickColour.g, CompassConfig.compass.cardinal.tickColour.b, CompassConfig.compass.cardinal.tickColour.a)
-				end
-
-				drawStyledText(degreesToIntercardinalDirection(tickDegree), tickPosition, CompassConfig.compass.position.y + CompassConfig.compass.cardinal.textOffset, {
-					size = CompassConfig.compass.cardinal.textSize,
-					colour = CompassConfig.compass.cardinal.textColour,
-					outline = true,
-					centered = true
-				})
-			elseif (tickDegree % 45.0) == 0 and CompassConfig.compass.intercardinal.show then
-				-- Draw intercardinal
-				if CompassConfig.compass.intercardinal.tickShow then
-					DrawRect(tickPosition, CompassConfig.compass.position.y, CompassConfig.compass.intercardinal.tickSize.w, CompassConfig.compass.intercardinal.tickSize.h, CompassConfig.compass.intercardinal.tickColour.r, CompassConfig.compass.intercardinal.tickColour.g, CompassConfig.compass.intercardinal.tickColour.b, CompassConfig.compass.intercardinal.tickColour.a)
-				end
-
-				if CompassConfig.compass.intercardinal.textShow then
-					drawStyledText(degreesToIntercardinalDirection(tickDegree), tickPosition, CompassConfig.compass.position.y + CompassConfig.compass.intercardinal.textOffset, {
-						size = CompassConfig.compass.intercardinal.textSize,
-						colour = CompassConfig.compass.intercardinal.textColour,
-						outline = true,
-						centered = true
-					})
-				end
-			else
-				-- Draw tick
-				DrawRect(tickPosition, CompassConfig.compass.position.y, CompassConfig.compass.tickSize.w, CompassConfig.compass.tickSize.h, CompassConfig.compass.tickColour.r, CompassConfig.compass.tickColour.g, CompassConfig.compass.tickColour.b, CompassConfig.compass.tickColour.a)
-			end
-
-			-- Advance to the next tick
-			tickDegree = tickDegree + CompassConfig.compass.ticksBetweenCardinals
-			tickPosition = tickPosition + pxDegree * CompassConfig.compass.ticksBetweenCardinals
-		end
-
-		if showCompass then
-			drawStyledText(
-				tostring(round(playerHeadingDegrees)),
-				CompassConfig.compass.position.x + CompassConfig.compass.width / 2,
-				CompassConfig.compass.position.y - CompassConfig.compass.needle.textOffset,
-				{
-					size = CompassConfig.compass.needle.textSize,
-					colour = CompassConfig.compass.needle.textColour,
-					outline = true,
-					centered = true
-				}
-			)
-			DrawRect(
-				CompassConfig.compass.position.x + CompassConfig.compass.width / 2,
-				CompassConfig.compass.position.y,
-				CompassConfig.compass.needle.needleSize.w,
-				CompassConfig.compass.needle.needleSize.h,
-				CompassConfig.compass.needle.needleColour.r,
-				CompassConfig.compass.needle.needleColour.g,
-				CompassConfig.compass.needle.needleColour.b,
-				CompassConfig.compass.needle.needleColour.a
-			)
-		end
-
-	end
+function tARMA.isCompassEnabled()
+    return showCompass
 end
-
-
+function tARMA.setCompassEnabled(f)
+    showCompass = f
+    SetResourceKvp("arma_compass", tostring(f))
+end
 Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
-		func_compass()
+	local g = GetResourceKvpString("arma_compass") or "false"
+	if g == "false" then
+		showCompass = false
+	else
+		showCompass = true
 	end
 end)
-
+local function h(i, j, k, l)
+    tARMA.DrawText(j, k, i, l.size, 0, 0, {l.colour.r, l.colour.g, l.colour.b, l.colour.a}, true)
+end
+local function m()
+    if showCompass then
+        local n = a.compass.width / a.compass.fov
+        local o = 0
+        if a.compass.followGameplayCam then
+            local p = GetGameplayCamRot(0)
+            o = 360.0 - (p.z + 360.0) % 360.0
+        else
+            o = 360.0 - GetEntityHeading(tARMA.getPlayerPed())
+        end
+        local q = o - a.compass.fov / 2
+        local r = a.compass.ticksBetweenCardinals - q % a.compass.ticksBetweenCardinals
+        local s = a.compass.position.x + r * n
+        q = q + r
+        while s < a.compass.position.x + a.compass.width do
+            if q % 90.0 == 0 then
+                if a.compass.cardinal.tickShow then
+                    DrawRect(
+                        s,
+                        a.compass.position.y,
+                        a.compass.cardinal.tickSize.w,
+                        a.compass.cardinal.tickSize.h,
+                        a.compass.cardinal.tickColour.r,
+                        a.compass.cardinal.tickColour.g,
+                        a.compass.cardinal.tickColour.b,
+                        a.compass.cardinal.tickColour.a
+                    )
+                end
+                h(
+                    degreesToIntercardinalDirection(q),
+                    s,
+                    a.compass.position.y + a.compass.cardinal.textOffset,
+                    {
+                        size = a.compass.cardinal.textSize,
+                        colour = a.compass.cardinal.textColour,
+                        outline = true,
+                        centered = true
+                    }
+                )
+            elseif q % 45.0 == 0 and a.compass.intercardinal.show then
+                if a.compass.intercardinal.tickShow then
+                    DrawRect(
+                        s,
+                        a.compass.position.y,
+                        a.compass.intercardinal.tickSize.w,
+                        a.compass.intercardinal.tickSize.h,
+                        a.compass.intercardinal.tickColour.r,
+                        a.compass.intercardinal.tickColour.g,
+                        a.compass.intercardinal.tickColour.b,
+                        a.compass.intercardinal.tickColour.a
+                    )
+                end
+                if a.compass.intercardinal.textShow then
+                    h(
+                        degreesToIntercardinalDirection(q),
+                        s,
+                        a.compass.position.y + a.compass.intercardinal.textOffset,
+                        {
+                            size = a.compass.intercardinal.textSize,
+                            colour = a.compass.intercardinal.textColour,
+                            outline = true,
+                            centered = true
+                        }
+                    )
+                end
+            else
+                DrawRect(
+                    s,
+                    a.compass.position.y,
+                    a.compass.tickSize.w,
+                    a.compass.tickSize.h,
+                    a.compass.tickColour.r,
+                    a.compass.tickColour.g,
+                    a.compass.tickColour.b,
+                    a.compass.tickColour.a
+                )
+            end
+            q = q + a.compass.ticksBetweenCardinals
+            s = s + n * a.compass.ticksBetweenCardinals
+        end
+        if showCompass then
+            h(
+                tostring(round(o)),
+                a.compass.position.x + a.compass.width / 2,
+                a.compass.position.y - a.compass.needle.textOffset,
+                {
+                    size = a.compass.needle.textSize,
+                    colour = a.compass.needle.textColour,
+                    outline = true,
+                    centered = true
+                }
+            )
+            DrawRect(
+                a.compass.position.x + a.compass.width / 2,
+                a.compass.position.y,
+                a.compass.needle.needleSize.w,
+                a.compass.needle.needleSize.h,
+                a.compass.needle.needleColour.r,
+                a.compass.needle.needleColour.g,
+                a.compass.needle.needleColour.b,
+                a.compass.needle.needleColour.a
+            )
+        end
+    end
+end
+tARMA.createThreadOnTick(m)
