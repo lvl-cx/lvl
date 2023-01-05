eede8c680be71214f848e54a4a2f094ac51ee878
cnr <34693611+menstruated@users.noreply.github.com>
2023-01-05 04:13:04 +0000

    tweak(cratedrops): rewrite client+server and new loottable

--- arma/client/cl_cratedrop.lua
+++ arma/client/cl_cratedrop.lua
@@ -1,159 +1,144 @@
-local models = {"p_cargo_chute_s", "sm_prop_smug_crate_s_narc", "titan", "s_m_m_pilot_02"}
-local activeCrate = nil
-local activeParachute = nil
-local crateBlip, radiusBlip = nil
-
-RegisterNetEvent("crateDrop")
-AddEventHandler("crateDrop", function(c)
-    TriggerEvent("removeCrate")
-    for k,v in pairs(models) do
-        tARMA.loadModel(v)
-     end
+radiusBlip = nil
+local a = {}
+local b
+local c = {"p_cargo_chute_s", "xs_prop_arena_crate_01a", "cuban800", "s_m_m_pilot_02"}
+local d
+RegisterNetEvent("ARMA:crateDrop",function(e, f, g)
+    -- e = vector3
+    -- f = crate id
+    -- g (if it's rig)
+    for h, i in pairs(c) do
+        tARMA.loadModel(i)
+    end
     RequestWeaponAsset("weapon_flare")
     while not HasWeaponAssetLoaded("weapon_flare") do
         Wait(0)
     end
-
-    local Coords = vector3(c.x, c.y, c.z)
-    local area = math.random(0, 360) + 0.0
-    local height = 1500.0
-    local radius = area / 180.0 * 3.14
-    local startToEnd = vector3(c.x, c.y, c.z) - vector3(math.cos(radius) * height, math.sin(radius) * height, -500.0)
-    local n = c.x - startToEnd.x
-    local o = c.y - startToEnd.y
-    local heading = GetHeadingFromVector_2d(n, o)
-    local aircraft = CreateVehicle("titan", startToEnd.x, startToEnd.y, startToEnd.z, heading, false, true)
-    DecorSetInt(aircraft, "ARMAACVeh", 955)
-    SetEntityHeading(aircraft, heading)
-    SetVehicleDoorsLocked(aircraft, 2)
-    SetEntityDynamic(aircraft, true)
-    ActivatePhysics(aircraft)
-    SetVehicleForwardSpeed(aircraft, 60.0)
-    SetHeliBladesFullSpeed(aircraft)
-    SetVehicleEngineOn(aircraft, true, true, false)
-    ControlLandingGear(aircraft, 3)
-    OpenBombBayDoors(aircraft)
-    SetEntityProofs(aircraft, true, false, true, false, false, false, false, false)
-    local pilotPed = CreatePedInsideVehicle(aircraft, 1, "s_m_m_pilot_02", -1, false, true)
-    SetBlockingOfNonTemporaryEvents(pilotPed, true)
-    SetPedRandomComponentVariation(pilotPed, false)
-    SetPedKeepTask(pilotPed, true)
-    SetTaskVehicleGotoPlaneMinHeightAboveTerrain(aircraft, 50)
-    TaskVehicleDriveToCoord(pilotPed,aircraft,vector3(c.x, c.y, c.z) + vector3(0.0, 0.0, 500.0),60.0,0,"titan",262144,15.0,-1.0)
-    local planeBlip = AddBlipForEntity(aircraft)
-    SetBlipSprite(planeBlip, 307)
-    SetBlipColour(planeBlip, 3)
-    local s = vector2(c.x, c.y)
-    local t = vector2(GetEntityCoords(aircraft).x, GetEntityCoords(aircraft).y)
-    while #(t - s) > 5.0 do
-        Wait(100)
-        t = vector2(GetEntityCoords(aircraft).x, GetEntityCoords(aircraft).y)
+    local j
+    if not g then
+        local k = math.random(0, 360) + 0.0
+        local l = 1500.0
+        local m = k / 180.0 * 3.14
+        local n = vector3(e.x, e.y, e.z) - vector3(math.cos(m) * l, math.sin(m) * l, -500.0)
+        local o = e.x - n.x
+        local p = e.y - n.y
+        local q = GetHeadingFromVector_2d(o, p)
+        j = CreateVehicle("cuban800", n.x, n.y, n.z, q, false, true)
+        DecorSetInt(j, "ARMAACVeh", 955)
+        SetEntityHeading(j, q)
+        SetVehicleDoorsLocked(j, 2)
+        SetEntityDynamic(j, true)
+        ActivatePhysics(j)
+        SetVehicleForwardSpeed(j, 60.0)
+        SetHeliBladesFullSpeed(j)
+        SetVehicleEngineOn(j, true, true, false)
+        ControlLandingGear(j, 3)
+        OpenBombBayDoors(j)
+        SetEntityProofs(j, true, false, true, false, false, false, false, false)
+        local r = CreatePedInsideVehicle(j, 1, "s_m_m_pilot_02", -1, false, true)
+        SetBlockingOfNonTemporaryEvents(r, true)
+        SetPedRandomComponentVariation(r, false)
+        SetPedKeepTask(r, true)
+        SetTaskVehicleGotoPlaneMinHeightAboveTerrain(j, 50)
+        TaskVehicleDriveToCoord(r,j,vector3(e.x, e.y, e.z) + vector3(0.0, 0.0, 50.0),60.0,0,"cuban800",262144,15.0,-1.0)
+        local s = AddBlipForEntity(j)
+        SetBlipSprite(s, 307)
+        SetBlipColour(s, 3)
+        local t = vector2(e.x, e.y)
+        local u = vector2(GetEntityCoords(j).x, GetEntityCoords(j).y)
+        while #(u - t) > 5.0 do
+            Wait(100)
+            u = vector2(GetEntityCoords(j).x, GetEntityCoords(j).y)
+        end
+        TaskVehicleDriveToCoord(r, j, 0.0, 0.0, 500.0, 60.0, 0, "cuban800", 262144, -1.0, -1.0)
+        SetTimeout(30000,function()
+            SetEntityAsNoLongerNeeded(r)
+            SetEntityAsNoLongerNeeded(j)
+        end)
     end
-    TaskVehicleDriveToCoord(pilotPed, aircraft, 0.0, 0.0, 500.0, 60.0, 0, "titan", 262144, -1.0, -1.0)
-    SetTimeout(30000,function()
-        DeleteEntity(aircraft)
-        DeleteEntity(pilotPed)
-        RemoveBlip(planeBlip)
-    end)
-
-    activeCrate = CreateObject(GetHashKey(models[2]), Coords, false, true, true)
-
-    SetEntityLodDist(activeCrate, 10000)
-    ActivatePhysics(activeCrate)
-    SetDamping(activeCrate, 2, 0.1)
-    SetEntityVelocity(activeCrate, 0.0, 0.0, -0.3)
-
-    activeParachute = CreateObject(GetHashKey(models[1]), Coords, false, true, true)
-    SetEntityLodDist(activeParachute, 10000)
-    SetEntityVelocity(activeParachute, 0.0, 0.0, -0.3)
-    ActivatePhysics(activeCrate)
-    AttachEntityToEntity(activeParachute, activeCrate, 0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, false, false, false, false, 2, true)
-    radiusBlip = AddBlipForRadius(Coords, 200.0)
+    local v = vector3(e.x, e.y, GetEntityCoords(j).z - 5.0)
+    a[f] = {}
+    a[f].crate = CreateObject("xs_prop_arena_crate_01a", v, false, true, true)
+    DecorSetInt(a[f].crate, "lootid", f)
+    DecorSetInt(a[f].crate, "ARMAACVeh", 955)
+    SetEntityLodDist(a[f].crate, 10000)
+    ActivatePhysics(a[f].crate)
+    SetDamping(a[f].crate, 2, 0.1)
+    SetEntityVelocity(a[f].crate, 0.0, 0.0, -0.1)
+    FreezeEntityPosition(a[f].crate, true)
+    Wait(500)
+    FreezeEntityPosition(a[f].crate, false)
+    local w = AddBlipForEntity(a[f].crate)
+    if g then
+        SetBlipSprite(w, 306)
+    else
+        SetBlipSprite(w, 501)
+    end
+    SetBlipColour(w, 2)
+    a[f].parachute = CreateObject("p_cargo_chute_s", v, false, true, true)
+    DecorSetInt(a[f].parachute, "ARMAACVeh", 955)
+    SetEntityLodDist(a[f].parachute, 10000)
+    SetEntityVelocity(a[f].parachute, 0.0, 0.0, -0.1)
+    ActivatePhysics(a[f].crate)
+    AttachEntityToEntity(a[f].parachute,a[f].crate,0,0.0,0.0,0.1,0.0,0.0,0.0,false,false,false,false,2,true)
+    radiusBlip = AddBlipForRadius(e.x, e.y, e.z, g and 50.0 or 200.0)
     SetBlipColour(radiusBlip, 1)
     SetBlipAlpha(radiusBlip, 180)
-
-    crateBlip = AddBlipForEntity(activeCrate)
-    SetBlipSprite(crateBlip, 501)
-    SetBlipColour(crateBlip, 2)
-
-    while GetEntityHeightAboveGround(activeCrate) > 2 do
+    local x = GetGameTimer()
+    while GetEntityHeightAboveGround(a[f].crate) > 2 and GetGameTimer() - x < 60000 do
         Wait(100)
     end
+    SetEntityCoords(a[f].crate, e + vector3(0.0, 0.0, -1.0))
+    d = GetSoundId()
+    PlaySoundFromEntity(d, "Crate_Beeps", a[f].crate, "MP_CRATE_DROP_SOUNDS", true, 0)
+    ShootSingleBulletBetweenCoords(GetEntityCoords(a[f].crate),GetEntityCoords(a[f].crate) - vector3(0.0001, 0.0001, 0.0001),0,false,"weapon_flare",0,true,false,-1.0)
+    DetachEntity(a[f].parachute, true, true)
+    DeleteEntity(a[f].parachute)
+    if DoesBlipExist(b) then
+        RemoveBlip(b)
+    end
+    local y = GetEntityCoords(a[f].crate)
+    AddOwnedExplosion(tARMA.getPlayerPed(), y.x, y.y, y.z, 1, 0.0, true, false, 3.0)
+    FreezeEntityPosition(a[f].crate, true)
+    for h, i in pairs(c) do
+        SetModelAsNoLongerNeeded(GetHashKey(i))
+    end
+end)
 
- 
-    DetachEntity(activeParachute, true, true)
-    DeleteEntity(activeParachute)
-    ShootSingleBulletBetweenCoords(GetEntityCoords(activeCrate),GetEntityCoords(activeCrate) - vector3(0.0001, 0.0001, 0.0001),0,false,"weapon_flare",0,true,false,-1.0)
-
-    Citizen.CreateThread(function()
-        while true do
-            Citizen.Wait(0)
-
-            local boxCoords = GetEntityCoords(activeCrate)
-            local playerCoords = GetEntityCoords(PlayerPedId())
-
-            if #(boxCoords - playerCoords) < 2.0 then
-                if (IsControlJustPressed(1, 51)) then
-                    if IsPedInAnyVehicle(GetPlayerPed(-1), false) == false then
-                        tARMA.loadAnimDict('amb@medic@standing@kneel@base')
-                        tARMA.loadAnimDict('anim@gangops@facility@servers@bodysearch@')
-                        TaskPlayAnim(PlayerPedId(), "amb@medic@standing@kneel@base" ,"base" ,8.0, -8.0, -1, 1, 0, false, false, false )
-                        TaskPlayAnim(PlayerPedId(), "anim@gangops@facility@servers@bodysearch@" ,"player_search" ,8.0, -8.0, -1, 48, 0, false, false, false )
-                        tARMA.notify("~g~Looting Crate Drop...")
-                        local e = GetGameTimer()
-                        local h = 15000
-                        CreateThread(function()
-                            tARMA.startCircularProgressBar("",h,nil,function()end)
-                        end)
-                        while e + h > GetGameTimer() do
-                            Wait(0)
-                        end
-                        ClearPedTasksImmediately(PlayerPedId())
-                        if IsPedInAnyVehicle(GetPlayerPed(-1), false) == false then
-                            if IsPedDeadOrDying(PlayerPedId(), true) == false then
-                                TriggerServerEvent('openLootCrate', boxCoords, playerCoords)
-                            else
-                                tARMA.notify("~r~You are Dead!")
-                            end
-                        else
-                        tARMA.notify("~r~You cannot loot while in a Vehicle!")
-                        end
-                    else
-
-                        tARMA.notify("~r~You cannot loot while in a Vehicle!")
-                    end
-
-                end
-            end
+RegisterNetEvent("ARMA:removeLootcrate",function(f)
+    if a[f] then
+        if DoesEntityExist(a[f].crate) then
+            DeleteEntity(a[f].crate)
         end
+        if DoesEntityExist(a[f].parachute) then
+            DeleteEntity(a[f].parachute)
+        end
+        SetTimeout(300000,function()
+            RemoveBlip(radiusBlip)
+        end)
+        StopSound(d)
+        ReleaseSoundId(d)
+    end
+end)
+RegisterNetEvent("ARMA:addCrateDropRedzone",function(f, e)
+    tARMA.loadModel("xs_prop_arena_crate_01a")
+    a[f] = {}
+    a[f].crate = CreateObject("xs_prop_arena_crate_01a", e + vector3(0.0, 0.0, -1.0), false, true, true)
+    DecorSetInt(a[f].crate, "lootid", f)
+    DecorSetInt(a[f].crate, "ARMAACVeh", 955)
+    FreezeEntityPosition(a[f].crate, true)
+    SetModelAsNoLongerNeeded("xs_prop_arena_crate_01a")
+    local w = AddBlipForEntity(a[f].crate)
+    SetBlipSprite(w, 501)
+    SetBlipColour(w, 2)
+    d = GetSoundId()
+    PlaySoundFromEntity(d, "Crate_Beeps", a[f].crate, "MP_CRATE_DROP_SOUNDS", true, 0)
+    radiusBlip = AddBlipForRadius(e.x, e.y, e.z, 200.0)
+    SetBlipColour(radiusBlip, 1)
+    SetBlipAlpha(radiusBlip, 180)
+end)
+RegisterNetEvent("ARMA:removeCrateRedzone",function()
+    SetTimeout(300000,function()
+        RemoveBlip(radiusBlip)
     end)
-end)
-
-function notify(string)
-    SetNotificationTextEntry("STRING")
-    AddTextComponentString(string)
-    DrawNotification(true, false)
-end
-
-RegisterNetEvent("removeCrate")
-AddEventHandler("removeCrate", function()
-    if activeCrate then
-        if DoesEntityExist(activeCrate) then
-            DeleteEntity(activeCrate)
-        end
-        if DoesEntityExist(activeParachute) then
-            DeleteEntity(activeParachute)
-        end
-        RemoveBlip(radiusBlip)
-        RemoveBlip(crateBlip)
-    end
-end)
-
-AddEventHandler("onResourceStop", function(resource)
-    if resource == GetCurrentResourceName() then
-        DeleteEntity(activeCrate)
-        DeleteEntity(activeParachute)
-        RemoveBlip(crateBlip)
-        RemoveBlip(radiusBlip)
-    end
 end)
--- arma/modules/sv_cratedrop.lua
+++ arma/modules/sv_cratedrop.lua
@@ -1,71 +1,93 @@
 
 
 
-local Coords = { --Where you want the crate to spawn ALL MESSAGES YOU CAN DELETE AFTER (WOLFHILL)
-    vector3(1865.0517578125,294.37481689453,168.91744995117+1600),
-    vector3(2945.5515136719,2795.4934082031,40.646308898926+1600),
-    vector3(-1627.5288085938,720.173828125,192.08515930176+1600),
-    vector3(-62.860485076904,-2493.8740234375,6.0251984596252+1600),
+local crateLocations = { --Where you want the crate to spawn ALL MESSAGES YOU CAN DELETE AFTER (WOLFHILL)
+    vector3(1865.0517578125,294.37481689453,168.91744995117),
+    vector3(2945.5515136719,2795.4934082031,40.646308898926),
+    vector3(-1627.5288085938,720.173828125,192.08515930176),
+    vector3(-62.860485076904,-2493.8740234375,6.0251984596252),
 }
+local activeCrates = {}
 
-local stayTime = 3600 --How long till the airdrop disappears
-local spawnTime = 20*60 --3600
-local used = false
-
-local dropMsg = "An airdrop is landing..."
-local removeMsg = "The airdrop has vanished..."
-local lootedMsg = "Someone looted the airdrop!"
+local stayTime = 15*60 --How long till the airdrop disappears
+local spawnTime = 20*60 -- Time between each airdrop
 
 local availableItems = { --Where you put you weapons and how frequently you want them to spawn E.G M1911 with its ammo. and put that in there twice and akm once the m1911 will have more chance of spawning
+    {"wbody|WEAPON_UMP45", 1},
     {"9mm Bullets", 250},
     {"wbody|WEAPON_MOSIN", 1},
+    {"wbody|WEAPON_AKKAL", 1},
     {"7.62mm Bullets", 250},
 }
 
-RegisterServerEvent('openLootCrate', function(playerCoords, boxCoords)
+RegisterServerEvent('ARMA:openCrate', function(crateID)
     local source = source
-    user_id = ARMA.getUserId(source)
-    if #(playerCoords - boxCoords) < 2.0 then
-        if not used then
-            used = true
-            lootrandom = math.random(1, 3)
+    local user_id = ARMA.getUserId(source)
+    print('Current Crate: '..crateID, 'Active Crates: '..json.encode(activeCrates))
+    if activeCrates[crateID] == nil then return end
+    if #(GetEntityCoords(GetPlayerPed(source)) - crateLocations[crateID]) < 2.0 then
+        FreezeEntityPosition(GetPlayerPed(source), true)
+        ARMAclient.startCircularProgressBar(source, {"", 15000, nil})
+        local anims = {
+            {'amb@medic@standing@kneel@base', 'base', 1},
+            {'anim@gangops@facility@servers@bodysearch@', 'player_search', 1},
+        }
+        ARMAclient.playAnim(source,{true,anims,false})
+        Wait(15000)
+        local lootrandom = math.random(3, 8)
+        while lootrandom > 0 do
+            local randomItem = math.random(1, #availableItems)
             for k,v in pairs(availableItems) do
-                while lootrandom > 0 do
+                if k == randomItem then
                     ARMA.giveInventoryItem(user_id, v[1], v[2], true)
-                    lootrandom = lootrandom - 1
                 end
             end
-            ARMA.giveMoney(user_id,math.random(50000,150000))
-            TriggerClientEvent('chatMessage', -1, "^1[ARMA RP]: ^0", {66, 72, 245}, "Crate drop has been looted.", "alert")
-            Citizen.Wait(300*1000)
-            TriggerClientEvent("removeCrate", -1)
+            lootrandom = lootrandom - 1
         end
+        ARMA.giveMoney(user_id,math.random(50000,150000))
+        TriggerClientEvent('chatMessage', -1, "^1[ARMA RP]: ^0", {66, 72, 245}, "Crate drop has been looted.", "alert")
+        FreezeEntityPosition(GetPlayerPed(source), false)
+        TriggerClientEvent("ARMA:removeLootcrate", crateID)
+        TriggerClientEvent("ARMA:removeCrateRedzone", -1)
+        table.remove(activeCrates, crateID)
     end
 end)
 
 Citizen.CreateThread(function()
     while true do
-        Wait(3600 * 1000)
-
-        local num = math.random(1, #Coords)
-        local coords = Coords[num]
-
-        TriggerClientEvent('crateDrop', -1, coords)
-        TriggerClientEvent('chatMessage', -1, "^1[ARMA RP]: ^0", {66, 72, 245}, dropMsg, "alert")
-        used = false
-        Wait(1000 * 3600)
-        TriggerClientEvent("removeCrate", -1)
+        Wait(spawnTime * 1000)
+        local crateID = math.random(1, #crateLocations)
+        local crateCoords = crateLocations[crateID]
+        local oilrig = false -- need to setup oil rig loot and coords
+        TriggerClientEvent('ARMA:crateDrop', -1, crateCoords, crateID, oilrig)
+        table.insert(activeCrates, crateID)
+        TriggerClientEvent('chatMessage', -1, "^1[ARMA RP]: ^0", {66, 72, 245}, "An airdrop is landing...", "alert")
+        Wait(1000 * stayTime)
+        if activeCrates[crateID] ~= nil then
+            TriggerClientEvent('chatMessage', -1, "^1[ARMA RP]: ^0", {66, 72, 245}, "The airdrop has disappeared.", "alert")
+            table.remove(activeCrates, num)
+            TriggerClientEvent("ARMA:removeLootcrate", num)
+        end
         Wait(1000)
     end
 end)
 
-RegisterCommand('cratedrop', function(source, args, RawCommand)
+RegisterCommand('testdrop', function(source)
+    local source = source
     local user_id = ARMA.getUserId(source)
-    local num = math.random(1, #Coords)
-    local coords = Coords[num]
     if user_id == 1 then
-        TriggerClientEvent('crateDrop', -1, coords)
-        TriggerClientEvent('chatMessage', -1, "^1[ARMA RP]: ^0", {66, 72, 245}, dropMsg, "alert")
-        used = false
+        local crateID = math.random(1, #crateLocations)
+        local crateCoords = crateLocations[crateID]
+        local oilrig = false -- need to setup oil rig loot and coords
+        TriggerClientEvent('ARMA:crateDrop', -1, crateCoords, crateID, oilrig)
+        table.insert(activeCrates, crateID)
+        TriggerClientEvent('chatMessage', -1, "^1[ARMA RP]: ^0", {66, 72, 245}, "An airdrop is landing...", "alert")
+        Wait(1000 * stayTime)
+        if activeCrates[crateID] ~= nil then
+            TriggerClientEvent('chatMessage', -1, "^1[ARMA RP]: ^0", {66, 72, 245}, "The airdrop has disappeared.", "alert")
+            table.remove(activeCrates, num)
+            TriggerClientEvent("ARMA:removeLootcrate", num)
+        end
+        Wait(1000)
     end
 end)
