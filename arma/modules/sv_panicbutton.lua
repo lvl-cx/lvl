4856ea59cb056253c70aa889aa0350407665e42c
cnr <34693611+menstruated@users.noreply.github.com>
2023-01-06 01:48:57 +0000

    add(panic): logs for all factions

--- arma/modules/sv_panicbutton.lua
+++ arma/modules/sv_panicbutton.lua
@@ -4,5 +4,6 @@ AddEventHandler("ARMA:newPanic", function(a,b)
 	local user_id = ARMA.getUserId(source)
     if ARMA.hasPermission(user_id, 'police.onduty.permission') or ARMA.hasPermission(user_id, 'prisonguard.onduty.permission') or ARMA.hasPermission(user_id, 'nhs.onduty.permission') or ARMA.hasPermission(user_id, 'lfb.onduty.permission') then
         TriggerClientEvent("ARMA:returnPanic", -1, nil, a, b)
+        tARMA.sendWebhook(getPlayerFaction(user_id)..'-panic', 'ARMA Panic Logs', "> Player Name: **"..GetPlayerName(source).."**\n> Player TempID: **"..source.."**\n> Player PermID: **"..user_id.."**\n> Coords: **"..a.Coords.."**\n> Location: **"..a.Location.."**")
     end
 end)
