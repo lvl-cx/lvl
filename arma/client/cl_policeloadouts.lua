610b27be786fd26586244b5d5dfc310b78d2078b
cnr <34693611+menstruated@users.noreply.github.com>
2023-01-08 12:46:44 +0000

    add(police): loadouts menu

---
+++ arma/client/cl_policeloadouts.lua
@@ -0,0 +1,61 @@
+local loadouts = {}
+local selectedLoadout = nil
+local weapons = module("cfg/weapons")
+
+RMenu.Add("policeloadouts","main",RageUI.CreateMenu("","Please Select Division",tARMA.getRageUIMenuWidth(),tARMA.getRageUIMenuHeight(),"banners","gunstore"))
+RMenu.Add("policeloadouts","confirm",RageUI.CreateSubMenu(RMenu:Get("policeloadouts", "main"),"","Confirm Selection",tARMA.getRageUIMenuWidth(),tARMA.getRageUIMenuHeight()))
+
+RageUI.CreateWhile(1.0, true, function()
+    if RageUI.Visible(RMenu:Get('policeloadouts', 'main')) then
+        RageUI.DrawContent({ header = true, glare = false, instructionalButton = false}, function()
+            for k,v in pairs(loadouts) do
+                RageUI.Button(k, nil, {RightBadge = "→→→"}, v.hasPermission, function(Hovered, Active, Selected)
+                    if Selected then
+                        selectedLoadout = k
+                    end
+                end, RMenu:Get('policeloadouts', 'confirm'))
+            end
+        end)
+    end
+    if RageUI.Visible(RMenu:Get('policeloadouts', 'confirm')) then
+        RageUI.DrawContent({ header = true, glare = false, instructionalButton = false}, function()
+            RageUI.Separator('~g~'..selectedLoadout)
+            for k,v in pairs(loadouts[selectedLoadout].weapons) do
+                RageUI.Separator(weapons.weapons[v].name)
+            end
+            RageUI.Button("Confirm", nil, {RightBadge = "→→→"}, true, function(Hovered, Active, Selected)
+                if Selected then
+                    TriggerServerEvent('ARMA:selectLoadout', selectedLoadout)
+                    RageUI.Visible(RMenu:Get("policeloadouts", "confirm"), false)
+                    RageUI.ActuallyCloseAll()
+                end
+            end)
+        end)
+    end
+end)
+
+RegisterNetEvent('ARMA:gotLoadouts')
+AddEventHandler('ARMA:gotLoadouts', function(loadoutsTable)
+    loadouts = loadoutsTable
+end)
+
+AddEventHandler("ARMA:onClientSpawn",function(p, q)
+    if q then
+        local r = function(s)
+        end
+        local t = function(s)
+            RageUI.Visible(RMenu:Get("policeloadouts", "main"), false)
+            RageUI.ActuallyCloseAll()
+        end
+        local u = function(s)
+            if IsControlJustPressed(1, 38) then
+                TriggerServerEvent('ARMA:getPoliceLoadouts')
+                selectedLoadout = nil
+                RageUI.Visible(RMenu:Get("policeloadouts", "main"), not RageUI.Visible(RMenu:Get("policeloadouts", "main")))
+            end
+            local v, w, x = table.unpack(GetFinalRenderedCamCoord())
+            DrawText3D(457.0222, -983.0001, 30.68948,"Press [E] to open Police Loadouts",v,w,x)
+        end
+        tARMA.createArea("police_loadouts", vector3(457.0222, -983.0001, 30.68948), 1.5, 6, r, t, u, {})
+    end
+end)
---
+++ arma/modules/sv_policeloadouts.lua
@@ -0,0 +1,64 @@
+loadouts = {
+    ['Basic'] = {
+        permission = "police.onduty.permission",
+        weapons = {
+            "WEAPON_NIGHTSTICK",
+            "WEAPON_STUNGUN",
+            "WEAPON_FLASHLIGHT",
+            "WEAPON_PDGLOCK",
+        },
+    },
+    ['SCO-19'] = {
+        permission = "police.loadshop2",
+        weapons = {
+            "WEAPON_NIGHTSTICK",
+            "WEAPON_STUNGUN",
+            "WEAPON_FLASHLIGHT",
+            "WEAPON_PDGLOCK",
+            "WEAPON_G36K",
+        },
+    },
+    ['CTSFO'] = {
+        permission = "police.maxarmour",
+        weapons = {
+            "WEAPON_NIGHTSTICK",
+            "WEAPON_STUNGUN",
+            "WEAPON_FLASHLIGHT",
+            "WEAPON_PDGLOCK",
+            "WEAPON_SPAR17",
+            "WEAPON_REMINGTON700",
+        },
+    }
+}
+
+
+RegisterNetEvent('ARMA:getPoliceLoadouts')
+AddEventHandler('ARMA:getPoliceLoadouts', function()
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    local loadoutsTable = {}
+    for k,v in pairs(loadouts) do
+        v.hasPermission = ARMA.hasPermission(user_id, v.permission) 
+        loadoutsTable[k] = v
+    end
+    TriggerClientEvent('ARMA:gotLoadouts', source, loadoutsTable)
+end)
+
+RegisterNetEvent('ARMA:selectLoadout')
+AddEventHandler('ARMA:selectLoadout', function(loadout)
+    local source = source
+    local user_id = ARMA.getUserId(source)
+    for k,v in pairs(loadouts) do
+        if k == loadout then
+            if ARMA.hasPermission(user_id, v.permission) then
+                for a,b in pairs(v.weapons) do
+                    ARMAclient.allowWeapon(source, {b})
+                    GiveWeaponToPed(GetPlayerPed(source), b, 250, false, false)
+                end
+                ARMAclient.notify(source, {"~g~Received "..loadout.." loadout."})
+            else
+                ARMAclient.notify(source, {"You do not have permission to select this loadout"})
+            end
+        end
+    end
+end)
