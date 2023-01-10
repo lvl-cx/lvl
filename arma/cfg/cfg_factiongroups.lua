dcb82872237389e1807e9a56b04796542aaae325
cnr <34693611+menstruated@users.noreply.github.com>
2023-01-10 00:13:26 +0000

    add(groups): metpd npas, cid

--- arma/cfg/cfg_factiongroups.lua
+++ arma/cfg/cfg_factiongroups.lua
@@ -1,39 +1,40 @@
 local cfg = {}
 
 cfg.metPoliceRanks = {
-    --{"CID Constable", 30000},
-    --{"CID Sergeant", 40000},
-    {"Special Constable", 55000},
-    --{"NPAS", 45000},
-    {"Custody Sergeant", 20000},
-    {"PCSO", 20000},
-    {"Police Constable", 25000},
-    {"Senior Constable", 30000},
-    {"Sergeant", 35000},
-    {"Inspector", 40000},
-    {"Chief Inspector", 45000},
-    {"Superintendent", 50000},
-    {"Chief Superintendent", 55000},
-    {"Commander", 60000},
-    {"Deputy Assistant Commissioner", 65000},
-    {"Assistant Commissioner", 70000},
-    {"Deputy Commissioner", 75000},
-    {"Commissioner", 80000}
+    {"CID Constable", 30000, "police.cidconstable.whitelisted"},
+    {"CID Sergeant", 40000, "police.cidsergeant.whitelisted"},
+    {"Special Constable", 55000, "police.specialconstable.whitelisted"},
+    {"NPAS", 45000, "police.npas.whitelisted"},
+    {"Custody Sergeant", 20000, "police.custodysergeant.whitelisted"},
+    {"PCSO", 20000, "police.pcso.whitelisted"},
+    {"PC", 25000, "police.constable.whitelisted"},
+    {"Senior Constable", 30000, "police.seniorconstable.whitelisted"},
+    {"Sergeant", 35000, "police.sergeant.whitelisted"},
+    {"Inspector", 40000, "police.inspector.whitelisted"},
+    {"Chief Inspector", 45000, "police.chiefinspector.whitelisted"},
+    {"Superintendent", 50000, "police.superintendent.whitelisted"},
+    {"Chief Superintendent", 55000, "police.chiefsuperintendent.whitelisted"},
+    {"Commander", 60000, "police.commander.whitelisted"},
+    {"Dep. Asst. Commissioner", 65000, "police.deputyassistantcommissioner.whitelisted"},
+    {"Assistant Commissioner", 70000, "police.assistantcommissioner.whitelisted"},
+    {"Deputy Commissioner", 75000, "police.deputycommissioner.whitelisted"},
+    {"Commissioner", 80000, "police.commissioner.whitelisted"}
 }
 
 cfg.nhsRanks = {
-    {"NHS Trainee Paramedic", 25000},
-    {"NHS Paramedic", 30000},
-    {"NHS Critical Care", 35000},
-    {"NHS Junior Doctor", 40000},
-    {"NHS Doctor", 45000},
-    {"NHS Senior Doctor", 50000},
-    {"NHS Specialist", 55000},
-    {"NHS Consultant", 60000},
-    {"NHS Captain", 65000},
-    {"NHS Deputy Chief", 70000},
-    {"NHS Assistant Chief", 75000},
-    {"NHS Head Chief", 80000}
+    {"HEMS", 40000, "nhs.hems.whitelisted"},
+    {"NHS Trainee Paramedic", 25000, "nhs.traineeparamedic.whitelisted"},
+    {"NHS Paramedic", 30000, "nhs.paramedic.whitelisted"},
+    {"NHS Critical Care", 35000, "nhs.criticalcare.whitelisted"},
+    {"NHS Junior Doctor", 40000, "nhs.juniordoctor.whitelisted"},
+    {"NHS Doctor", 45000, "nhs.doctor.whitelisted"},
+    {"NHS Senior Doctor", 50000, "nhs.seniordoctor.whitelisted"},
+    {"NHS Specialist", 55000, "nhs.specialist.whitelisted"},
+    {"NHS Consultant", 60000, "nhs.consultant.whitelisted"},
+    {"NHS Captain", 65000, "nhs.captain.whitelisted"},
+    {"NHS Deputy Chief", 70000, "nhs.deputychief.whitelisted"},
+    {"NHS Assistant Chief", 75000, "nhs.assistantchief.whitelisted"},
+    {"NHS Head Chief", 80000, "nhs.headchief.whitelisted"}
 }
 
 cfg.lfbRanks = {
@@ -50,18 +51,24 @@ cfg.lfbRanks = {
 }
 
 cfg.hmpRanks = {
-    {"Trainee Prison Officer", 20000},
-    {"Prison Officer", 30000},
-    {"Senior Officer", 32500},
-    {"Specialist Officer", 35000},
-    {"Principal Officer", 37500},
-    {"Supervising Officer", 40000},
-    {"Honourable Guard", 40000},
-    {"Custodial Officer", 42500},
-    {"Custodial Supervisor", 45000},
-    {"Divisional Commander", 50000},
-    {"Deputy Governor", 55000},
-    {"Governor", 60000},
+    {"Trainee Prison Officer", 20000, "hmp.traineeprisonofficer.whitelisted"},
+    {"Prison Officer", 30000, "hmp.prisonofficer.whitelisted"},
+    {"Senior Officer", 32500, "hmp.seniorofficer.whitelisted"},
+    {"Specialist Officer", 35000, "hmp.specialistofficer.whitelisted"},
+    {"Principal Officer", 37500, "hmp.principalofficer.whitelisted"},
+    {"Supervising Officer", 40000, "hmp.supervisingofficer.whitelisted"},
+    {"Honourable Guard", 40000, "hmp.honourableguard.whitelisted"},
+    {"Custodial Officer", 42500, "hmp.custodialofficer.whitelisted"},
+    {"Custodial Supervisor", 45000, "hmp.custodialsupervisor.whitelisted"},
+    {"Divisional Commander", 50000, "hmp.divisionalcommander.whitelisted"},
+    {"Deputy Governor", 55000, "hmp.deputygovernor.whitelisted"},
+    {"Governor", 60000, "hmp.governor.whitelisted"},
+}
+
+cfg.casinoRanks = {
+    {"Casino Security", 20000},
+    {"Casino Manager", 30000},
+    {"Casino Owner", 50000},
 }
 
 return cfg
