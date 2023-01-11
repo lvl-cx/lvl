68ad50c6533fa9fcb440fb2fe1895155575ee411
cnr <34693611+menstruated@users.noreply.github.com>
2023-01-11 02:19:15 +0000

    tweak(bot): add !ticketlb

---
+++ arma-bot/commands/ticketlb.js
@@ -0,0 +1,32 @@
+var AsciiTable = require('ascii-table');
+const resourcePath = global.GetResourcePath ?
+    global.GetResourcePath(global.GetCurrentResourceName()) : global.__dirname
+const settingsjson = require(resourcePath + '/settings.js')
+
+exports.runcmd = (fivemexports, client, message, params) => {
+    fivemexports.ghmattimysql.execute("SELECT * FROM arma_staff_tickets ORDER BY ticket_count DESC", [], (result) => {
+        var tickets = []
+        if (result) {
+            for (i = 0; i < result.length; i++) {
+                if (i < 10) {
+                    tickets.push(`\n${result[i].username}(${result[i].user_id}) - ${result[i].ticket_count}`)
+                }
+            }
+            let embed = {
+                "title": `Ticket Leaderboard`,
+                "description": '```'+tickets.join('').replace(',', '')+'```',
+                "color": settingsjson.settings.botColour,
+                "footer": {
+                    "text": ""
+                },
+                "timestamp": new Date()
+            }
+            message.channel.send({embed})
+        }
+    })
+}
+
+exports.conf = {
+    name: "ticketlb",
+    perm: 1
+}
