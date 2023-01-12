0d7da5703b56b09a0bc15e9e1c16a23602fc9bb6
cnr <34693611+menstruated@users.noreply.github.com>
2023-01-12 04:15:55 +0000

    tweak(discord): add discord button rich presense

--- arma/client/cl_discord.lua
+++ arma/client/cl_discord.lua
@@ -1,6 +1,6 @@
 Citizen.CreateThread(function()
     if SetDiscordRichPresenceAction then 
         SetDiscordRichPresenceAction(0,"Join ARMA","fivem://connect/s1.armarp.co.uk")
-        SetDiscordRichPresenceAction(0, "Discord","discord.gg/armarp")
+        SetDiscordRichPresenceAction(1, "Discord","https://discord.gg/armarp")
     end 
 end)
