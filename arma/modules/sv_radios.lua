829a6b744db8eae1a224d1a8260563fd4aa991ea
cnr <34693611+menstruated@users.noreply.github.com>
2023-01-12 18:18:47 +0000

    tweak(radios): update based on channel position

--- arma/modules/sv_radios.lua
+++ arma/modules/sv_radios.lua
@@ -44,9 +44,9 @@ AddEventHandler("ARMA:playerSpawn", function(user_id, source, first_spawn)
         for k,v in pairs(cfg.sortOrder.police) do
             if ARMA.hasPermission(user_id, v) then
                 local sortOrder = k
-                table.insert(radioChannels[radioType]['players'], {name = GetPlayerName(source), sortOrder = sortOrder})
+                table.insert(radioChannels[radioType]['players'], {name = GetPlayerName(source), sortOrder = sortOrder, user_id = user_id})
                 TriggerClientEvent('ARMA:radiosCreateChannel', source, radioChannels[radioType].channel, radioChannels[radioType].name, radioChannels[radioType].players, first_spawn)
-                TriggerClientEvent('ARMA:radiosAddPlayer', -1, radioType, user_id, {name = GetPlayerName(source), sortOrder = sortOrder})
+                TriggerClientEvent('ARMA:radiosAddPlayer', -1, radioChannels[radioType].channel, user_id, {name = GetPlayerName(source), sortOrder = sortOrder})
             end
         end
     end
@@ -58,9 +58,10 @@ AddEventHandler('playerDropped', function(reason)
     local radioType = getRadioType(user_id)
     if radioType then
         for k,v in pairs(radioChannels[radioType]['players']) do
-            if v.name == GetPlayerName(source) then
-                table.remove(radioChannels[radioType]['players'], k)
-                TriggerClientEvent('ARMA:radiosRemovePlayer', -1, radioChannels[radioType].channel, user_id)
+            for k,v in pairs(radioChannels[radioType]['players']) do
+                if v.user_id == user_id then
+                    TriggerClientEvent('ARMA:radiosRemovePlayer', -1, radioChannels[radioType].channel, k)
+                end
             end
         end
     end
@@ -72,6 +73,10 @@ AddEventHandler("ARMA:radiosSetIsMuted", function(mutedState)
     local user_id = ARMA.getUserId(source)
     local radioType = getRadioType(user_id)
     if radioType then
-        TriggerClientEvent('ARMA:radiosSetPlayerIsMuted', -1, radioChannels[radioType].channel, user_id, mutedState)
+        for k,v in pairs(radioChannels[radioType]['players']) do
+            if v.user_id == user_id then
+                TriggerClientEvent('ARMA:radiosSetPlayerIsMuted', -1, radioChannels[radioType].channel, k, mutedState)
+            end
+        end
     end
 end)
