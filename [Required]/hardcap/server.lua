c190f696c7702dd6eb3a9e21e9de3254ec303fab
cnr <122835363+cnr1337@users.noreply.github.com>
2023-01-16 21:55:31 +0000

    remove required

--- [Required]/hardcap/server.lua
+++ [Required]/hardcap/server.lua
@@ -19,12 +19,7 @@ end)
 
 AddEventHandler('playerConnecting', function(name, setReason)
   local cv = GetConvarInt('sv_maxclients', 32)
-
-  print('Connecting: ' .. name .. '^7')
-
   if playerCount >= cv then
-    print('Full. :(')
-
     setReason('This server is full (past ' .. tostring(cv) .. ' players).')
     CancelEvent()
   end
