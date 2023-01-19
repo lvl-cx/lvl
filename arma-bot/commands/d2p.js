064136c149397d8df1749647da3062d15043ac88
cnr <122835363+cnr1337@users.noreply.github.com>
2023-01-19 00:52:52 +0000

    tweak(bot): allow d2p in support cord

--- arma-bot/commands/d2p.js
+++ arma-bot/commands/d2p.js
@@ -29,5 +29,6 @@ exports.runcmd = (fivemexports, client, message, params) => {
 exports.conf = {
     name: "d2p",
     perm: 1,
-    guild: "975490533344559154"
+    guild: "975490533344559154",
+    support: true,
 }
