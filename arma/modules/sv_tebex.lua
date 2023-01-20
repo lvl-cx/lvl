9ec025dba48eb121ca506f558bc39011341fdda9
cnr <122835363+elfbars@users.noreply.github.com>
2023-01-20 21:07:04 +0000

    tweak(tebex): add rank commands

--- arma/modules/sv_tebex.lua
+++ arma/modules/sv_tebex.lua
@@ -1,42 +1,15 @@
 -- Tebex functions need redoing to match
 -- Supporter, Premium, Supreme, Kingpin, Rainmaker, Baller
 
--- function supporter(_, arg)
---     print("^3User Has Bought Package! ^7")
--- 	user_id = tonumber(arg[1])
---     usource = ARMA.getUserSource(user_id)
---     print(GetPlayerName(usource)..'['..user_id..'] has bought Supporter')
---     ARMAclient.notify(usource, {"~g~You have purchased the Recruit Rank! ❤️"})
---     ARMA.giveBankMoney(user_id, 1000000)
---     ARMA.addUserGroup(user_id,"Supporter")    
--- end
-
--- function platinum(_, arg)
--- 	user_id = tonumber(arg[1])
---     usource = ARMA.getUserSource(user_id)
---     print(GetPlayerName(usource)..'['..user_id..'] has bought Platinum')
---     ARMAclient.notify(usource, {"~g~You have purchased the Platinum Rank! ❤️"})
---     ARMA.giveBankMoney(user_id, 5000000)
---     ARMA.addUserGroup(user_id,"Platinum")    
--- end
-
--- function godfather(_, arg)
--- 	user_id = tonumber(arg[1])
---     usource = ARMA.getUserSource(user_id)
---     print(GetPlayerName(usource)..'['..user_id..'] has bought Godfather')
---     ARMAclient.notify(usource, {"~g~You have purchased the Godfather Rank! ❤️"})
---     ARMA.giveBankMoney(user_id, 10000000)
---     ARMA.addUserGroup(user_id,"Godfather")
--- end
-
--- function underboss(_, arg)
--- 	user_id = tonumber(arg[1])
---     usource = ARMA.getUserSource(user_id)
---     print(GetPlayerName(usource)..'['..user_id..'] has bought Underboss')
---     ARMAclient.notify(usource, {"~g~You have purchased the Underboss Rank! ❤️"})
---     ARMA.giveBankMoney(user_id, 20000000)
---     ARMA.addUserGroup(user_id,"Underboss")
--- end
+function rank(_, arg)
+	local user_id = tonumber(arg[1])
+    local usource = ARMA.getUserSource(user_id)
+    local rank = arg[2]
+    print(user_id.." has bought "..rank.."! ^7")
+    print(GetPlayerName(usource)..'['..user_id..'] has bought '..rank)
+    ARMAclient.notify(usource, {"~g~You have purchased the "..rank.." Rank! ❤️"})
+    ARMA.addUserGroup(user_id,rank)    
+end
 
 function moneybag(_, arg)
     user_id = tonumber(arg[1])
@@ -46,9 +19,5 @@ function moneybag(_, arg)
     ARMA.giveBankMoney(user_id, tonumber(arg[2]))
 end
 
-
--- RegisterCommand("supporter", supporter, true)
--- RegisterCommand("platinum", platinum, true)
--- RegisterCommand("godfather", godfather, true)
--- RegisterCommand("underboss", underboss, true)
+RegisterCommand("rank", rank, true)
 RegisterCommand("moneybag", moneybag, true)
