0e58abdfe37337b0739b7c4ffb4cce67261321a3
cnr <122835363+elfbars@users.noreply.github.com>
2023-01-28 16:19:55 +0000

    tweak(bot): end status lb

--- arma-bot/bot.js
+++ arma-bot/bot.js
@@ -135,9 +135,9 @@ const init = async() => {
     });
 }
 
-setInterval(function(){
-    promotionDetection();
-}, 60*1000); // 60 * 1000 milsec
+// setInterval(function(){
+//     promotionDetection();
+// }, 60*1000);
 
 function promotionDetection(){
   client.users.forEach(user =>{ //iterate over each user
