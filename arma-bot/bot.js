0969301b2d234ec8c82e4ca53cb8860b0a65566e
cnr <122835363+elfbars@users.noreply.github.com>
2023-01-28 00:45:49 +0000

    bot

--- arma-bot/bot.js
+++ arma-bot/bot.js
@@ -198,13 +198,14 @@ client.getPerms = function(msg) {
 
 client.on('message', (message) => {
     if (!message.author.bot){
-        if (message.content.includes('discord.gg/')){
-            if (!message.content.includes('discord.gg/armarp')){
-                message.delete()
-                return
-            }
-        }
-        else if (message.channel.name.includes('auction-')){
+        // if (message.content.includes('discord.gg/')){
+        //     if (!message.content.includes('discord.gg/armarp')){
+        //         message.delete()
+        //         return
+        //     }
+        // }
+        // else 
+        if (message.channel.name.includes('auction-')){
             if (message.channel.name == '・auction-room'){
                 return
             }
--- arma-bot/commands/trader.js
+++ arma-bot/commands/trader.js
@@ -1,19 +1,19 @@
-const resourcePath = global.GetResourcePath ?
-    global.GetResourcePath(global.GetCurrentResourceName()) : global.__dirname
-const settingsjson = require(resourcePath + '/settings.js')
+// const resourcePath = global.GetResourcePath ?
+//     global.GetResourcePath(global.GetCurrentResourceName()) : global.__dirname
+// const settingsjson = require(resourcePath + '/settings.js')
 
-exports.runcmd = (fivemexports, client, message, params) => {
-    let embed = {
-        "title": "ARMA Trader Discord",
-        "description": `https://discord.gg/X5hV8E39Sf`,
-        "color": settingsjson.settings.botColour,
-        "footer": {
-            "text": ""
-        },
-        "timestamp": new Date()
-    }
-    message.channel.send({ embed })
-}
+// exports.runcmd = (fivemexports, client, message, params) => {
+//     let embed = {
+//         "title": "ARMA Trader Discord",
+//         "description": `https://discord.gg/X5hV8E39Sf`,
+//         "color": settingsjson.settings.botColour,
+//         "footer": {
+//             "text": ""
+//         },
+//         "timestamp": new Date()
+//     }
+//     message.channel.send({ embed })
+// }
 
 exports.conf = {
     name: "trader",
