3420aa771135feb57f5f028aaa4d0eea5c0ee9b2
cnr <122835363+elfbars@users.noreply.github.com>
2023-01-29 22:16:51 +0000

    add(bot): !validate for tbx

---
+++ arma-bot/commands/validate.js
@@ -0,0 +1,69 @@
+var AsciiTable = require('ascii-table');
+const Discord = require('discord.js');
+const fs = require('fs');
+const { default: axios } = require('axios');
+
+const resourcePath = global.GetResourcePath ?
+    global.GetResourcePath(global.GetCurrentResourceName()) : global.__dirname
+const settingsjson = require(resourcePath + '/settings.js')
+
+exports.runcmd = (fivemexports, client, message, params) => {
+    if (!params[0]) {
+        return message.reply('Invalid args! Correct term is: ' + process.env.PREFIX + 'validate [tbx-id]')
+    }
+    else {
+        axios.get(`https://plugin.tebex.io/payments/${params[0]}`, {
+            method: 'GET',
+            headers: { 'X-Tebex-Secret': "dce03e16078522544b88710291791bb8951f9db6"},
+        }).then((res) => {
+            //console.log(res.data)
+            if (!res.data) {
+                return message.reply("Currently unable to fetch from tebex api, use the online store to validate the ID")
+            }else{
+                if (res.data.status !== 'Complete') {
+                    return message.reply(`Purchase is currently marked as ${res.data.status}`)
+                }else{
+                    const packages = res.data.packages.map(package => package.name)
+                    let embed = {
+                        "title": `**Tebex Purchase Profile**`,
+                        "description": `> Purchase is currently marked as ${res.data.status}`,
+                        "color": settingsjson.settings.botColour,
+                        "fields": [
+                            {
+                                name: '> Player Name',
+                                value: res.data.player.name,
+                            },
+                            {
+                                name: '> Price Paid',
+                                value: `${res.data.currency.symbol}${res.data.amount}`,
+                            },
+                            {
+                                name: '> Date Purchased',
+                                value: res.data.date.slice(0,10),
+                            },
+                            {
+                                name: '> Item(s) Purchased',
+                                value: packages.join(', \n'),
+                            },
+                        ],
+                        "footer": {
+                            "text": `Requested by ${message.author.username}`
+                        },
+                        "timestamp": new Date()
+                    }
+                    message.channel.send({embed})
+                }
+            }
+        }).catch(err => {
+            console.log(err)
+            return message.reply({ embeds: [new EmbedBuilder().setColor("#a264f8").setDescription("> No purchase found with that ID.")] })
+        })
+    }
+}
+
+exports.conf = {
+    name: "validate",
+    perm: 7,
+    guild: "975490533344559154",
+    support: true,
+}
