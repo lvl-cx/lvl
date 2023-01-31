37713d4c0387a5024bb21fc55b9be9499ea33c56
cnr <122835363+elfbars@users.noreply.github.com>
2023-01-31 22:50:25 +0000

    tweak(rpzones):

--- arma/modules/sv_rpzones.lua
+++ arma/modules/sv_rpzones.lua
@@ -1,12 +1,14 @@
 local rpZones = {}
+local numRP = 0
 RegisterServerEvent("ARMA:createRPZone")
 AddEventHandler("ARMA:createRPZone", function(a)
 	local source = source
 	local user_id = ARMA.getUserId(source)
     if ARMA.hasPermission(user_id, 'group.remove') then
-        a['uuid'] = #rpZones+1
-        rpZones = a
-        TriggerClientEvent('ARMA:createRPZone', -1, rpZones)
+        numRP = numRP + 1
+        a['uuid'] = numRP
+        rpZones[numRP] = a
+        TriggerClientEvent('ARMA:createRPZone', -1, a)
     end
 end)
 
@@ -15,11 +17,11 @@ AddEventHandler("ARMA:removeRPZone", function(b)
 	local source = source
 	local user_id = ARMA.getUserId(source)
     if ARMA.hasPermission(user_id, 'group.remove') then
-        TriggerClientEvent('ARMA:removeRPZone', -1, b)
         if next(rpZones) then
             for k,v in pairs(rpZones) do
                 if v.uuid == b then
                     rpZones[k] = nil
+                    TriggerClientEvent('ARMA:removeRPZone', -1, b)
                 end
             end
         end
