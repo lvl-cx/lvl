e5b1259d3cb0d089cbd0d9aecc025f05b97f79c5
cnr <122835363+elfbars@users.noreply.github.com>
2023-02-01 20:30:07 +0000

    tweak(turf): remove license req for cap

--- arma/modules/sv_turf.lua
+++ arma/modules/sv_turf.lua
@@ -147,31 +147,29 @@ RegisterNetEvent('ARMA:gangCaptureSuccess')
 AddEventHandler('ARMA:gangCaptureSuccess', function(turfname)
     local source = source
 	local user_id = ARMA.getUserId(source)
-	if ARMA.hasGroup(user_id, 'Gang') then
-		for k,v in pairs(turfData) do
-			if v.name == turfname then
-				exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
-					for K,V in pairs(gotGangs) do
-						for I,L in pairs(json.decode(V.gangmembers)) do
-							if tostring(user_id) == I then
-								TriggerClientEvent('chatMessage', -1, "^0The "..v.name.." trader has been captured by "..V.gangname..".", { 128, 128, 128 }, message, "alert")
-								for a,b in pairs(json.decode(V.gangmembers)) do
-									turfData[k].gangOwner = V.gangname
-									turfData[k].commission = V.commission
-									turfData[k].cooldown = 300
-									turfData[k].beingCaptured = false
-									local data = turfData
-									data[k].ownership = true
-									TriggerClientEvent('ARMA:updateTurfOwner', -1, k, V.gangname)
-									if ARMA.getUserSource(tonumber(a)) ~= nil then
-										TriggerClientEvent('ARMA:gotTurfOwnershipData', ARMA.getUserSource(tonumber(a)), data)
-									end
+	for k,v in pairs(turfData) do
+		if v.name == turfname then
+			exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
+				for K,V in pairs(gotGangs) do
+					for I,L in pairs(json.decode(V.gangmembers)) do
+						if tostring(user_id) == I then
+							TriggerClientEvent('chatMessage', -1, "^0The "..v.name.." trader has been captured by "..V.gangname..".", { 128, 128, 128 }, message, "alert")
+							for a,b in pairs(json.decode(V.gangmembers)) do
+								turfData[k].gangOwner = V.gangname
+								turfData[k].commission = V.commission
+								turfData[k].cooldown = 300
+								turfData[k].beingCaptured = false
+								local data = turfData
+								data[k].ownership = true
+								TriggerClientEvent('ARMA:updateTurfOwner', -1, k, V.gangname)
+								if ARMA.getUserSource(tonumber(a)) ~= nil then
+									TriggerClientEvent('ARMA:gotTurfOwnershipData', ARMA.getUserSource(tonumber(a)), data)
 								end
 							end
 						end
 					end
-				end)
-			end
+				end
+			end)
 		end
 	end
 end)
@@ -180,172 +178,156 @@ RegisterNetEvent('ARMA:gangDefenseSuccess')
 AddEventHandler('ARMA:gangDefenseSuccess', function(turfname)
     local source = source
 	local user_id = ARMA.getUserId(source)
-	if ARMA.hasGroup(user_id, 'Gang') then
-		exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
-			for K,V in pairs(gotGangs) do
-				for I,L in pairs(json.decode(V.gangmembers)) do
-					if tostring(user_id) == I then
-						for a,b in pairs(turfData) do
-							if b.name == turfname then
-								TriggerClientEvent('chatMessage', -1, "^0The "..b.name.." trader is no longer being attacked.", { 128, 128, 128 }, message, "alert")
-								turfData[a] = {ownership = true, gangOwner = V.gangname, commission = b.commission, cooldown = 300}
-								TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
-								return
-							end
+	exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
+		for K,V in pairs(gotGangs) do
+			for I,L in pairs(json.decode(V.gangmembers)) do
+				if tostring(user_id) == I then
+					for a,b in pairs(turfData) do
+						if b.name == turfname then
+							TriggerClientEvent('chatMessage', -1, "^0The "..b.name.." trader is no longer being attacked.", { 128, 128, 128 }, message, "alert")
+							turfData[a] = {ownership = true, gangOwner = V.gangname, commission = b.commission, cooldown = 300}
+							TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
+							return
 						end
 					end
 				end
 			end
-		end)
-	end
+		end
+	end)
 end)
 
 RegisterNetEvent('ARMA:setNewWeedPrice')
 AddEventHandler('ARMA:setNewWeedPrice', function(price)
     local source = source
 	local user_id = ARMA.getUserId(source)
-	if ARMA.hasGroup(user_id, 'Gang') then
-		exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
-			for K,V in pairs(gotGangs) do
-				for I,L in pairs(json.decode(V.gangmembers)) do
-					if tostring(user_id) == I and V.gangname == turfData[1].gangOwner then
-						turfData[1].commission = price
-						TriggerClientEvent('chatMessage', -1, "^0Weed trader commission set to "..price.."%", { 128, 128, 128 }, message, "alert")
-						ARMA.updateTraderInfo()
-						TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
-						return
-					end
+	exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
+		for K,V in pairs(gotGangs) do
+			for I,L in pairs(json.decode(V.gangmembers)) do
+				if tostring(user_id) == I and V.gangname == turfData[1].gangOwner then
+					turfData[1].commission = price
+					TriggerClientEvent('chatMessage', -1, "^0Weed trader commission set to "..price.."%", { 128, 128, 128 }, message, "alert")
+					ARMA.updateTraderInfo()
+					TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
+					return
 				end
 			end
-		end)
-	end
+		end
+	end)
 end)
 
 RegisterNetEvent('ARMA:setNewCocainePrice')
 AddEventHandler('ARMA:setNewCocainePrice', function(price)
     local source = source
 	local user_id = ARMA.getUserId(source)
-	if ARMA.hasGroup(user_id, 'Gang') then
-		exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
-			for K,V in pairs(gotGangs) do
-				for I,L in pairs(json.decode(V.gangmembers)) do
-					if tostring(user_id) == I and V.gangname == turfData[2].gangOwner then
-						turfData[2].commission = price
-						TriggerClientEvent('chatMessage', -1, "^0Cocaine trader commission set to "..price.."%", { 128, 128, 128 }, message, "alert")
-						ARMA.updateTraderInfo()
-						TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
-						return
-					end
+	exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
+		for K,V in pairs(gotGangs) do
+			for I,L in pairs(json.decode(V.gangmembers)) do
+				if tostring(user_id) == I and V.gangname == turfData[2].gangOwner then
+					turfData[2].commission = price
+					TriggerClientEvent('chatMessage', -1, "^0Cocaine trader commission set to "..price.."%", { 128, 128, 128 }, message, "alert")
+					ARMA.updateTraderInfo()
+					TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
+					return
 				end
 			end
-		end)
-	end
+		end
+	end)
 end)
 
 RegisterNetEvent('ARMA:setNewMethPrice')
 AddEventHandler('ARMA:setNewMethPrice', function(price)
     local source = source
 	local user_id = ARMA.getUserId(source)
-	if ARMA.hasGroup(user_id, 'Gang') then
-		exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
-			for K,V in pairs(gotGangs) do
-				for I,L in pairs(json.decode(V.gangmembers)) do
-					if tostring(user_id) == I and V.gangname == turfData[3].gangOwner then
-						turfData[3].commission = price
-						TriggerClientEvent('chatMessage', -1, "^0Meth trader commission set to "..price.."%", { 128, 128, 128 }, message, "alert")
-						ARMA.updateTraderInfo()
-						TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
-						return
-					end
+	exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
+		for K,V in pairs(gotGangs) do
+			for I,L in pairs(json.decode(V.gangmembers)) do
+				if tostring(user_id) == I and V.gangname == turfData[3].gangOwner then
+					turfData[3].commission = price
+					TriggerClientEvent('chatMessage', -1, "^0Meth trader commission set to "..price.."%", { 128, 128, 128 }, message, "alert")
+					ARMA.updateTraderInfo()
+					TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
+					return
 				end
 			end
-		end)
-	end
+		end
+	end)
 end)
 
 RegisterNetEvent('ARMA:setNewHeroinPrice')
 AddEventHandler('ARMA:setNewHeroinPrice', function(price)
     local source = source
 	local user_id = ARMA.getUserId(source)
-	if ARMA.hasGroup(user_id, 'Gang') then
-		exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
-			for K,V in pairs(gotGangs) do
-				for I,L in pairs(json.decode(V.gangmembers)) do
-					if tostring(user_id) == I and V.gangname == turfData[4].gangOwner then
-						turfData[4].commission = price
-						TriggerClientEvent('chatMessage', -1, "^0Heroin trader commission set to "..price.."%", { 128, 128, 128 }, message, "alert")
-						ARMA.updateTraderInfo()
-						TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
-						return
-					end
+	exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
+		for K,V in pairs(gotGangs) do
+			for I,L in pairs(json.decode(V.gangmembers)) do
+				if tostring(user_id) == I and V.gangname == turfData[4].gangOwner then
+					turfData[4].commission = price
+					TriggerClientEvent('chatMessage', -1, "^0Heroin trader commission set to "..price.."%", { 128, 128, 128 }, message, "alert")
+					ARMA.updateTraderInfo()
+					TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
+					return
 				end
 			end
-		end)
-	end
+		end
+	end)
 end)
 
 RegisterNetEvent('ARMA:setNewLargeArmsCommission')
 AddEventHandler('ARMA:setNewLargeArmsCommission', function(price)
     local source = source
 	local user_id = ARMA.getUserId(source)
-	if ARMA.hasGroup(user_id, 'Gang') then
-		exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
-			for K,V in pairs(gotGangs) do
-				for I,L in pairs(json.decode(V.gangmembers)) do
-					if tostring(user_id) == I and V.gangname == turfData[5].gangOwner then
-						turfData[5].commission = price
-						TriggerClientEvent('chatMessage', -1, "^0Large Arms trader commission set to "..price.."%", { 128, 128, 128 }, message, "alert")
-						ARMA.updateTraderInfo()
-						TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
-						TriggerClientEvent('ARMA:recalculateLargeArms', -1, price)
-						return
-					end
+	exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
+		for K,V in pairs(gotGangs) do
+			for I,L in pairs(json.decode(V.gangmembers)) do
+				if tostring(user_id) == I and V.gangname == turfData[5].gangOwner then
+					turfData[5].commission = price
+					TriggerClientEvent('chatMessage', -1, "^0Large Arms trader commission set to "..price.."%", { 128, 128, 128 }, message, "alert")
+					ARMA.updateTraderInfo()
+					TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
+					TriggerClientEvent('ARMA:recalculateLargeArms', -1, price)
+					return
 				end
 			end
-		end)
-	end
+		end
+	end)
 end)
 
 RegisterNetEvent('ARMA:setNewLSDNorthPrice')
 AddEventHandler('ARMA:setNewLSDNorthPrice', function(price)
     local source = source
 	local user_id = ARMA.getUserId(source)
-	if ARMA.hasGroup(user_id, 'Gang') then
-		exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
-			for K,V in pairs(gotGangs) do
-				for I,L in pairs(json.decode(V.gangmembers)) do
-					if tostring(user_id) == I and V.gangname == turfData[6].gangOwner then
-						turfData[6].commission = price
-						TriggerClientEvent('chatMessage', -1, "^0LSD North trader commission set to "..price.."%", { 128, 128, 128 }, message, "alert")
-						ARMA.updateTraderInfo()
-						TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
-						return
-					end
+	exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
+		for K,V in pairs(gotGangs) do
+			for I,L in pairs(json.decode(V.gangmembers)) do
+				if tostring(user_id) == I and V.gangname == turfData[6].gangOwner then
+					turfData[6].commission = price
+					TriggerClientEvent('chatMessage', -1, "^0LSD North trader commission set to "..price.."%", { 128, 128, 128 }, message, "alert")
+					ARMA.updateTraderInfo()
+					TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
+					return
 				end
 			end
-		end)
-	end
+		end
+	end)
 end)
 
 RegisterNetEvent('ARMA:setNewLSDSouthPrice')
 AddEventHandler('ARMA:setNewLSDSouthPrice', function(price)
     local source = source
 	local user_id = ARMA.getUserId(source)
-	if ARMA.hasGroup(user_id, 'Gang') then
-		exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
-			for K,V in pairs(gotGangs) do
-				for I,L in pairs(json.decode(V.gangmembers)) do
-					if tostring(user_id) == I and V.gangname == turfData[7].gangOwner then
-						turfData[7].commission = price
-						TriggerClientEvent('chatMessage', -1, "^0LSD South trader commission set to "..price.."%", { 128, 128, 128 }, message, "alert")
-						ARMA.updateTraderInfo()
-						TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
-						return
-					end
+	exports['ghmattimysql']:execute('SELECT * FROM arma_gangs', function(gotGangs)
+		for K,V in pairs(gotGangs) do
+			for I,L in pairs(json.decode(V.gangmembers)) do
+				if tostring(user_id) == I and V.gangname == turfData[7].gangOwner then
+					turfData[7].commission = price
+					TriggerClientEvent('chatMessage', -1, "^0LSD South trader commission set to "..price.."%", { 128, 128, 128 }, message, "alert")
+					ARMA.updateTraderInfo()
+					TriggerClientEvent('ARMA:gotTurfOwnershipData', -1, turfData)
+					return
 				end
 			end
-		end)
-	end
+		end
+	end)
 end)
 
 function ARMA.turfSaleToGangFunds(amount, drugtype)
