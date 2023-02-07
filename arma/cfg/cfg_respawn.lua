48609b080f9701511d23129b804ffc6b02776633
Xonaa-Dev <rhysevans6@outlook.com>
2023-02-07 01:32:45 +0000

    tweak(respawn): images

--- arma/cfg/cfg_respawn.lua
+++ arma/cfg/cfg_respawn.lua
@@ -5,35 +5,35 @@ cfg.spawnLocations = {
         name = "Mission Row Police Station",
         coords = vector3(441.07565307618,-983.34790039062,30.689599990844),
         permission = {"police.onduty.permission"},
-        image = "https://cdn.cmg.city/content/fivem/houses/missionrowpd.png",
+        image = "https://i.imgur.com/stYzJMf.png",
         price = 0
     },
     ["Sandy Shores Police Station"] = {
         name = "Sandy Shores Police Station",
         coords = vector3(1853.52734375,3686.9440917969,34.286659240723),
         permission = {"police.onduty.permission"},
-        image = "https://cdn.cmg.city/content/fivem/houses/sandypd.png",
+        image = "https://i.imgur.com/KLeRYTq.png",
         price = 0
     },
     ["Paleto Police Station"] = {
         name = "Paleto Police Station",
         coords = vector3(-441.43273925781,6018.3095703125,31.576251983643),
         permission = {"police.onduty.permission"},
-        image = "https://cdn.cmg.city/content/fivem/houses/paletopd.png",
+        image = "https://i.imgur.com/biWmA3X.png",
         price = 0
     },
     ["Vespucci Police Station"] = {
         name = "Vespucci Police Station",
         coords = vector3(-1102.1322021484,-840.05621337891,19.001459121704),
         permission = {"police.onduty.permission"},
-        image = "https://cdn.cmg.city/content/fivem/houses/vespuccipd.png",
+        image = "https://i.imgur.com/AGJfPON.png",
         price = 0
     },
     ["VIP Island"] = {
         name = "VIP Island",
         coords = vector3(-2168.3569335938,5137.6884765625,2.8299977779388),
         permission = {"vip.aircraft"},
-        image = "https://cdn.cmg.city/content/fivem/houses/vipisland.png",
+        image = "https://i.imgur.com/fWapijz.png",
         price = 0
     },
     ["Rebel Diner"] = {
@@ -47,35 +47,35 @@ cfg.spawnLocations = {
         name = "St Thomas Hospital",
         coords = vector3(363.48422241211,-591.30035400391,28.679122924805),
         permission = {},
-        image = "https://cdn.cmg.city/content/fivem/houses/stthomas.png",
+        image = "https://i.imgur.com/20j1m0Q.png",
         price = 0
     },
     ["Royal London Hospital"] = {
         name = "Royal London Hospital",
         coords = vector3(337.48727416992,-1393.4056396484,32.509284973145),
         permission = {},
-        image = "https://cdn.cmg.city/content/fivem/houses/royallondon.png",
+        image = "https://i.imgur.com/MfrCuDg.png",
         price = 0
     },
     ["Sandy Shores Medical Centre"] = {
         name = "Sandy Shores Medical Centre",
         coords = vector3(1842.0667724609,3669.3212890625,33.679866790771),
         permission = {},
-        image = "https://cdn.cmg.city/content/fivem/houses/sandymedical.png",
+        image = "https://i.imgur.com/8esIXxM.png",
         price = 0
     },
     ["Paleto Medical Centre"] = {
         name = "Paleto Medical Centre",
         coords = vector3(-244.62612915039,6328.8041992188,32.426197052002),
         permission = {},
-        image = "https://cdn.cmg.city/content/fivem/houses/paletomedical.png",
+        image = "https://i.imgur.com/hJ7Koci.png",
         price = 0
     },
     ["Cayo Perico Airport"] = {
         name = "Cayo Perico Airport",
         coords = vector3(4510.021484375,-4513.7797851562,4.0945315361023),
         permission = {"rebellicense.whitelisted"},
-        image = "https://cdn.cmg.city/content/fivem/houses/cayoperico.png",
+        image = "https://i.imgur.com/m0sfoc6.png",
         price = 0
     }
 }
