31ee9744a0ad740c25454f5a97905d10fda09647
rhys <80773857+Rlhys@users.noreply.github.com>
2023-07-03 18:42:01 +0100

    add: (watermark)

---
+++ oasis-name/client/client.lua
@@ -0,0 +1,16 @@
+RegisterNetEvent('TimeAndDateDisplay-FiveM')
+AddEventHandler('TimeAndDateDisplay-FiveM', function(time)
+    if Config.ShowServerName and Config.ServerName then
+        time = time .." | © " .. Config.ServerName
+    end
+    if Config.ShowPlayerName then
+        time = time .."\nPlayer Name: " .. GetPlayerName(NetworkGetEntityOwner(PlayerPedId()))
+    end
+    if Config.ShowPlayerID then
+        time = time .."\nPlayer ID: " .. GetPlayerServerId(NetworkGetEntityOwner(PlayerPedId()))
+    end
+    SendNUIMessage({
+        action = "setTimeAndDate",
+        time = time
+    })
+end)
---
+++ oasis-name/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'bodacious'
+game 'gta5'
+
+name 'TimeAndDateDisplay-FiveM'
+author 'LucaNL#2230'
+description 'A script that allows you to always show the real life time and real life date on the top right of your screen.'
+version 'v2.0.1'
+url 'https://github.com/LucaNL/TimeAndDateDisplay-FiveM'
+
+server_script {
+    'server/server.lua',
+    'version.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client/client.lua',
+    'config.lua'
+}
+
+ui_page "html/index.html"
+
+files {
+    "html/index.html",
+    "html/time.js"
+}
